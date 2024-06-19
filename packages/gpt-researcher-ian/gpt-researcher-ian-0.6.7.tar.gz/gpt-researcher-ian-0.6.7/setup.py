894133e670b6d211996690861b5fc1ea1c33c6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:40 +0000

    Add package gpt-researcher-ian/0.6.7/gpt-researcher-ian-0.6.7.tar.gz

---
+++ packages/gpt-researcher-ian/gpt-researcher-ian-0.6.7.tar.gz/gpt-researcher-ian-0.6.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+exclude_packages = ["selenium", "webdriver", "fastapi", "fastapi.*", "uvicorn", "jinja2"]
+
+with open(r"README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    reqs = [line.strip() for line in f if not any(pkg in line for pkg in exclude_packages)]
+
+setup(
+    name="gpt-researcher-ian",
+    version="0.6.7",
+    description="GPT Researcher is an autonomous agent designed for comprehensive online research on a variety of tasks.",
+    package_dir={'gpt_researcher_ian': 'gpt_researcher_ian'},
+    packages=find_packages(exclude=exclude_packages),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/assafelovic/gpt-researcher",
+    author="Ian de Jesus",
+    author_email="iandjx@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    install_requires=reqs,
+
+
+)
