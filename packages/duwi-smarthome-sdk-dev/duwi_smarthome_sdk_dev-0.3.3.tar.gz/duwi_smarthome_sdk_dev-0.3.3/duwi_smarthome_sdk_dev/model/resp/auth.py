5bf5023c3e5c24742d91fefe97d8c22360467960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:44 +0000

    Add package duwi-smarthome-sdk-dev/0.3.3/duwi_smarthome_sdk_dev-0.3.3.tar.gz

---
+++ packages/duwi-smarthome-sdk-dev/duwi_smarthome_sdk_dev-0.3.3.tar.gz/duwi_smarthome_sdk_dev-0.3.3/duwi_smarthome_sdk_dev/model/resp/auth.py
@@ -0,0 +1,28 @@
+from typing import Optional
+import datetime
+
+
+class AuthToken:
+    def __init__(
+            self,
+            access_token: str,
+            access_token_expire_time: str,
+            refresh_token: str,
+            refresh_token_expire_time: str
+    ):
+        self.access_token: str = access_token
+        self.access_token_expire_time: datetime.datetime = self._parse_datetime(
+            access_token_expire_time) if access_token_expire_time else None
+        self.refresh_token: str = refresh_token
+        self.refresh_token_expire_time: datetime.datetime = self._parse_datetime(
+            refresh_token_expire_time) if refresh_token_expire_time else None
+
+    @staticmethod
+    def _parse_datetime(datetime_str: str) -> datetime.datetime:
+        return datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+    def __str__(self) -> str:
+        return (f"Access Token: {self.access_token}, "
+                f"Access Token Expires On: {self.access_token_expire_time}, "
+                f"Refresh Token: {self.refresh_token}, "
+                f"Refresh Token Expires On: {self.refresh_token_expire_time}")
