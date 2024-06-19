78b061379220f47b2a6843d99b28b0237a6835c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:13 +0000

    Add package dde-agent-lib/1.0.0a9/dde-agent-lib-1.0.0a9.tar.gz

---
+++ packages/dde-agent-lib/dde-agent-lib-1.0.0a9.tar.gz/dde-agent-lib-1.0.0a9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dde-agent-lib',
+    version='1.0.0a9',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author='geogpt',
+    author_email='zhuquezhitu@zhejianglab.com',
+    description='geogpt agent library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+)
