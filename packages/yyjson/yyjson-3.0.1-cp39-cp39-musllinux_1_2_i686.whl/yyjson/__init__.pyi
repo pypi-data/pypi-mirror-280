73d3b9c14f6b96c7b2a6f5b81bf658e4d3d657f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:03 +0000

    Add package yyjson/3.0.1/yyjson-3.0.1-cp39-cp39-musllinux_1_2_i686.whl

---
+++ packages/yyjson/yyjson-3.0.1-cp39-cp39-musllinux_1_2_i686.whl/yyjson/__init__.pyi
@@ -0,0 +1,90 @@
+import enum
+from typing import Any, Optional, List, Dict, Union
+
+class ReaderFlags(enum.IntFlag):
+    STOP_WHEN_DONE = 0x02
+    ALLOW_TRAILING_COMMAS = 0x04
+    ALLOW_COMMENTS = 0x08
+    ALLOW_INF_AND_NAN = 0x10
+    NUMBERS_AS_RAW = 0x20
+
+class WriterFlags(enum.IntFlag):
+    PRETTY = 0x01
+    ESCAPE_UNICODE = 0x02
+    ESCAPE_SLASHES = 0x04
+    ALLOW_INF_AND_NAN = 0x08
+    INF_AND_NAN_AS_NULL = 0x10
+
+Content = Union[str, bytes, List, Dict]
+
+class Document:
+    as_obj: Any
+    def __init__(self, content: Content, flags: Optional[ReaderFlags] = ...): ...
+    def __len__(self) -> int: ...
+    def get_pointer(self, pointer: str) -> Any: ...
+    def dumps(
+        self, flags: Optional[WriterFlags] = ..., at_pointer: Optional[str] = ...
+    ) -> str: ...
+    def patch(
+        self,
+        patch: "Document",
+        *,
+        at_pointer: Optional[str] = None,
+        use_merge_patch: bool = False
+    ) -> "Document": ...
+    @property
+    def is_thawed(self) -> bool: ...
+    def freeze(self) -> None: ...
+    def thaw(self) -> None: ...
+
+def load(
+    fp,
+    *,
+    cls=None,
+    object_hook=None,
+    parse_float=None,
+    parse_int=None,
+    parse_constant=None,
+    object_pairs_hook=None,
+    **kw
+): ...
+def loads(
+    s,
+    *,
+    cls=None,
+    object_hook=None,
+    parse_float=None,
+    parse_int=None,
+    parse_constant=None,
+    object_pairs_hook=None,
+    **kw
+): ...
+def dumps(
+    obj,
+    *,
+    skipkeys=False,
+    ensure_ascii=True,
+    check_circular=True,
+    allow_nan=True,
+    cls=None,
+    indent=None,
+    separators=None,
+    default=None,
+    sort_keys=False,
+    **kw
+): ...
+def dump(
+    obj,
+    fp,
+    *,
+    skipkeys=False,
+    ensure_ascii=True,
+    check_circular=True,
+    allow_nan=True,
+    cls=None,
+    indent=None,
+    separators=None,
+    default=None,
+    sort_keys=False,
+    **kw
+): ...
