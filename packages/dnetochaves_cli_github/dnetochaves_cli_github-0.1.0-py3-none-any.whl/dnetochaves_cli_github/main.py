6d236a0a6620b94d2a87bb7c95c28452da178940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 15:03:14 +0000

    Add package dnetochaves_cli_github/0.1.0/dnetochaves_cli_github-0.1.0-py3-none-any.whl

---
+++ packages/dnetochaves_cli_github/dnetochaves_cli_github-0.1.0-py3-none-any.whl/dnetochaves_cli_github/main.py
@@ -0,0 +1,20 @@
+import os
+
+import typer
+from rich import print
+from dotenv import load_dotenv
+
+from dnetochaves_cli_github.cli.repo import repo_app
+from dnetochaves_cli_github.cli.user import user_app
+
+
+if os.path.isfile(".env"):
+    load_dotenv()
+
+app = typer.Typer()
+
+app.add_typer(repo_app, name="repo", help="repository commands")
+app.add_typer(user_app, name="user", help="user commands")
+
+if __name__ == "__main__":
+    app()
