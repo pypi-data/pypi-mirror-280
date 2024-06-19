a5f6d46ad63f967d1483f3ff762313031854178f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:40 +0000

    Add package spotlight-sdk/0.1.19/spotlight-sdk-0.1.19.tar.gz

---
+++ packages/spotlight-sdk/spotlight-sdk-0.1.19.tar.gz/spotlight-sdk-0.1.19/spotlight/api/rule/data_rule_result/asynchronous.py
@@ -0,0 +1,51 @@
+from typing import Dict, Any, List, Optional
+
+from spotlight.api.rule.data_rule_result.__util import (
+    _get_data_rule_result_request_info,
+    _get_data_rule_results_request_info,
+)
+from spotlight.core.common.decorators import async_data_request
+from spotlight.core.common.requests import (
+    __async_get_request,
+)
+
+
+@async_data_request()
+async def async_get_data_rule_result(id: str) -> Dict[str, Any]:
+    """
+    Asynchronously get data rule result by ID.
+
+    Args:
+        id (str): Data rule result ID
+
+    Returns:
+        Dict[str, Any]: Data rule result response
+    """
+    request_info = _get_data_rule_result_request_info(id)
+    return await __async_get_request(**request_info)
+
+
+@async_data_request()
+def async_get_data_rule_results(
+    data_rule_id: Optional[str] = None,
+    start: Optional[int] = None,
+    end: Optional[int] = None,
+    override_find_all: Optional[bool] = False,
+) -> List[Dict[str, Any]]:
+    """
+    Asynchronously retrieves a list of data rule results. If a `data_rule_id` is provided, it returns results specific to that data rule.
+    Results can also be filtered by a time range, specified by `start` and `end` timestamps.
+
+    Args:
+        data_rule_id (Optional[str]): The ID of the specific data rule to get results for. If None, results for all data rules are returned.
+        start (Optional[int]): The start of the time range as a Unix timestamp. If provided without `end`, fetches results from this timestamp onward.
+        end (Optional[int]): The end of the time range as a Unix timestamp. If provided without `start`, fetches results up to this timestamp.
+        override_find_all (Optional[bool]): If True, overrides the default limitation to fetch all results. This parameter is restricted to admin use only. Non-admin users will receive a 'Forbidden' exception if they attempt to use it. Defaults to False.
+
+    Returns:
+        List[Dict[str, Any]]: A list of data rule result responses.
+    """
+    request_info = _get_data_rule_results_request_info(
+        data_rule_id, start, end, override_find_all
+    )
+    return __async_get_request(**request_info)
