01619a78ed746ca49592a9758f0e5360d6dcd7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 15:03:18 +0000

    Add package aiotdlib/0.24.1/aiotdlib-0.24.1.tar.gz

---
+++ packages/aiotdlib/aiotdlib-0.24.1.tar.gz/aiotdlib-0.24.1/aiotdlib/__init__.py
@@ -0,0 +1,85 @@
+__version__ = "0.24.1"
+
+from .client import Client
+from .client_settings import ClientOptions
+from .client_settings import ClientParseMode
+from .client_settings import ClientProxySettings
+from .client_settings import ClientProxyType
+from .client_settings import ClientSettings
+from .filters import AllFilter
+from .filters import AnimationFilter
+from .filters import AudioFilter
+from .filters import BaseFilter
+from .filters import BaseObjectFilter
+from .filters import BotCommandFilter
+from .filters import ContactFilter
+from .filters import DiceFilter
+from .filters import DocumentFilter
+from .filters import FilterCallable
+from .filters import Filters
+from .filters import GameFilter
+from .filters import InvertedFilter
+from .filters import InvoiceFilter
+from .filters import LocationFilter
+from .filters import MergedFilter
+from .filters import MessageFilter
+from .filters import PhotoFilter
+from .filters import PollFilter
+from .filters import RegexFilter
+from .filters import StickerFilter
+from .filters import TextFilter
+from .filters import UnsupportedFilter
+from .filters import VenueFilter
+from .filters import VideoFilter
+from .filters import VideoNoteFilter
+from .filters import VoiceNoteFilter
+from .filters import XORFilter
+from .handlers import Handler
+from .handlers import HandlerCallable
+from .middlewares import MiddlewareCallable
+from .tdjson import TDJson
+from .tdjson import TDLibLogVerbosity
+from .utils import PendingRequest
+
+__all__ = [
+    "Client",
+    "ClientOptions",
+    "ClientParseMode",
+    "ClientProxySettings",
+    "ClientProxyType",
+    "ClientSettings",
+    "AllFilter",
+    "AnimationFilter",
+    "AudioFilter",
+    "BaseFilter",
+    "BaseObjectFilter",
+    "BotCommandFilter",
+    "ContactFilter",
+    "DiceFilter",
+    "DocumentFilter",
+    "FilterCallable",
+    "Filters",
+    "GameFilter",
+    "InvertedFilter",
+    "InvoiceFilter",
+    "LocationFilter",
+    "MergedFilter",
+    "MessageFilter",
+    "PhotoFilter",
+    "PollFilter",
+    "RegexFilter",
+    "StickerFilter",
+    "TextFilter",
+    "UnsupportedFilter",
+    "VenueFilter",
+    "VideoFilter",
+    "VideoNoteFilter",
+    "VoiceNoteFilter",
+    "XORFilter",
+    "Handler",
+    "HandlerCallable",
+    "MiddlewareCallable",
+    "TDJson",
+    "TDLibLogVerbosity",
+    "PendingRequest",
+]
