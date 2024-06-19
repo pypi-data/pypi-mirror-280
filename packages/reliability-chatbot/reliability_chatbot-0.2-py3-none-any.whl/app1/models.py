8b73587cae672b04b2e2ab97b5043c2290b5f534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:18 +0000

    Add package reliability-chatbot/0.2/reliability_chatbot-0.2-py3-none-any.whl

---
+++ packages/reliability-chatbot/reliability_chatbot-0.2-py3-none-any.whl/app1/models.py
@@ -0,0 +1,59 @@
+
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+from django.db import models
+
+class ChatSession(models.Model):
+    user_name = models.CharField(max_length=255)
+    # session_id = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+
+class SimilarQuestion(models.Model):
+    user_name = models.CharField(max_length=255)
+    # session_id = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+from django.db import models
+
+class ChatSession(models.Model):
+    user_name = models.CharField(max_length=255)
+    # session_id = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+
+class SimilarQuestion(models.Model):
+    user_name = models.CharField(max_length=255)
+    # session_id = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+
+class Feedback(models.Model):
+    # user_name = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+    feedback = models.BooleanField()  # This should be a BooleanField
+
+    def __str__(self):
+        return f"  {'Positive' if self.feedback else 'Negative'}"
+
+
+
+
+
+
+
+
