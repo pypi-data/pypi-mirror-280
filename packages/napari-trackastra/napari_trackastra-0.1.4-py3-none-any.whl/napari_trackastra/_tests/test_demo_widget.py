a39c6a85c35d107cfdab18885cd632e8b3ac46b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:13 +0000

    Add package napari-trackastra/0.1.4/napari_trackastra-0.1.4-py3-none-any.whl

---
+++ packages/napari-trackastra/napari_trackastra-0.1.4-py3-none-any.whl/napari_trackastra/_tests/test_demo_widget.py
@@ -0,0 +1,21 @@
+import napari
+from trackastra.data import example_data_bacteria
+
+from napari_trackastra._widget import Tracker
+
+
+def test_demo_widget():
+    viewer = napari.Viewer()
+    img, mask = example_data_bacteria()
+    viewer.add_image(img)
+    viewer.add_labels(mask)
+
+    # Test widget only on CPU
+    tracker = Tracker(viewer, device="cpu")
+    viewer.window.add_dock_widget(tracker)
+    tracker._run()
+    tracker._save()
+
+
+if __name__ == "__main__":
+    test_demo_widget()
