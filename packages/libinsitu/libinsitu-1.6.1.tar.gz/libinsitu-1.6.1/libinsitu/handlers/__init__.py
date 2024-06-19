a3ce43e1d7355a52521029a7064255e44af2654c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:18 +0000

    Add package libinsitu/1.6.1/libinsitu-1.6.1.tar.gz

---
+++ packages/libinsitu/libinsitu-1.6.1.tar.gz/libinsitu-1.6.1/libinsitu/handlers/__init__.py
@@ -0,0 +1,33 @@
+from libinsitu.handlers import SAURAN, SKYNET
+from libinsitu.handlers.ABOM import ABOMHandler
+from libinsitu.handlers.BSRN import BSRNHandler
+from libinsitu.handlers.ESMAP import ESMAPHandler
+from libinsitu.handlers.IEA_PVPS import IEA_PVPSHandler
+from libinsitu.handlers.ISE_PVLive import ISEPVLive
+from libinsitu.handlers.NREL_MIDC import NRELHandler
+from libinsitu.handlers.RAD import RADHandler
+from libinsitu.handlers.SAURAN import SAURANHandler
+from libinsitu.handlers.SKYNET import SkyNetHandler
+from libinsitu.handlers.enerMENA import EnerMENAHandler
+from libinsitu.handlers.base_handler import InSituHandler
+from libinsitu.handlers.METEO_FRANCE import MeteoFranceHandler
+
+# Static map of handlers
+HANDLERS = {
+    "BSRN" : BSRNHandler,
+    "enerMENA" : EnerMENAHandler,
+    "ABOM" : ABOMHandler,
+    "ESMAP": ESMAPHandler,
+    "ISE_PVLive" : ISEPVLive,
+    "SAURAN" : SAURANHandler,
+    "NREL_MIDC" : NRELHandler,
+    "SURFRAD" : RADHandler,
+    "SOLRAD" : RADHandler,
+    "SKYNET" : SkyNetHandler,
+    "IEA_PVPS" : IEA_PVPSHandler,
+    "METEO_FRANCE": MeteoFranceHandler
+}
+
+def listNetworks() :
+    return sorted(list(HANDLERS.keys()))
+
