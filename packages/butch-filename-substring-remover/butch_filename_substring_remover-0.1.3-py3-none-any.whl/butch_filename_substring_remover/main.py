5686eb7b8686caa49d27a6fb124766a3bca6cc57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:54 +0000

    Add package butch-filename-substring-remover/0.1.3/butch_filename_substring_remover-0.1.3-py3-none-any.whl

---
+++ packages/butch-filename-substring-remover/butch_filename_substring_remover-0.1.3-py3-none-any.whl/butch_filename_substring_remover/main.py
@@ -0,0 +1,21 @@
+
+import os
+import sys
+import argparse
+
+from .rename_directory import rename_directory
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Batch renamer script')
+    parser.add_argument('root_directory', type=str, help='Root directory to rename files and folders in')
+    parser.add_argument('substrings_to_remove', nargs='+', help='List of substrings to remove')
+    args = parser.parse_args()
+
+    try:
+        rename_directory(args.root_directory, args.substrings_to_remove)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
