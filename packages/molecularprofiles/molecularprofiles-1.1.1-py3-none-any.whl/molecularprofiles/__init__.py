4f14187beaae6698fdf2202a12fdc0125d845a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 15:03:17 +0000

    Add package molecularprofiles/1.1.1/molecularprofiles-1.1.1-py3-none-any.whl

---
+++ packages/molecularprofiles/molecularprofiles-1.1.1-py3-none-any.whl/molecularprofiles/__init__.py
@@ -0,0 +1,9 @@
+"""
+molecularprofiles
+
+The package provides a set of useful routines
+for the retrieval and analysis of the meteorological data
+from different data assimilation systems
+"""
+
+from .version import __version__
