0e616f5b464707ae84051c8b2f0d730203515cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:16 +0000

    Add package ataraxis-time/1.0.1/ataraxis_time-1.0.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ataraxis-time/ataraxis_time-1.0.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ataraxis_time/time_helpers/__init__.py
@@ -0,0 +1,5 @@
+"""This package provides general-purpose methods used to work with date and time data.
+
+All methods are directly accessible using package namespace."""
+
+from .helper_functions import convert_time, get_timestamp
