0a90c124d457f9997c2a12b5887a7c850d6e6c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:49 +0000

    Add package fake-bpy-module-latest/20240619/fake_bpy_module_latest-20240619-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240619-py3-none-any.whl/bpy/ops/collection/__init__.pyi
@@ -0,0 +1,160 @@
+import typing
+import collections.abc
+import bpy.types
+
+GenericType1 = typing.TypeVar("GenericType1")
+GenericType2 = typing.TypeVar("GenericType2")
+
+def create(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    name: str | typing.Any = "Collection",
+):
+    """Create an object collection from selected objects
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param name: Name, Name of the new collection
+    :type name: str | typing.Any
+    """
+
+    ...
+
+def export_all(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Invoke all configured exporters on this collection
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
+
+def exporter_add(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    name: str | typing.Any = "",
+):
+    """Add Exporter
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param name: Name, FileHandler idname
+    :type name: str | typing.Any
+    """
+
+    ...
+
+def exporter_export(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    index: typing.Any | None = 0,
+):
+    """Invoke the export operation
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param index: Index, Exporter index
+    :type index: typing.Any | None
+    """
+
+    ...
+
+def exporter_remove(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    index: typing.Any | None = 0,
+):
+    """Remove Exporter
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param index: Index, Exporter index
+    :type index: typing.Any | None
+    """
+
+    ...
+
+def objects_add_active(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    collection: str | None = "",
+):
+    """Add the object to an object collection that contains the active object
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param collection: Collection, The collection to add other selected objects to
+    :type collection: str | None
+    """
+
+    ...
+
+def objects_remove(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    collection: str | None = "",
+):
+    """Remove selected objects from a collection
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param collection: Collection, The collection to remove this object from
+    :type collection: str | None
+    """
+
+    ...
+
+def objects_remove_active(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+    *,
+    collection: str | None = "",
+):
+    """Remove the object from an object collection that contains the active object
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    :param collection: Collection, The collection to remove other selected objects from
+    :type collection: str | None
+    """
+
+    ...
+
+def objects_remove_all(
+    override_context: bpy.types.Context | dict[str, typing.Any] | None = None,
+    execution_context: int | str | None = None,
+    undo: bool | None = None,
+):
+    """Remove selected objects from all collections
+
+    :type override_context: bpy.types.Context | dict[str, typing.Any] | None
+    :type execution_context: int | str | None
+    :type undo: bool | None
+    """
+
+    ...
