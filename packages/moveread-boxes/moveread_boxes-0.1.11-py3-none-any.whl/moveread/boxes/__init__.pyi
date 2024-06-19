1033c782698b3ef409828deb2f5498337f01cc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 15:03:14 +0000

    Add package moveread-boxes/0.1.11/moveread_boxes-0.1.11-py3-none-any.whl

---
+++ packages/moveread-boxes/moveread_boxes-0.1.11-py3-none-any.whl/moveread/boxes/__init__.pyi
@@ -0,0 +1,8 @@
+from .annotations import Annotations, Rectangle, Vec2, exportable, ExportableGrid, ExportableContours, ExportableAnnotations, MissingMeta
+from .export_ import export
+
+__all__ = [
+  'Annotations', 'Rectangle', 'Vec2', 'MissingMeta',
+  'exportable', 'ExportableGrid', 'ExportableContours', 'ExportableAnnotations',
+  'export',
+]
