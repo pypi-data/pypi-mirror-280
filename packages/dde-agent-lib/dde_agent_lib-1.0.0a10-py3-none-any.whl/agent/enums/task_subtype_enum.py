5f2d1fcd049848693a646946933ba2def686bde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:11 +0000

    Add package dde-agent-lib/1.0.0a10/dde_agent_lib-1.0.0a10-py3-none-any.whl

---
+++ packages/dde-agent-lib/dde_agent_lib-1.0.0a10-py3-none-any.whl/agent/enums/task_subtype_enum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class TaskSubtypeEnum(Enum):
+    DEFAULT = "0"
+    NORMAL_QS = "101"
+    DOCUMENT_PARSING = "102"
+    DATA_VISILIZATION = "103"
+    PICTURE_2_TABLE = "104"
