6d2a739fe3a26228948ad86f7f0ab0c2c65aa1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:02 +0000

    Add package nlp-synt-data/0.0.5/nlp_synt_data-0.0.5.tar.gz

---
+++ packages/nlp-synt-data/nlp_synt_data-0.0.5.tar.gz/nlp_synt_data-0.0.5/README.md
@@ -0,0 +1,91 @@
+# nlp-synt-data [![PyPi version](https://img.shields.io/pypi/v/nlp-synt-data.svg)](https://pypi.python.org/pypi/nlp-synt-data/) [![](https://img.shields.io/badge/python-3.7+-blue.svg)](https://www.python.org/downloads/) ![t](https://img.shields.io/badge/status-maintained-yellow.svg) [![](https://img.shields.io/github/license/tommasoromano/nlp-synt-data.svg)](https://github.com/tommasoromano/nlp-synt-data/blob/master/LICENSE.md)
+
+Synthetic Data Tools for Natural Language Processing (NLP) and Large Language Models (LLM) tasks
+
+- generate prompts (and prompt ids)
+- generate synthesized data (and data ids)
+- retrieve prompts and data from ids (to reduce generated dataset size)
+
+## Installation
+
+```
+pip install nlp-synt-data
+```
+
+## PromptGenerator
+
+### generate
+
+```python
+prompts_dict = {
+    "a": ["promptA0", "promptA1"],
+    "b": ["promptB0", "promptB1"],
+    "c": ["promptC0", "promptC1"],
+    "d": ["promptD0", "promptD1"],
+    "e": ["promptE0", "promptE1"],
+}
+PromptGenerator.generate(prompts_dict, [["c","e"],["a","b","d"]])
+```
+
+```
+[('c#0_e#0', 'promptC0 promptE0'), ('c#0_e#1', 'promptC0 promptE1'), ('c#1_e#0', 'promptC1 promptE0'), ('c#1_e#1', 'promptC1 promptE1'), ('a#0_b#0_d#0', 'promptA0 promptB0 promptD0'), ('a#0_b#0_d#1', 'promptA0 promptB0 promptD1'), ('a#0_b#1_d#0', 'promptA0 promptB1
+promptD0'), ('a#0_b#1_d#1', 'promptA0 promptB1 promptD1'), ('a#1_b#0_d#0', 'promptA1 promptB0 promptD0'), ('a#1_b#0_d#1', 'promptA1 promptB0 promptD1'), ('a#1_b#1_d#0', 'promptA1 promptB1 promptD0'), ('a#1_b#1_d#1', 'promptA1 promptB1 promptD1')]
+```
+
+### get
+
+```python
+PromptGenerator.get("c#0_e#0", prompts_dict)
+```
+
+```
+promptC0 promptE0
+```
+
+## DataGenerator
+
+### generate
+
+```python
+texts_with_keys = [
+    "[PERSON]",
+    "[PERSON] is working as a [JOB] in [POS]",
+    ]
+substitutions = {
+    "JOB": ["job0", "job1"],
+    "PERSON": ["person0", "person1"],
+    "POS": ["pos0", "pos1"]
+}
+
+DataGenerator.generate(texts_with_keys, substitutions)
+```
+
+```
+[('t#0_PERSON#0', 'person0'), ('t#0_PERSON#1', 'person1'), ('t#1_JOB#0_PERSON#0_POS#0', 'person0 is working as a job0 in pos0'), ('t#1_JOB#0_PERSON#0_POS#1', 'person0 is working as a job0 in pos1'), ('t#1_JOB#0_PERSON#1_POS#0', 'person1 is working as a job0 in pos0'), ('t#1_JOB#0_PERSON#1_POS#1', 'person1 is working as a job0 in pos1'), ('t#1_JOB#1_PERSON#0_POS#0', 'person0 is working as a job1 in pos0'), ('t#1_JOB#1_PERSON#0_POS#1', 'person0 is working as a job1 in pos1'), ('t#1_JOB#1_PERSON#1_POS#0', 'person1 is working as a job1 in pos0'), ('t#1_JOB#1_PERSON#1_POS#1', 'person1 is working as a job1 in pos1')]
+```
+
+### get
+
+```python
+DataGenerator.get("t#1_JOB#0_PERSON#0_POS#0", texts_with_keys, substitutions)
+```
+
+```
+{'keys': {'JOB': 'job0', 'PERSON': 'person0', 'POS': 'pos0'}, 'text': 'person0 is working as a job0 in pos0', 'text_with_keys': '[PERSON] is working as a [JOB] in [POS]'}
+```
+
+## ResponseGenerator
+
+#### generate
+
+This is an example of using this library with ollama llm models
+
+```python
+prompts = PromptGenerator.generate(prompts_dict, prompt_keys)
+data = DataGenerator.generate(texts_with_keys, substitutions)
+model_func = lambda prompt, text: ollama.chat(model='llama3:instruct', messages=[
+                { 'role': 'system', 'content': prompt, },
+                { 'role': 'user', 'content': text, },
+            ])['message']['content']
+generator.ResponseGenerator.generate("results.csv", data, prompts, model_func)
+```
---
+++ packages/nlp-synt-data/nlp_synt_data-0.0.5.tar.gz/nlp_synt_data-0.0.5/nlp_synt_data/generator.py
@@ -0,0 +1,191 @@
+from itertools import combinations
+import pandas as pd
+import os
+
+class PromptGenerator():
+
+    @staticmethod
+    def generate(
+        prompts_dict: dict,
+        prompt_keys: list[list[str]]
+    ):
+        """- prompts_dict: dict of prompts to be generated
+        - prompt_keys: list of prompt keys to be generated 
+        e.g. [[c,e],] generates [(c#0_e#0, promptC0+promptE0), (c#0_e#1, promptC0+promptE1), ..]
+        - returns: list of tuples of (prompt_id, prompt_text).
+        """
+
+        def check_key(key):
+            if key not in prompts_dict:
+                raise ValueError(f"Key {key} not in prompts_dict")
+            if any(char.isdigit() for char in key):
+                raise ValueError(f"Key {key} contains numbers, which is not allowed")
+            if any(char == "_" for char in key):
+                raise ValueError(f"Key {key} contains underscores, which is not allowed")
+            if any(char == "#" for char in key):
+                raise ValueError(f"Key {key} contains underscores, which is not allowed")
+
+        def add_prompts(key, prev_prompts):
+            return [
+            (f"{p_key}_{key}#{x_i}", p_p + ' ' + x_p)
+            for p_key, p_p in prev_prompts
+            for x_i, x_p in enumerate(prompts_dict[key])
+            ]
+        
+
+        final_prompts = []
+
+        for keys in prompt_keys:
+
+            if len(keys) < 1:
+                raise ValueError("Prompt keys must be at least 1")
+            check_key(keys[0])
+            acc_prompts = [(f"{keys[0]}#{i}", p) for i,p in enumerate(prompts_dict[keys[0]])]
+
+            for key in keys[1:]:
+                check_key(key)
+                acc_prompts = add_prompts(key, acc_prompts)
+
+            final_prompts += acc_prompts
+        return final_prompts
+
+    @staticmethod
+    def get(
+        prompt_id: str,
+        prompts_dict: dict
+    ):
+        """- prompt_id: string of prompt_id to get
+        - prompts_dict: 
+        - returns: prompt
+        """
+        prompt = ""
+        keys = prompt_id.split("_")
+        for _key in keys:
+            n = int(_key.split("#")[1])
+            key = _key.split("#")[0]
+            if key not in prompts_dict:
+                raise ValueError(f"Key {key} not in prompts_dict")
+            if n >= len(prompts_dict[key]):
+                raise ValueError(f"Index {n} out of range for key {key}")
+
+            if prompt != "":
+                prompt += " " + prompts_dict[key][n]
+            else:
+                prompt += prompts_dict[key][n]
+
+        return prompt
+    
+
+class DataGenerator():
+
+    @staticmethod
+    def generate(
+        texts_with_keys: list[tuple[str,str]],
+        substitutions: dict[str,list[tuple[str,str]]],
+    ):
+        """- texts_with_keys: list of (text with keys, label)
+        - substitutions: dict of list of (substitution, label)
+        - returns: list of tuple (text_id, text, list of labels)"""
+
+        final_texts = []
+
+        if "t" in substitutions:
+            raise ValueError("Key 't' is reserved for text id")
+        if any([any(char.isdigit() for char in key) for key in substitutions]):
+            raise ValueError("Keys cannot contain numbers")
+        if any([any(char == '_' for char in key) for key in substitutions]):
+            raise ValueError("Keys cannot contain underscores")
+        if any([any(char == '#' for char in key) for key in substitutions]):
+            raise ValueError("Keys cannot contain #")
+        
+        def add_text(key, prev_prompts):
+            return [
+            (f"{p_key}_{key}#{x_i}", p_p.replace(f"[{key}]", x_p), p_ls+[x_l])
+            for p_key, p_p, p_ls in prev_prompts
+            for x_i, (x_p, x_l) in enumerate(substitutions[key])
+            ]
+        
+        final_texts = []
+
+        for it, (text, label) in enumerate(texts_with_keys):
+            _real_keys = [key for key in substitutions if key in text]
+            acc_texts = [(f"t#{it}", text, [label])]
+            for key in _real_keys:
+                acc_texts = add_text(key, acc_texts)
+            final_texts += acc_texts
+
+        return final_texts
+    
+    @staticmethod
+    def get(
+        text_id: str,
+        texts_with_keys: list[tuple[str,str]],
+        substitutions: dict[str,list[tuple[str,str]]],
+    ):
+        
+        keys = text_id.split("_")
+        if keys[0][:2] != "t#":
+            raise ValueError("Invalid text id")
+        
+        res = {
+            'keys': {},
+        }
+        res['text_with_keys'] = texts_with_keys[int(keys[0].split("#")[1])]
+        text = res['text_with_keys'][0]
+        for _key in keys[1:]:
+            n = int(_key.split("#")[1])
+            key = _key.split("#")[0]
+            if key not in substitutions:
+                raise ValueError(f"Key {key} not in substitutions")
+            if n >= len(substitutions[key]):
+                raise ValueError(f"Index {n} out of range for key {key}")
+            
+            res["keys"][key] = substitutions[key][n]
+            text = text.replace(f"[{key}]", substitutions[key][n][0])
+        res["text"] = (text, res["text_with_keys"][1])
+        return res
+    
+class ResponseGenerator():
+
+    @staticmethod
+    def generate(
+        file_path: str,
+        texts: list[tuple[str, str, str]],
+        prompts: list[tuple[str, str]],
+        model_func: callable,
+        save_every: int = 50,
+        make_labels: bool = False,
+        verbose: bool = True,
+    ):
+        """- data_path_name: name of the data file to save/load
+        - texts: list of tuples (text_id, text)
+        - prompts: list of tuples (prompt_id, prompt)
+        - model_func: function that takes (prompt, text) and returns response
+        """
+
+        try:
+            res_df = pd.read_csv(file_path)
+            verbose and print(f"Loaded {len(res_df)} rows.")
+            make_labels = 'text_labels' in res_df.columns
+        except: 
+            if not make_labels:
+                res_df = pd.DataFrame({'prompt_id':[], 'text_id':[], 'response':[]})
+            else:
+                res_df = pd.DataFrame({'prompt_id':[], 'text_id':[], 'text_labels':[], 'response':[]})
+            verbose and print("Created new dataframe.")
+
+        for prompt_id, prompt in prompts:
+            for text_id, text, labels in texts:
+
+                if len(res_df) > 0 and res_df[(res_df['prompt_id'] == prompt_id) & (res_df['text_id'] == text_id)].shape[0] > 0:
+                    continue
+
+                res = model_func(prompt, text)
+                if make_labels:
+                    res_df.loc[len(res_df)] = [prompt_id, text_id, str(labels), res]
+                else:
+                    res_df.loc[len(res_df)] = [prompt_id, text_id, res]
+
+                if len(res_df) % save_every == 0:
+                    verbose and print(f"Processed {len(res_df)} rows.")
+                    res_df.to_csv(file_path, index=False)
