02e7278810a5b294ebb3d3cca92be405a44b1d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:02 +0000

    Add package mlqf/1.0.0/mlqf-1.0.0.tar.gz

---
+++ packages/mlqf/mlqf-1.0.0.tar.gz/mlqf-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='mlqf',
+      version='1.0.0',
+      description='none',
+      packages=['mlqf'],
+      author_email='mhivanov4@gmail.com',
+      install_requires=['pyperclip','scikit-learn','numpy','IPython'],
+      zip_safe=False)
