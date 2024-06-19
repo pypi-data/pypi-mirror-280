989c6caf8d4ce4be361d416d8215fef8c50c3bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:03 +0000

    Add package qontract-reconcile/0.10.1rc913/qontract_reconcile-0.10.1rc913-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc913-py3-none-any.whl/reconcile/utils/helm.py
@@ -0,0 +1,49 @@
+import json
+import tempfile
+from collections.abc import Mapping
+from subprocess import (
+    CalledProcessError,
+    run,
+)
+from typing import Any
+
+import yaml
+
+from reconcile.utils.runtime.sharding import ShardSpec
+
+
+class HelmTemplateError(Exception):
+    pass
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, ShardSpec):
+            return o.__dict__
+        return super().default(o)
+
+
+def template(values: Mapping[str, Any]) -> Mapping[str, Any]:
+    try:
+        with tempfile.NamedTemporaryFile(mode="w+", encoding="locale") as values_file:
+            values_file.write(json.dumps(values, cls=JSONEncoder))
+            values_file.flush()
+            cmd = [
+                "helm",
+                "template",
+                "./helm/qontract-reconcile",
+                "-n",
+                "qontract-reconcile",
+                "-f",
+                values_file.name,
+            ]
+            result = run(cmd, capture_output=True, check=True)
+    except CalledProcessError as e:
+        msg = f'Error running helm template [{" ".join(cmd)}]'
+        if e.stdout:
+            msg += f" {e.stdout.decode()}"
+        if e.stderr:
+            msg += f" {e.stderr.decode()}"
+        raise HelmTemplateError(msg)
+
+    return yaml.safe_load(result.stdout.decode())
