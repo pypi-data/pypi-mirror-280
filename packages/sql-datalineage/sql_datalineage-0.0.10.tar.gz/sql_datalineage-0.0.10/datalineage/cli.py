e7ab7f28eb36a5b0c0340c6126e30063cf576609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:14 +0000

    Add package sql-datalineage/0.0.10/sql_datalineage-0.0.10.tar.gz

---
+++ packages/sql-datalineage/sql_datalineage-0.0.10.tar.gz/sql_datalineage-0.0.10/datalineage/cli.py
@@ -0,0 +1,53 @@
+import click
+import json
+
+from datalineage.renderer.renderer import Renderer
+from datalineage.renderer.mermaid_renderer import MermaidRenderer
+
+from datalineage.lineage import lineage
+from datalineage.util import safe_read, safe_write
+from datalineage.logger import setup_logger
+
+logger = setup_logger(__name__)
+
+
+@click.command()
+@click.option("-i", "--input", prompt="Input path", help="The input path of the sql.")
+@click.option("-o", "--output", default=None, prompt=False, help="The output path of the result.")
+@click.option(
+    "-d", "--dialect", default=None, prompt=False, help="The dialect which input belong to."
+)
+@click.option(
+    "--schema-path",
+    prompt="Path to schema json file",
+    help="Path to schema json file, which contains sqlglot schema.",
+)
+def make_lineage(input, output, dialect, schema_path):
+    """Generate column level lineage for a query."""
+
+    sql = safe_read(input)
+    if not sql:
+        raise ValueError("Can not read input sql from file: {}".format(input))
+
+    schema = safe_read(schema_path)
+    schema = json.loads(schema) if schema else None
+
+    tree = lineage(sql, dialect, schema)
+
+    out_renderer: Renderer = MermaidRenderer()
+    rendered_content = out_renderer.render(tree)
+
+    if output:
+        logger.info("Rendering output into a file")
+        safe_write(output, rendered_content)
+    else:
+        logger.info("Rendering output into stdout")
+        click.echo(rendered_content)
+
+
+def main():
+    make_lineage()
+
+
+if __name__ == "__main__":
+    main()
