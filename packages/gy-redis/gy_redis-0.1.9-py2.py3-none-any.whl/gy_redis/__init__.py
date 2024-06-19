e3ac3e1ba630ed372902a11f25b7d75ab230e006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:51 +0000

    Add package gy-redis/0.1.9/gy_redis-0.1.9-py2.py3-none-any.whl

---
+++ packages/gy-redis/gy_redis-0.1.9-py2.py3-none-any.whl/gy_redis/__init__.py
@@ -0,0 +1,44 @@
+import sys
+
+from gy_redis.manager import (
+    RedisManager,
+)
+
+from gy_redis.handler import (
+    RedisBytesHandler,
+    RedisImageHandler,
+    RedisDictHandler,
+    RedisConnector,
+)
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+
+def int_or_str(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+try:
+    __version__ = metadata.version("gy_redis")
+except metadata.PackageNotFoundError:
+    __version__ = "99.99.99"
+
+
+try:
+    VERSION = tuple(map(int_or_str, __version__.split(".")))
+except AttributeError:
+    VERSION = tuple(99, 99, 99)
+
+__all__ = [
+    "RedisManager",
+    "RedisConnector",
+    "RedisBytesHandler",
+    "RedisDictHandler",
+    "RedisImageHandler",
+]
