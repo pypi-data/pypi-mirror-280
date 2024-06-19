981dac7347f20283376ea0434993784652002d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 15:03:10 +0000

    Add package posting/1.0.0b14/posting-1.0.0b14.tar.gz

---
+++ packages/posting/posting-1.0.0b14.tar.gz/posting-1.0.0b14/src/posting/widgets/request/url_bar.py
@@ -0,0 +1,89 @@
+from textual import on
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.containers import Horizontal
+from textual.widgets import Input, Button
+
+from posting.highlight_url import URLHighlighter
+from posting.widgets.request.method_selection import MethodSelection
+
+
+class UrlInput(Input):
+    """
+    The URL input.
+    """
+
+    DEFAULT_CSS = """\
+    UrlInput {
+        border: none;
+        width: 1fr;
+        &:focus {
+            border: none;
+            padding: 0 1;
+            & .input--cursor {
+              color: $text;
+              background: $accent-lighten-2;
+            }
+        }
+        &.error {
+            border-left: thick $error;
+        }
+    }
+    """
+
+    BINDINGS = [
+        Binding("down", "app.focus_next", "Focus next"),
+    ]
+
+    def on_mount(self):
+        self.highlighter = URLHighlighter()
+
+    @on(Input.Changed)
+    def on_change(self, event: Input.Changed) -> None:
+        self.remove_class("error")
+
+
+class SendRequestButton(Button, can_focus=False):
+    """
+    The button for sending the request.
+    """
+
+    DEFAULT_CSS = """\
+    SendRequestButton {
+        padding: 0 1;
+        height: 1;
+        min-width: 10;
+        background: $primary;
+        color: $text;
+        border: none;
+        text-style: none;
+        &:hover {
+            text-style: b;
+            padding: 0 1;
+            border: none;
+            background: $primary-darken-1;
+        }
+    }
+    """
+
+
+class UrlBar(Horizontal):
+    """
+    The URL bar.
+    """
+
+    DEFAULT_CSS = """\
+    UrlBar {
+        height: 1;
+        padding: 0 3;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            yield MethodSelection("GET")
+            yield UrlInput(
+                placeholder="Enter a URL...",
+                id="url-input",
+            )
+            yield SendRequestButton("Send")
