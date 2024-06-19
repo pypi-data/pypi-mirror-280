ae06760699505e539eded708082e3974c2806b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:03:11 +0000

    Add package unique-log-questions/0.6/unique_log_questions-0.6-py3-none-any.whl

---
+++ packages/unique-log-questions/unique_log_questions-0.6-py3-none-any.whl/unique_log_question_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('unique_questions/', views.display_unique_questions, name='display_questions_answers'),
+    path('log_questions/', views.display_log_questions, name='display_log_answers'),
+    
+]
