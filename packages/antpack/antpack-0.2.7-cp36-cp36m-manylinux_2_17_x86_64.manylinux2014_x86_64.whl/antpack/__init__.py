94422d717a6e3dd4827e7c92257b97c13f675d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:02:49 +0000

    Add package antpack/0.2.7/antpack-0.2.7-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/antpack/antpack-0.2.7-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/antpack/__init__.py
@@ -0,0 +1,7 @@
+#Used for determining the version if running setup.
+#Only change if building a new version.
+__version__ = "0.2.7"
+
+from .numbering_tools import SingleChainAnnotator, MultiChainAnnotator
+from .vj_tools.vj_gene_assignment import VJGeneTool
+from .scoring_tools import SequenceScoringTool, HumanizationTool, LiabilitySearchTool
