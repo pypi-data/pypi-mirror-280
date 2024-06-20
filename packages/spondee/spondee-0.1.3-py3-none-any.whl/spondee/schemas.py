c064a765d2cdd88040c81fdffe03b61681beda9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:04:01 +0000

    Add package spondee/0.1.3/spondee-0.1.3-py3-none-any.whl

---
+++ packages/spondee/spondee-0.1.3-py3-none-any.whl/spondee/schemas.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+class Sentence(BaseModel):
+    sidx: int = Field("Simple sentence index position.")
+    subject: List[str] = Field(default=[])
+    subject_text: List[str] = Field(default=[])
+    predicate: List[str] = Field(default=[])
+    predicate_text: List[str] = Field(default=[])
+
+class Numeric(BaseModel):
+    sidx: int = Field("Simple sentence index position.")
+    text: str = Field("Numeric value")
+    start_char: int
+    end_char: int
