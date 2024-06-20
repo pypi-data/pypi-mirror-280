3f909e5ac31df5a0143b3c84731e1b1fb8b138f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 14:03:41 +0000

    Add package ataraxis-time/1.0.2/ataraxis_time-1.0.2-pp310-pypy310_pp73-macosx_10_15_x86_64.whl

---
+++ packages/ataraxis-time/ataraxis_time-1.0.2-pp310-pypy310_pp73-macosx_10_15_x86_64.whl/ataraxis_time/precision_timer/__init__.py
@@ -0,0 +1,9 @@
+"""This package provides the PrecisionTimer class with various support functions (such as benchmark() function used to
+evaluate class performance).
+
+PrecisionTimer class acts as the main API access point that allows using the C-bindings for the core fast timer class
+written in C++. It is highly advised to carry out all timer-related operations through this high-level API.
+"""
+
+from .timer_benchmark import benchmark
+from .timer_class import PrecisionTimer
