524a46ea5db47e3309f833468b506a4d8d2f56d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:03:24 +0000

    Add package itk-phasesymmetry/2.0.0/itk_phasesymmetry-2.0.0-cp311-abi3-manylinux2014_x86_64.whl

---
+++ packages/itk-phasesymmetry/itk_phasesymmetry-2.0.0-cp311-abi3-manylinux2014_x86_64.whl/itk/Configuration/PhaseSymmetry_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('butterworth_filter_freq_image_source', 'log_gabor_freq_image_source', 'phase_symmetry_image_filter', 'sinusoid_image_source', 'steerable_filter_freq_image_source', )
