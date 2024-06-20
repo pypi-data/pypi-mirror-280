166f4c04f4e68e5a6a52e353bbddffbac72deadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:30 +0000

    Add package breathmetrics/0.1.0/breathmetrics-0.1.0.tar.gz

---
+++ packages/breathmetrics/breathmetrics-0.1.0.tar.gz/breathmetrics-0.1.0/utils/average_list.py
@@ -0,0 +1,43 @@
+"""
+Module: average_list
+
+This module provides a function for calculating the average of a list after removing outliers.
+
+Dependencies:
+- utils.delete_outliers.delete_outliers (custom utility function for removing outliers from a list)
+
+"""
+from utils.delete_outliers import delete_outliers
+
+
+def average(lst):
+    """
+    Calculate the average of a list after removing outliers.
+
+    Args:
+        lst (List[float]): A list of numerical values.
+
+    Returns:
+        float: The calculated average.
+
+    This function calculates the average of a list after removing outliers using the 'delete_outliers' function.
+
+    Usage:
+    1. Import this function from the module.
+    2. Call the function with a list of numerical values as an argument.
+
+    Example:
+    ```python
+    from utils.average_list import average
+
+    values = [12.5, 13.2, 11.8, 100.0, 12.7, 12.9, 11.5]
+    avg = average(values)
+    ```
+    """
+    sumofall = 0
+    lst = delete_outliers(lst)
+    for i in range(len(lst)):
+        sumofall += lst[i]
+    nooflist = len(lst)
+    average = round((sumofall / nooflist), 2)
+    return average
