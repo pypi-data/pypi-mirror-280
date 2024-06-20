33d949fda5f51743432d2530f7424d95433f4644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:03:07 +0000

    Add package django-customvueadmin/0.2.36/django_customvueadmin-0.2.36.tar.gz

---
+++ packages/django-customvueadmin/django_customvueadmin-0.2.36.tar.gz/django_customvueadmin-0.2.36/custom_admin/views/admin_page.py
@@ -0,0 +1,45 @@
+import json
+
+from django.conf import os, settings
+from django.shortcuts import render
+from django.template.exceptions import TemplateDoesNotExist
+from django.views import View
+
+from custom_admin.utils import get_schema
+
+TEMPLATE_NOT_FOUND = '''
+Custom admin cannot find template "{template}".
+Make sure settings.TEMPLATES contains 'APP_DIRS': True
+'''
+
+
+class CustomAdminView(View):
+    '''
+    # Settings in case of h2ttps:
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    '''
+
+    template = 'custom_admin/admin_index.html'
+
+    def get(self, request):
+        custom_admin = getattr(settings, 'CUSTOM_ADMIN', {})
+
+        admin_settings = {
+            'title': custom_admin.get('title', 'Admin'),
+            'logo_image': custom_admin.get('logo_image'),
+            'backend_prefix': custom_admin.get('backend_prefix'),
+        }
+
+        if not admin_settings.get('backend_prefix'):
+            admin_settings['backend_prefix'] = request.build_absolute_uri('/custom_admin/')
+
+        context = {
+            'SETTINGS': admin_settings,
+            'SETTINGS_JSON': json.dumps(admin_settings),
+        }
+
+        try:
+            return render(request, self.template, context)
+        except TemplateDoesNotExist as e:
+            raise TemplateDoesNotExist(TEMPLATE_NOT_FOUND.format(template=self.template)) from e
