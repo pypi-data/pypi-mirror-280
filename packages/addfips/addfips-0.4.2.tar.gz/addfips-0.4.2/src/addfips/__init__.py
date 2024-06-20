dd0410269e2f81c3f89cb87c74f8486e992abf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:03:22 +0000

    Add package addfips/0.4.2/addfips-0.4.2.tar.gz

---
+++ packages/addfips/addfips-0.4.2.tar.gz/addfips-0.4.2/src/addfips/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# This file is part of addfips.
+# http://github.com/fitnr/addfips
+# Licensed under the GPL-v3.0 license:
+# http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2016, fitnr <fitnr@fakeisthenewreal>
+
+"""
+Add FIPS codes to lists and files that contain the names of US state and counties.
+"""
+
+from .addfips import AddFIPS
+
+__version__ = '0.4.2'
+
+__all__ = ['addfips']
