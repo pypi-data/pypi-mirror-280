fae96bf3cf973fbf31cb3862e5ffede77ef34a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:03:34 +0000

    Add package inspyre-toolbox/1.5.3/inspyre_toolbox-1.5.3.tar.gz

---
+++ packages/inspyre-toolbox/inspyre_toolbox-1.5.3.tar.gz/inspyre_toolbox-1.5.3/inspyre_toolbox/log_engine/__init__.py
@@ -0,0 +1,10 @@
+from inspy_logger import InspyLogger, Loggable
+from inspyre_toolbox.version import parse_version
+
+
+__VERSION__ = parse_version()
+
+ROOT_LOGGER = InspyLogger('InspyreToolbox')
+
+
+ROOT_LOGGER.debug(f'Module loaded: log_engine (inspyre_toolbox - v{__VERSION__})')
