c589ac9dc3f176e267afa851d211717d03bfafd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:37 +0000

    Add package ragstack-ai-langflow-base/0.0.13/ragstack_ai_langflow_base-0.0.13-py3-none-any.whl

---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-0.0.13-py3-none-any.whl/langflow/components/outputs/ChatOutput.py
@@ -0,0 +1,30 @@
+from typing import Optional, Union
+
+from langflow.base.io.chat import ChatComponent
+from langflow.field_typing import Text
+from langflow.schema import Record
+from langflow.schema.message import Message
+
+
+class ChatOutput(ChatComponent):
+    display_name = "Chat Output"
+    description = "Display a chat message in the Playground."
+    icon = "ChatOutput"
+
+    def build(
+        self,
+        sender: Optional[str] = "Machine",
+        sender_name: Optional[str] = "AI",
+        input_value: Optional[Union[str, Message, Record]] = None,
+        session_id: Optional[str] = None,
+        files: Optional[list[str]] = None,
+        return_message: Optional[bool] = False,
+    ) -> Union[Message, Text]:
+        return super().build_with_record(
+            sender=sender,
+            sender_name=sender_name,
+            input_value=input_value,
+            session_id=session_id,
+            files=files,
+            return_message=return_message,
+        )
---
+++ packages/ragstack-ai-langflow-base/ragstack_ai_langflow_base-0.0.13-py3-none-any.whl/langflow/interface/types.py
@@ -0,0 +1,26 @@
+from langflow.custom.utils import build_custom_components
+
+
+custom_components_from_file = {}
+
+
+def get_all_types_dict(components_paths):
+    """Get all types dictionary combining native and custom components."""
+    global custom_components_from_file
+    if len(custom_components_from_file) == 0:
+        custom_components_from_file = build_custom_components(components_paths=components_paths)
+    return custom_components_from_file
+
+
+def get_all_components(components_paths, as_dict=False):
+    """Get all components names combining native and custom components."""
+    all_types_dict = get_all_types_dict(components_paths)
+    components = [] if not as_dict else {}
+    for category in all_types_dict.values():
+        for component in category.values():
+            component["name"] = component["display_name"]
+            if as_dict:
+                components[component["name"]] = component
+            else:
+                components.append(component)
+    return components
