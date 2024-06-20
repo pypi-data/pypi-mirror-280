bc30532a1ce4a81a07b62824f43c692348608a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:10 +0000

    Add package rooms-shared-services/0.1.174/rooms_shared_services-0.1.174-py3-none-any.whl

---
+++ packages/rooms-shared-services/rooms_shared_services-0.1.174-py3-none-any.whl/rooms_shared_services/src/storage/utils.py
@@ -0,0 +1,35 @@
+from typing import Any
+from rooms_shared_services.src.encoders.json import RawDynamodbEncoder
+import json
+from decimal import Decimal
+from uuid import UUID
+from datetime import datetime
+
+
+def convert_to_raw(data_dict: dict, root_level: bool = False) -> dict:
+    return {
+        item_key: convert_to_raw_value(item_value, root_level=root_level)
+        for (item_key, item_value) in data_dict.items()
+    }
+
+def convert_to_raw_value(item_value: Any, root_level: bool = False) -> dict | list:
+    match item_value:
+        case Decimal():
+            item_value_dict = {"N": str(item_value)}
+        case dict():
+            item_value_dict = {"M": json.dumps(item_value, cls=RawDynamodbEncoder)}
+        case list():
+            item_value_dict = {"L": json.dumps(item_value, cls=RawDynamodbEncoder)}
+        case str():
+            item_value_dict = {"S": item_value}
+        case bool():
+            item_value_dict = {"BOOL": "true" if item_value else "false"}
+        case None:
+            item_value_dict = {"NULL": ""}
+        case UUID():
+            item_value_dict = {"S": str(item_value)}
+        case datetime():
+            item_value_dict = {"S": item_value.isoformat()}            
+        case _:
+            raise ValueError("Invalid item value type: {}".format(item_value))
+    return item_value_dict if root_level else list(item_value_dict.values()).pop()
