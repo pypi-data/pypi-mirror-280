d68021296a75ab53c8b4b4d463bb6e2c1b99de87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 14:03:33 +0000

    Add package pyrwkv-tokenizer/0.8.5/pyrwkv_tokenizer-0.8.5-cp39-none-win32.whl

---
+++ packages/pyrwkv-tokenizer/pyrwkv_tokenizer-0.8.5-cp39-none-win32.whl/pyrwkv_tokenizer/__init__.py
@@ -0,0 +1,27 @@
+# import the contents of the Rust library into the Python extension
+from .pyrwkv_tokenizer import *
+from .pyrwkv_tokenizer import __all__
+from pathlib import Path
+
+# optional: include the documentation from the Rust module
+from .pyrwkv_tokenizer import __doc__  # noqa: F401
+
+__all__ = __all__ + ["RWKVTokenizer"]
+__version__ = "0.8.5"
+
+
+class RWKVTokenizer:
+    def __init__(self, name="WorldTokenizer", vocab_filepath=None) -> None:
+        if name != "WorldTokenizer":
+            raise Exception(f"The {name} is not supported.")
+        self.vocab_filepath = str(Path(__path__[0]) / "rwkv_vocab_v20230424.txt") \
+            if vocab_filepath is None else vocab_filepath
+        self.tokenizer = WorldTokenizer(self.vocab_filepath)
+
+    def encode(self, text: str):
+        tokens_ids = self.tokenizer.encode(text)
+        return tokens_ids
+
+    def decode(self, tokens_ids):
+        text = self.tokenizer.decode(tokens_ids)
+        return text
