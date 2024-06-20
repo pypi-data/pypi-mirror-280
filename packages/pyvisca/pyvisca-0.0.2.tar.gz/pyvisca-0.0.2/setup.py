6cf8971b834b4cabd26d78f1ef4c51d97807a3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:02:54 +0000

    Add package pyvisca/0.0.2/pyvisca-0.0.2.tar.gz

---
+++ packages/pyvisca/pyvisca-0.0.2.tar.gz/pyvisca-0.0.2/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='pyvisca',
+    version='0.0.2',
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=[],
+)
