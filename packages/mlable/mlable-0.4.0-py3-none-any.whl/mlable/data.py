8dc04066f456a83aad502d65a67293f5a13701fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:02:54 +0000

    Add package mlable/0.4.0/mlable-0.4.0-py3-none-any.whl

---
+++ packages/mlable/mlable-0.4.0-py3-none-any.whl/mlable/data.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+# PIPELINE ####################################################################
+
+def process(dataset: tf.data.Dataset, pipeline: list, replace: bool=True, feature: str=None) -> tf.data.Dataset:
+    # specify how to combine each operation result with the original dataset
+    __replace = len(list(pipeline)) * [replace] if isinstance(replace, bool) else replace
+    # apply the operation successively  
+    for __fn, __repl in zip(pipeline, __replace):
+        __new = __dataset.map(__fn)
+        __dataset = __new if __repl else __dataset.concatenate(__new)
+    return __dataset
