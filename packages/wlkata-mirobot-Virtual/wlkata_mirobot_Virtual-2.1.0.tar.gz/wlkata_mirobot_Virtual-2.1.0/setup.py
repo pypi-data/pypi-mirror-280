f63b9bdb69cdea224a1cf98c9b126e26f0cf44fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:19 +0000

    Add package wlkata-mirobot-Virtual/2.1.0/wlkata_mirobot_Virtual-2.1.0.tar.gz

---
+++ packages/wlkata-mirobot-Virtual/wlkata_mirobot_Virtual-2.1.0.tar.gz/wlkata_mirobot_Virtual-2.1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import  setup
+import setuptools
+packages = ['wlkata_mirobot_Virtual']
+setup(name='wlkata_mirobot_Virtual',
+	version='2.1.0',
+	author='zuto',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
