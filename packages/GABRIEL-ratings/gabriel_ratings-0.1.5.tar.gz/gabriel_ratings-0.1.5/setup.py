cb10ffd98eb18791c56759f4813a7ebfc61b33cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 06:03:28 +0000

    Add package GABRIEL-ratings/0.1.5/gabriel_ratings-0.1.5.tar.gz

---
+++ packages/GABRIEL-ratings/gabriel_ratings-0.1.5.tar.gz/gabriel_ratings-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GABRIEL-ratings', 
+    version='0.1.5',  
+    author='Hemanth Asirvatham and Elliott Mokski',  
+    author_email='elliottpmokski@gmail.com',  
+    description='THE GABRIEL library for numerical analysis of texts in the social sciences.', 
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/elliottmokski/GABRIEL-distribution',  
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'gabriel': ['Prompts/*.j2'],
+    },
+    install_requires=[
+        'numpy>=1.18.0',
+        'pandas>=1.0.0',
+        'openai>=1.0.0',
+        'Jinja2>=2.11.0',
+        'tqdm>=4.42.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
+    python_requires='>=3.6'
+)
