2a14a35800d193d18dc96de48f511faa2b8e5ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:16 +0000

    Add package netgen-mesher/6.2.2404.post5.dev0/netgen_mesher-6.2.2404.post5.dev0-cp312-cp312-macosx_10_15_universal2.whl

---
+++ packages/netgen-mesher/netgen_mesher-6.2.2404.post5.dev0-cp312-cp312-macosx_10_15_universal2.whl/netgen/__init__.pyi
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from netgen.libngpy._meshing import _Redraw
+from pyngcore.pyngcore import Timer
+from . import config
+from . import libngpy
+__all__ = ['Redraw', 'TimeFunction', 'Timer', 'config', 'libngpy']
+def Redraw(*args, **kwargs):
+    ...
+def TimeFunction(func, name = None):
+    ...
+def _check_python_version():
+    ...
+def _get_diagnostics():
+    ...
+__diagnostics_template: str = '\nNetgen diagnostics:\n    sys.platform:          {sys.platform}\n    sys.executable:        {sys.executable}\n    sys.version:           {sys.version}\n    Netgen python version: {config.PYTHON_VERSION}\n    Netgen path            {__file__}\n    Netgen config          {config.__file__}\n    Netgen version         {config.NETGEN_VERSION}\n    sys.path: {sys.path}\n'
+_netgen_bin_dir: str = '/Users/gitlab-runner/builds/builds/rL7WHzyj/0/ngsolve/netgen/_skbuild/macosx-10.15-universal2-3.12/cmake-install/bin'
+_netgen_lib_dir: str = '/Users/gitlab-runner/builds/builds/rL7WHzyj/0/ngsolve/netgen/_skbuild/macosx-10.15-universal2-3.12/cmake-install/netgen'
