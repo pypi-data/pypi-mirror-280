98f228ce1217d59bbb463e54609c7c931862b0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:23 +0000

    Add package aac-init/0.2.4/aac_init-0.2.4.tar.gz

---
+++ packages/aac-init/aac_init-0.2.4.tar.gz/aac_init-0.2.4/aac_init/scripts/telnet_tool.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2024, Wang Xiao <xiawang3@cisco.com>
+
+import time
+import telnetlib
+
+from aac_init.scripts.logging_tool import setup_logging
+
+logger = setup_logging()
+
+
+class TelnetClient:
+    def __init__(
+            self,
+            telnet_ip,
+            telnet_port,
+            telnet_username,
+            telnet_password
+    ):
+        self.tn = telnetlib.Telnet()
+        self.host_ip = telnet_ip
+        self.port = telnet_port
+        self.username = telnet_username
+        self.password = telnet_password
+
+    def login_host(self):
+        try:
+            self.tn.open(self.host_ip, self.port)
+
+        except Exception as e:
+            logger.error(
+                '{}:{} connected failed!'
+                .format(self.host_ip, self.port)
+            )
+            logger.error(f"Exception details:\n {e}")
+            self.tn.close()
+            return False
+
+        self.tn.read_until(b'login: ', timeout=10)
+        self.tn.write(self.username.encode('ascii') + b'\n')
+        self.tn.read_until(b'Password: ', timeout=10)
+        self.tn.write(self.password.encode('ascii') + b'\n')
+        time.sleep(2)
+        command_result = self.tn.read_very_eager()\
+            .decode('ascii')
+        # print(command_result)
+        if 'Login incorrect' not in command_result:
+            logger.info(
+                'Login {}:{} successfully.'
+                .format(self.host_ip, self.port)
+            )
+            self.tn.write(b"exit\n")
+            self.tn.close()
+            return True
+        else:
+            logger.error(
+                'Login {}:{} failed due to wrong username or password!'
+                .format(self.host_ip, self.port)
+            )
+            self.tn.close()
+            return False
