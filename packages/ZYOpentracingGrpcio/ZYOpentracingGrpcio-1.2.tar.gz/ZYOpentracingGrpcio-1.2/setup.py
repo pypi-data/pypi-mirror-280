b21178ac6eff4ca44da3461526256de6d1c2c8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:31 +0000

    Add package ZYOpentracingGrpcio/1.2/ZYOpentracingGrpcio-1.2.tar.gz

---
+++ packages/ZYOpentracingGrpcio/ZYOpentracingGrpcio-1.2.tar.gz/ZYOpentracingGrpcio-1.2/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ZYOpentracingGrpcio',
+    version='1.2',
+    description='Python OpenTracing Extensions for gRPC',
+    author='LightStep',
+    license='',
+    install_requires=['opentracing>=2.4.0', 'grpcio>=1.63.0', 'six>=1.16.0'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'future'],
+    keywords=['opentracing'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=find_packages(exclude=['docs*', 'tests*', 'examples*']))
