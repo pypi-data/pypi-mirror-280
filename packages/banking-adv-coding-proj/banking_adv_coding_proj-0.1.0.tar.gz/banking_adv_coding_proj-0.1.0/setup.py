6fce74a9e01214968b06ce5bfb81b89191894446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:03:23 +0000

    Add package banking-adv-coding-proj/0.1.0/banking_adv_coding_proj-0.1.0.tar.gz

---
+++ packages/banking-adv-coding-proj/banking_adv_coding_proj-0.1.0.tar.gz/banking_adv_coding_proj-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name = "banking-adv-coding-proj",
+    version = " 0.1.0",
+    author = "Zuha Haider",
+    author_email = "zuha.haider110@gmail.com",
+    description = "A banking system that helps you deposit, withdraw, change account password and shows you your account details.",
+    long_description = open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages= find_packages(),
+    package_data={
+        'mypackage' : ['static/*.png', "static/*.ttf"],
+    },
+    
+    entry_point={
+        "console_scripts": [
+            "run = main",
+            "read = options:read_accounts"
+            
+        ]
+    }
+)
+
