f4fec2211875d901f061ae79444e1cc06ae03810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:03 +0000

    Add package factorio-rcon-py/2.1.3/factorio_rcon_py-2.1.3-py3-none-any.whl

---
+++ packages/factorio-rcon-py/factorio_rcon_py-2.1.3-py3-none-any.whl/factorio_rcon/__init__.py
@@ -0,0 +1,23 @@
+from ._impl import (
+    AsyncRCONClient as AsyncRCONClient,
+    InvalidPassword as InvalidPassword,
+    InvalidResponse as InvalidResponse,
+    PacketType as PacketType,
+    RCONBaseError as RCONBaseError,
+    RCONClient as RCONClient,
+    RCONClosed as RCONClosed,
+    RCONConnectError as RCONConnectError,
+    RCONMessage as RCONMessage,
+    RCONNetworkError as RCONNetworkError,
+    RCONNotConnected as RCONNotConnected,
+    RCONReceiveError as RCONReceiveError,
+    RCONSendError as RCONSendError,
+    RCONSharedBase as RCONSharedBase,
+)
+
+# update the module paths to avoid factorio_rcon._impl.obj names
+for value in locals().copy().values():
+    if getattr(value, "__module__", "").startswith("factorio_rcon."):
+        value.__module__ = __name__
+# make sure value doesn't hang around as a module attribute
+del value  # pylint: disable=undefined-loop-variable
