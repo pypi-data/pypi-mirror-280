99445860da676eafc14a5ef7247dca8fbe86f1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:03:43 +0000

    Add package planet-lib/0.1/planet_lib-0.1-py3-none-any.whl

---
+++ packages/planet-lib/planet_lib-0.1-py3-none-any.whl/planet_lib/math_utils.py
@@ -0,0 +1,25 @@
+def add(a, b):
+    """
+    Sum of two numbers
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: The sum.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Subtract of two numbers
+
+    Args:
+        a (float): First number
+        b (float): Second number
+
+    Returns:
+        float: The difference
+    """
+    return a - b
