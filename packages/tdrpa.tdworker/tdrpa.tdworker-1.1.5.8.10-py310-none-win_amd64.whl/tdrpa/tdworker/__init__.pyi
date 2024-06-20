49f6a9849834d1b402a65616f01cbb2de031e74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:23 +0000

    Add package tdrpa.tdworker/1.1.5.8.10/tdrpa.tdworker-1.1.5.8.10-py310-none-win_amd64.whl

---
+++ packages/tdrpa.tdworker/tdrpa.tdworker-1.1.5.8.10-py310-none-win_amd64.whl/tdrpa/tdworker/__init__.pyi
@@ -0,0 +1,5 @@
+from ._excel import Excel as Excel
+from ._w import Window as Window
+from ._winE import WinElement as WinElement
+from ._winK import WinKeyboard as WinKeyboard
+from ._winM import WinMouse as WinMouse
