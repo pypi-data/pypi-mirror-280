77609e9837cf088b2573ef68e1da4852205bbc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:04:41 +0000

    Add package PandoraBox/0.3.0/pandorabox-0.3.0.tar.gz

---
+++ packages/PandoraBox/pandorabox-0.3.0.tar.gz/pandorabox-0.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PandoraBox',
+    version='0.3.0',
+    packages=find_packages(),
+    description='Python Box Is All You Need. You Can Create Python Environment, Execut Python, Close Python Environment Freely and Easily.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='pydaxing',
+    author_email='pydaxing@gmail.com',
+    url='https://github.com/pydaxing/PythonBox',
+    entry_points={
+        'console_scripts': [
+            'pbox=src.app.main',
+        ],
+    },
+    install_requires=[
+        # 依赖列表
+        'requests',
+        'fastapi',
+        'uvicorn',
+        'pydantic',
+        'jupyter-client',
+        'ipython',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'loguru',
+        'jsonlines',
+    ],
+
+)
+
+
