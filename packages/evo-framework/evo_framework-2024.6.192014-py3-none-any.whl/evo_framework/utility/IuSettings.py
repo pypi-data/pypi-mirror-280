430673ba53602a6cb164a38c2a4f71fab29c648a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:04:20 +0000

    Add package evo-framework/2024.6.192014/evo_framework-2024.6.192014-py3-none-any.whl

---
+++ packages/evo-framework/evo_framework-2024.6.192014-py3-none-any.whl/evo_framework/utility/IuSettings.py
@@ -0,0 +1,11 @@
+#========================================================================================================================================
+# CyborgAI CC BY-NC-ND 4.0 Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 Internation    https://github.com/cyborg-ai-git |
+#========================================================================================================================================
+from evo_framework.core.evo_core_crypto.utility.IuCryptHash import IuCryptHash
+from evo_framework.core.evo_core_crypto.utility.IuCryptChacha import IuCryptChacha
+class IuSettings:
+
+    @staticmethod
+    def doEncrypt(base64:str) ->dict:
+        return {}
+        
