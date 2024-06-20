72c31fb9c5b9e76df4b415641bd4699870513f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:13 +0000

    Add package blackswan-api/0.1.0a42/blackswan_api-0.1.0a42-py3-none-any.whl

---
+++ packages/blackswan-api/blackswan_api-0.1.0a42-py3-none-any.whl/black_swan_ai/types/chat/create_chat_completion_response.py
@@ -0,0 +1,35 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from typing_extensions import Literal
+
+from ..._models import BaseModel
+from .chat_completion_choice import ChatCompletionChoice
+
+__all__ = ["CreateChatCompletionResponse", "Usage"]
+
+
+class Usage(BaseModel):
+    completion_tokens: int
+
+    prompt_tokens: int
+
+    total_tokens: int
+
+
+class CreateChatCompletionResponse(BaseModel):
+    id: str
+
+    choices: List[ChatCompletionChoice]
+
+    created: int
+
+    model: str
+
+    object: Literal["chat.completion"]
+
+    moderate_before: Optional[bool] = None
+
+    system_fingerprint: Optional[str] = None
+
+    usage: Optional[Usage] = None
