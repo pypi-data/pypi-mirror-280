952405732ac4e2ab06c0421f44225b975a65c7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:25 +0000

    Add package project001/0.1.0/project001-0.1.0.tar.gz

---
+++ packages/project001/project001-0.1.0.tar.gz/project001-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['project001', 'project001.pkg1']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.32.3,<3.0.0']
+
+setup_kwargs = {
+    'name': 'project001',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '### this is project001\n',
+    'author': 'johnypeng',
+    'author_email': '2651903873@qq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
