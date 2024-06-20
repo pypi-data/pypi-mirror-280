c815f1dd24c6631f268d68c274fe19dcb6ab1084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:35 +0000

    Add package tiegcmpy/1.5.4/tiegcmpy-1.5.4.tar.gz

---
+++ packages/tiegcmpy/tiegcmpy-1.5.4.tar.gz/tiegcmpy-1.5.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tiegcmpy',
+    version='1.5.4',
+    author = "Nikhil Rao",
+    author_email = "nikhilr@ucar.edu",
+    description='A Python3 post processing tool for TIE-GCM',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/NCAR/tiegcmpy', 
+    python_requires='>=3.8',
+    install_requires=[
+        'cartopy',
+        'matplotlib',
+        'numpy',
+        'xarray'
+        'opencv-python',
+        'ipython',
+        'geomag',
+    ],
+    package_dir={'': 'src'},  
+    packages=find_packages(where='src'), 
+    entry_points={
+        'console_scripts': [
+            'tiegcmpy= tiegcmpy.main:main',
+            'getoptions= tiegcmpy.getoptions:main'
+        ]
+    }
+)
