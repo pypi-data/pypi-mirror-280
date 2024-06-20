5f035b819d5db2f92e0babbd0c2563018599bf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 06:03:39 +0000

    Add package simple-aws-rds/0.5.3/simple_aws_rds-0.5.3-py3-none-any.whl

---
+++ packages/simple-aws-rds/simple_aws_rds-0.5.3-py3-none-any.whl/simple_aws_rds/api.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import exc
+from .exc import StatusError
+from .rds import RDSDBInstanceStatusEnum
+from .rds import RDSDBInstanceStatusGroupEnum
+from .rds import RDSDBInstance
+from .rds import RDSDBInstanceIterProxy
+from .rds import RDSDBSnapshotStatusEnum
+from .rds import RDSDBSnapshot
+from .rds import RDSDBSnapshotIterProxy
