ed527b5087c2a1203e00e6163a1a4c939190b29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:03:17 +0000

    Add package fixposition/0.5.0/fixposition-0.5.0.tar.gz

---
+++ packages/fixposition/fixposition-0.5.0.tar.gz/fixposition-0.5.0/src/fixposition/messages/hdt.py
@@ -0,0 +1,32 @@
+"""nmea hdt message
+https://docs.fixposition.com/fd/nmea-gp-hdt
+"""
+
+from fixposition.checksum import validate_checksum
+from typing import NamedTuple
+
+from roxbot.config import PrecisionConfig
+
+PREC_CFG = PrecisionConfig()
+
+
+class Data(NamedTuple):
+    heading: float
+    true_ind: str
+
+    def to_mqtt(self) -> dict:
+
+        return {
+            "heading": self.heading,
+        }
+
+
+@validate_checksum
+def parse(message: str) -> Data:
+    # Split the message to exclude the start '$', checksum, and end characters
+    data_part = message[1 : message.index("*")]
+
+    # Split the message fields
+    fields = data_part.split(",")
+
+    return Data(heading=round(float(fields[1]), PREC_CFG.angle), true_ind=fields[2])
