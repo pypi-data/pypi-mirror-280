625c7b859b1fc43ac0772b7694bb251dc76f4847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:47 +0000

    Add package xwakes/0.0.0/xwakes-0.0.0.tar.gz

---
+++ packages/xwakes/xwakes-0.0.0.tar.gz/xwakes-0.0.0/setup.py
@@ -0,0 +1,48 @@
+# copyright ############################### #
+# This file is part of the Xwakes Package.  #
+# Copyright (c) CERN, 2021.                 #
+# ######################################### #
+
+from setuptools import setup, find_packages, Extension
+from pathlib import Path
+
+#######################################
+# Prepare list of compiled extensions #
+#######################################
+
+extensions = []
+
+#########
+# Setup #
+#########
+
+version_file = Path(__file__).parent / 'xwakes/_version.py'
+dd = {}
+with open(version_file.absolute(), 'r') as fp:
+    exec(fp.read(), dd)
+__version__ = dd['__version__']
+
+setup(
+    name='xwakes',
+    version=__version__,
+    description='Wakefield and impedance toolbox for Xsuite',
+    long_description='Wakefield and impedance toolbox for Xsuite',
+    url='https://github.com/xsuite/xsuite/issues',
+    author='G. Iadarola et al.',
+    license='Apache 2.0',
+    download_url="https://pypi.python.org/pypi/xwakes",
+    project_urls={
+            "Bug Tracker": "https://github.com/xsuite/xsuite/issues",
+            "Source Code": "https://github.com/xsuite/xwakes/",
+        },
+    packages=find_packages(),
+    ext_modules = extensions,
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.0',
+        'pyyaml',
+        ],
+    extras_require={
+        'tests': ['pytest'],
+        },
+    )
