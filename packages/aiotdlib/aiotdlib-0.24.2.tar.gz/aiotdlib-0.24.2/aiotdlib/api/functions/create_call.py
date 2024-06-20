00d95904ddb26fd3216a8f49e69723748ccc9b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:02:59 +0000

    Add package aiotdlib/0.24.2/aiotdlib-0.24.2.tar.gz

---
+++ packages/aiotdlib/aiotdlib-0.24.2.tar.gz/aiotdlib-0.24.2/aiotdlib/api/functions/create_call.py
@@ -0,0 +1,34 @@
+# =============================================================================== #
+#                                                                                 #
+#    This file has been generated automatically!! Do not change this manually!    #
+#                                                                                 #
+# =============================================================================== #
+from __future__ import annotations
+
+import typing
+
+from pydantic import Field
+
+from ..types.base import *
+
+from ..types.all import (
+    CallProtocol,
+)
+
+
+class CreateCall(BaseObject):
+    """
+    Creates a new call
+
+    :param user_id: Identifier of the user to be called
+    :type user_id: :class:`Int53`
+    :param protocol: The call protocols supported by the application
+    :type protocol: :class:`CallProtocol`
+    :param is_video: Pass true to create a video call
+    :type is_video: :class:`Bool`
+    """
+
+    ID: typing.Literal["createCall"] = Field("createCall", validation_alias="@type", alias="@type")
+    user_id: Int53
+    protocol: CallProtocol
+    is_video: Bool = False
