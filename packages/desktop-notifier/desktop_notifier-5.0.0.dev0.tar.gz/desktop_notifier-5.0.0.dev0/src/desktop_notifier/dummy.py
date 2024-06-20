13d91d3f729a383190de3eacb2b00aefe09613b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 14:03:20 +0000

    Add package desktop-notifier/5.0.0.dev0/desktop_notifier-5.0.0.dev0.tar.gz

---
+++ packages/desktop-notifier/desktop_notifier-5.0.0.dev0.tar.gz/desktop_notifier-5.0.0.dev0/src/desktop_notifier/dummy.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Dummy backend for unsupported platforms
+"""
+
+from __future__ import annotations
+
+# local imports
+from .base import Notification, DesktopNotifierBase
+
+
+class DummyNotificationCenter(DesktopNotifierBase):
+    """A dummy backend for unsupported platforms"""
+
+    def __init__(
+        self,
+        app_name: str = "Python",
+        notification_limit: int | None = None,
+    ) -> None:
+        super().__init__(app_name, notification_limit)
+
+    async def request_authorisation(self) -> bool:
+        """
+        Request authorisation to send notifications.
+
+        :returns: Whether authorisation has been granted.
+        """
+        return True
+
+    async def has_authorisation(self) -> bool:
+        """
+        Whether we have authorisation to send notifications.
+        """
+        return True
+
+    async def _send(
+        self,
+        notification: Notification,
+        notification_to_replace: Notification | None,
+    ) -> None:
+        pass
+
+    async def _clear(self, notification: Notification) -> None:
+        pass
+
+    async def _clear_all(self) -> None:
+        pass
