07461c4606d1250b19c6121582430a921cefc67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 14:03:27 +0000

    Add package tutor-webui/18.0.0/tutor-webui-18.0.0.tar.gz

---
+++ packages/tutor-webui/tutor-webui-18.0.0.tar.gz/tutor-webui-18.0.0/setup.py
@@ -0,0 +1,64 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutorwebui", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="tutor-webui",
+    version=ABOUT["__version__"],
+    url="https://overhang.io/overhangio/tutor-webui",
+    project_urls={
+        "Documentation": "https://docs.tutor.edly.io/",
+        "Code": "https://github.com/overhangio/tutor-webui",
+        "Issue tracker": "https://github.com/overhangio/tutor-webui/issues",
+        "Community": "https://discuss.openedx.org",
+    },
+    license="AGPLv3",
+    author="Overhang.IO",
+    author_email="contact@overhang.io",
+    maintainer="Edly",
+    maintainer_email="hina.khadim@arbisoft.com",
+    description="Web-based user interface plugin for Tutor",
+    long_description=load_readme(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=["tutor>=18.0.0,<19.0.0", "click_repl>=0.3.0"],
+    extras_require={"dev": "tutor[dev]>=18.0.0,<19.0.0"},
+    entry_points={
+        "tutor.plugin.v1": ["webui = tutorwebui.plugin"],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
