da9d04af074a76bce6b5564cffa2875e33146fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:29 +0000

    Add package imfusion-sdk/0.0.4/imfusion_sdk-0.0.4-py3-none-any.whl

---
+++ packages/imfusion-sdk/imfusion_sdk-0.0.4-py3-none-any.whl/imfusion/__main__.py
@@ -0,0 +1,2 @@
+print("This is only a placeholder package as we are restricted by PyPI's file size limit."
+      "\nTo download the real package, use ``pip install imfusion-sdk --extra-index-url=https://pypi.imfusion.com``.")
