8f7764e92398ac7cadbf4152a9106eb93d74c812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:04:23 +0000

    Add package ffs-docker-compose/1.0.0/ffs_docker_compose-1.0.0.tar.gz

---
+++ packages/ffs-docker-compose/ffs_docker_compose-1.0.0.tar.gz/ffs_docker_compose-1.0.0/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import ffs_docker_compose.cli
+
+
+if __name__ == "__main__":
+    print("hello world")
+#    ffs_docker_compose.print_color("hello")
+    ffs_docker_compose.cli()
