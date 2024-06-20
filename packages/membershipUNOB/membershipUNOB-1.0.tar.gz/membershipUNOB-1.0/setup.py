6ac2b36498aac78c1fa0ecbb168d0e3043f79e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 06:03:51 +0000

    Add package membershipUNOB/1.0/membershipUNOB-1.0.tar.gz

---
+++ packages/membershipUNOB/membershipUNOB-1.0.tar.gz/membershipUNOB-1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="membershipUNOB",
+    version="1.0",
+    packages=find_packages(),
+    package_data={
+        "membershipUNOB.utils": ["*.json", "*.html"],
+        "membershipUNOB.gql": ["*.gql"],
+    },
+    include_package_data=True,
+    install_requires=[
+        "beautifulsoup4",
+        "requests",
+        "selenium",
+        "webdriver_manager",
+        "fastapi",
+        "aiohttp",
+        "asyncpg",
+        "aiodataloader",
+        "sqlalchemy",
+        "sqlalchemy_utils",
+        "uvicorn",
+        "gunicorn",
+    ],
+    author="Minh Quang Bui - Quang Tai Do",
+    author_email="minhquang.bui@unob.cz - quangtai.do@unob.cz",
+    description="Membership management for UNOB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/getbricked/membershipunob",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
