5e2cfa83ae5df0969d8073513b75867850c32302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:37 +0000

    Add package astmain/0.0.6/astmain-0.0.6.tar.gz

---
+++ packages/astmain/astmain-0.0.6.tar.gz/astmain-0.0.6/astmain/__init__.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+
+def con():
+    print("conconconconconconconconconcon            :", 111111111111)
+
+
+def fss(file_path, text):
+    try:
+        with open(file_path, "w", encoding='utf-8') as f:
+            f.write(text)
+
+        print("成功  fss          :", 1)
+    except Exception as error:
+        print("失败  fss         :", str(error))
