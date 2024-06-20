940c1cfe533729dca88faf87310a2658348527b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 14:03:26 +0000

    Add package stringaling/0.0.1/stringaling-0.0.1.tar.gz

---
+++ packages/stringaling/stringaling-0.0.1.tar.gz/stringaling-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# python setup.py sdist bdist_wheel
+from setuptools import setup, find_packages
+
+setup(
+    name='stringaling',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+        # e.g. 'numpy>=1.11.1'
+    ],
+)
