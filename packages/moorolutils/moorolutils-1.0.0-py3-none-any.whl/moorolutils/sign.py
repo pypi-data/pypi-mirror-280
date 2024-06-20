8b8cb702170dd3b0832e7790b1b2b00d146f3892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:20 +0000

    Add package moorolutils/1.0.0/moorolutils-1.0.0-py3-none-any.whl

---
+++ packages/moorolutils/moorolutils-1.0.0-py3-none-any.whl/moorolutils/sign.py
@@ -0,0 +1,33 @@
+import json
+import hmac
+import hashlib
+import base64
+
+
+class HMACUtil:
+
+    def __init__(self, data, secret):
+        if isinstance(data, dict):
+            # should be request.body avoid this problem, not drf request.data
+            data = json.dumps(data)
+
+        data = data.replace(' ', '')
+        data = data.replace('\n', '')
+        data = data.replace('\t', '')
+        data = data.replace('\r', '')
+
+        if not isinstance(data, bytes):
+            data = data.encode('utf-8')
+
+        self.data = data
+        self.secret = secret
+
+    def verify_signature(self, signature):
+        if not isinstance(signature, bytes):
+            signature = signature.encode('utf-8')
+        computed_hmac = self.sign()
+        return hmac.compare_digest(computed_hmac, signature)
+
+    def sign(self):
+        digest = hmac.new(self.secret.encode('utf-8'), self.data, digestmod=hashlib.sha256).digest()
+        return base64.b64encode(digest)
