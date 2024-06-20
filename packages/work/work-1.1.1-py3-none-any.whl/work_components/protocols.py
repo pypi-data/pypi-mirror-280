a656dc8dd01574d1e01887760e051af24ae62ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:27 +0000

    Add package work/1.1.1/work-1.1.1-py3-none-any.whl

---
+++ packages/work/work-1.1.1-py3-none-any.whl/work_components/protocols.py
@@ -0,0 +1,24 @@
+"""Protocols (interfaces) for typing."""
+
+from typing import Protocol
+
+# pylint: disable=missing-function-docstring
+
+
+### <MIGRATE-midnight> To be removed in v1.3 ###
+
+
+class IMigratable(Protocol):
+    """Protocol for a migratable object."""
+
+    def migrate_records_touching_midnight(self, end_lower_bound) -> None: ...
+
+
+class IFlags(Protocol):
+    """Protocol of a `Flags` object."""
+
+    def is_set(self, key: str) -> bool: ...
+
+    def set(self, *flags: str) -> None: ...
+
+    def remove(self, *flags: str) -> None: ...
