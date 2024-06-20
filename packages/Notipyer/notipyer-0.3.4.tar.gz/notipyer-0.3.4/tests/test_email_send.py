fe1d910844578ffbc20920b6ac70c7f1aefed13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:03:30 +0000

    Add package Notipyer/0.3.4/notipyer-0.3.4.tar.gz

---
+++ packages/Notipyer/notipyer-0.3.4.tar.gz/notipyer-0.3.4/tests/test_email_send.py
@@ -0,0 +1,24 @@
+from credentials import *
+from notipyer.email_notify import send_email, set_email_config
+
+set_email_config(username, password, name)
+
+html = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi!<br>
+       How are you?<br>
+    </p>
+  </body>
+</html>
+"""
+
+print(send_email(subject="my_subject",
+                 message="my_content",
+                 to_addr=[target_email],
+                 reply_to_addr="chiragjn120@gmail.com",
+                 attachment_path='credentials.py.sample',
+                 is_async=True,
+                 html_text=html))
+print('Email Sent')
