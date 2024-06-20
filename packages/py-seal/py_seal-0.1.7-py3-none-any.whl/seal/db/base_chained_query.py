7adb23982c0095a8243f245ae1357a61a662b079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:04:11 +0000

    Add package py-seal/0.1.7/py_seal-0.1.7-py3-none-any.whl

---
+++ packages/py-seal/py_seal-0.1.7-py3-none-any.whl/seal/db/base_chained_query.py
@@ -0,0 +1,169 @@
+import abc
+from loguru import logger
+
+
+class BaseChainedQuery(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def meta(self):
+        ...
+
+    def __init__(self, clz=None, placeholder=None, table: str = None, logic_delete_col: str = None):
+        self.clz = clz
+        self.table = table if table is not None else self.clz.table_name()
+        self.__conditions: list[tuple] = [(logic_delete_col if logic_delete_col is not None else 'deleted', 0, '=')]
+        self.__select_cols = ()
+        self.__order_by = ()
+        self.ignore_cols = ()
+        self.__raw = ""
+        self.placeholder = placeholder
+
+        if clz is None:
+            self.is_dynamic = True
+            self.table_info = self.meta().get_table_info(self.table)
+        else:
+            if clz.dynamic:
+                self.is_dynamic = True
+                self.table_info = self.meta().get_table_info(self.table)
+            else:
+                self.is_dynamic = False
+
+    def columns(self):
+        if self.is_dynamic:
+            return [key for key, db_type in self.table_info.columns]
+        if len(self.__select_cols) == 0:
+            return [col if col not in self.ignore_cols else '' for col in self.clz.columns()]
+        return self.__select_cols
+
+    def select_cols(self):
+        return ', '.join(self.columns())
+
+    def __args(self):
+        if len(self.__conditions) == 0:
+            return ()
+        return tuple([cond[1] for cond in self.__conditions])
+
+    def __where(self):
+        if len(self.__conditions) == 0:
+            return ''
+        return 'where ' + ' and '.join([f'{cond[0]} {cond[2]} {self.placeholder}' for cond in self.__conditions])
+
+    def raw(self, raw_sql: str):
+        self.__raw = raw_sql
+        return self
+
+    def select(self, *cols):
+        self.__select_cols = cols
+        return self
+
+    def ignore(self, *cols):
+        self.ignore_cols = cols
+        return self
+
+    def eq(self, col, value):
+        self.__conditions.append((col, value, '='))
+        return self
+
+    def ne(self, col, value):
+        self.__conditions.append((col, value, '!='))
+        return self
+
+    def gt(self, col, value):
+        self.__conditions.append((col, value, '>'))
+        return self
+
+    def ge(self, col, value):
+        self.__conditions.append((col, value, '>='))
+        return self
+
+    def lt(self, col, value):
+        self.__conditions.append((col, value, '<'))
+        return self
+
+    def le(self, col, value):
+        self.__conditions.append((col, value, '<='))
+        return self
+
+    def in_(self, col, value):
+        self.__conditions.append((col, value, 'in'))
+        return self
+
+    def l_like(self, col, value):
+        self.__conditions.append((col, f'%{value}', 'like'))
+        return self
+
+    def r_like(self, col, value):
+        self.__conditions.append((col, f'{value}%', 'like'))
+        return self
+
+    def like(self, col, value):
+        self.__conditions.append((col, f'%{value}%', 'like'))
+        return self
+
+    def order_by(self, *sorts):
+        self.__order_by = sorts
+        return self
+
+    def count_statement(self) -> tuple[str, tuple]:
+        sql = f'SELECT count(1) FROM {self.table} {self.__where()}'
+        args = self.__args()
+        logger.info(f'#### sql: {sql}')
+        logger.info(f'#### args: {args}')
+        return sql, args
+
+    def select_statement(self) -> tuple[str, tuple]:
+        order_by = ', '.join(f'{sort[0]} {sort[1]}' for sort in self.__order_by)
+        order_by = '' if len(self.__order_by) == 0 else f'order by {order_by}'
+        sql = f'SELECT {self.select_cols()} FROM {self.table} {self.__where()} {order_by}'
+        args = self.__args()
+        logger.info(f'#### sql: {sql}')
+        logger.info(f'#### args: {args}')
+        return sql, args
+
+    def page_statement(self, page: int, page_size: int) -> tuple[str, tuple]:
+        limit = f'limit {page_size} offset {(page - 1) * page_size}'
+        order_by = ', '.join(f'{sort[0]} {sort[1]}' for sort in self.__order_by)
+        order_by = '' if len(self.__order_by) == 0 else f'order by {order_by}'
+        sql = f'SELECT {self.select_cols()} FROM {self.table} {self.__where()} {order_by} {limit}'
+        args = self.__args()
+        logger.info(f'#### sql: {sql}')
+        logger.info(f'#### args: {args}')
+        return sql, args
+
+    def mapping_statement(self) -> tuple[str, tuple]:
+        sql = f'{self.__raw} {self.__where()}'
+        args = self.__args()
+        logger.info(f'#### sql: {sql}')
+        logger.info(f'#### args: {args}')
+        return sql, args
+
+    def fetchall(self, cursor) -> list:
+        rows = cursor.fetchall()
+        entities = []
+        for row in rows:
+            if self.is_dynamic:
+                if type(row) is dict:
+                    entities.append(row)
+                else:
+                    entities.append({field[0]: row[i] for i, field in enumerate(self.table_info.model_fields)})
+            else:
+                if type(row) is dict:
+                    entities.append(self.clz(**row))
+                else:
+                    entities.append(self.clz(**{col: row[i] for i, col in enumerate(self.clz.columns())}))
+        return entities
+
+    def fetchone(self, cursor):
+        row = cursor.fetchone()
+        if row is None:
+            return None
+        if self.is_dynamic:
+            if type(row) is dict:
+                return row
+            else:
+                return {field[0]: row[i] for i, field in enumerate(self.table_info.model_fields)}
+        else:
+            if type(row) is dict:
+                return self.clz(**row)
+            else:
+                return self.clz(**{col: row[i] for i, col in enumerate(self.clz.columns())})
---
+++ packages/py-seal/py_seal-0.1.7-py3-none-any.whl/seal/db/sqlite/__init__.py
@@ -0,0 +1,2 @@
+from .chained_query import ChainedQuery, ChainedQuery as Query, ChainedQuery as Q
+from .chained_update import ChainedUpdate, ChainedUpdate as Update, ChainedUpdate as U
