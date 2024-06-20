7af5ac8570b2642e151ca3b7d0c2a532922dcba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:03:04 +0000

    Add package cbx/0.1.6/cbx-0.1.6.tar.gz

---
+++ packages/cbx/cbx-0.1.6.tar.gz/cbx-0.1.6/cbx/dynamics/__init__.py
@@ -0,0 +1,15 @@
+from .pdyn import ParticleDynamic, CBXDynamic
+from .cbo import CBO
+from .cbo_memory import CBOMemory
+from .pso import PSO
+from .cbs import CBS
+from .polarcbo import PolarCBO
+
+__all__ = ['ParticleDynamic', 
+           'CBXDynamic', 
+           'CBO', 
+           'CBOMemory', 
+           'PSO', 
+           'CBS',
+           'PolarCBO']
+
---
+++ packages/cbx/cbx-0.1.6.tar.gz/cbx-0.1.6/cbx/dynamics/cbs.py
@@ -0,0 +1,64 @@
+import warnings
+#%%
+import numpy as np
+from .pdyn import CBXDynamic
+from ..scheduler import scheduler
+#%%
+class CBS(CBXDynamic):
+    def __init__(self, f, mode='sampling', noise='covariance',
+                 M=1,
+                 track_args=None, 
+                 **kwargs):
+        track_args = track_args if track_args is not None else{'names':[]}
+        super().__init__(f, track_args=track_args, noise=noise, M=M, **kwargs)
+        
+        if self.batched:
+            raise NotImplementedError('Batched mode not implemented for CBS!')
+        if self.x.ndim > 3:
+            raise NotImplementedError('Multi dimensional domains not implemented for CBS! The particle should have the dimension M x N x d, where d is an integer!')
+            
+        self.exp_dt = np.exp(-self.dt)
+        if mode == 'sampling':
+            self.lamda = 1/(1 + self.alpha)
+        elif mode == 'optimization':
+            self.lamda = 1
+        else:
+            raise NotImplementedError("Invalid mode")
+        
+        if noise not in ['covariance', 'sampling']:
+            raise warnings.warn('For CBS usually covariance or sampling noise is used!', stacklevel=2)
+        
+        
+    def inner_step(self,):
+        self.consensus, energy = self.compute_consensus()
+        self.energy = energy
+        self.drift = self.x - self.consensus
+        self.update_covariance()
+        self.x = self.consensus + self.exp_dt * self.drift + self.noise()
+        
+    def run(self, sched = 'default'):
+            if self.verbosity > 0:
+                print('.'*20)
+                print('Starting Run with dynamic: ' + self.__class__.__name__)
+                print('.'*20)
+
+            if sched is None:
+                sched = scheduler(self, [])
+            elif sched == 'default':
+                sched = self.default_sched()
+            else:
+                if not isinstance(sched, scheduler):
+                    raise RuntimeError('Unknonw scheduler specified!')
+
+            while not self.terminate():
+                self.step()
+                sched.update(self)
+                
+    def optimize(self, sched = 'default'):
+        self.run(sched=sched)
+        
+    def default_sched(self,):
+        return scheduler([])
+        
+    def process_particles(self,):
+        pass
