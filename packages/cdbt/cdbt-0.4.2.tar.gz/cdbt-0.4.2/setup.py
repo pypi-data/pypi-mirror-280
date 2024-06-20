8b0f245831876e199f058f50eb6cd1a2813b4045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:04:07 +0000

    Add package cdbt/0.4.2/cdbt-0.4.2.tar.gz

---
+++ packages/cdbt/cdbt-0.4.2.tar.gz/cdbt-0.4.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="cdbt",
+    version="0.4.2",
+    description="A CLI tool to manage dbt builds with state handling and manifest management",
+    author="Craig Lathrop",
+    author_email="development@coldborecapital.com",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "pyperclip",
+        "snowflake-connector-python[pandas]",
+        "python-dotenv",
+        "openai",
+        "sqlfluff",
+        "sqlfluff-templater-dbt",
+        "wordninja",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cdbt=cdbt.cmdline:cdbt",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
