0efeffe67b1051ea84f3b56c11da9156c68e7c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:13 +0000

    Add package github_repo_traffic/2.1.1/github_repo_traffic-2.1.1.tar.gz

---
+++ packages/github_repo_traffic/github_repo_traffic-2.1.1.tar.gz/github_repo_traffic-2.1.1/github_repo_traffic/readme.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv(Path.cwd() / ".env")
+    workplace_path = Path(os.getenv("GITHUB_WORKSPACE")).resolve() / os.getenv("TRAFFIC_DIR")
+    github_path = Path(os.getenv("GITHUB_WORKSPACE")).resolve() / ".github"
+
+    readme_path = next((path for path in github_path.iterdir() if path.stem.startswith("TEMPLATE_README")), None)
+
+    if readme_path is None:
+        raise Exception("Error: A README template must be present in the base of the .github directory")
+
+    with open(readme_path, encoding="utf-8") as readme_template:
+        readme_template = readme_template.read()
+
+    if (workplace_path / "views_chart.txt").exists():
+        with open(workplace_path / "views_chart.txt", encoding="utf-8") as views_chart:
+            views_chart = views_chart.read()
+    else:
+        views_chart = ""
+
+    if (workplace_path / "clones_chart.txt").exists():
+        with open(workplace_path / "clones_chart.txt", encoding="utf-8") as clones_chart:
+            clones_chart = clones_chart.read()
+    else:
+        clones_chart = ""
+
+    readme = readme_template.format(VIEWS_CHART=views_chart, CLONES_CHART=clones_chart)
+
+    readme_name = readme_path.name.replace("TEMPLATE_", "")
+
+    with open(Path(os.getenv("GITHUB_WORKSPACE")).resolve() / readme_name, "w", encoding="utf-8") as readme_file:
+        readme_file.write(readme)
+
+
+if __name__ == "__main__":
+    main()
