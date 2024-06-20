606c885e3c6a5c045ecb5687574bb28c54c46686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:03:28 +0000

    Add package dataverse-sdk/1.3.2/dataverse_sdk-1.3.2-py3-none-any.whl

---
+++ packages/dataverse-sdk/dataverse_sdk-1.3.2-py3-none-any.whl/dataverse_sdk/constants.py
@@ -0,0 +1,21 @@
+from enum import Enum, EnumMeta
+from typing import Any, Optional
+
+
+class BaseEnumMeta(EnumMeta):
+    _value_set: Optional[set[Any]] = None
+
+    def __contains__(cls, item):
+        if cls._value_set is None:
+            cls._value_set: set[Any] = {v.value for v in cls.__members__.values()}
+        return item in cls._value_set
+
+
+class DataverseHost(str, Enum, metaclass=BaseEnumMeta):
+    DEV = "https://dev.dataverse.linkervision.ai/curation"
+    DEV2 = "https://dev2.dataverse.linkervision.ai/curation"
+    DEV3 = "https://dev3.dataverse.linkervision.ai/curation"
+    STAGING = "https://staging.dataverse.linkervision.ai/curation"
+    DEMO = "https://demo.dataverse.linkervision.ai"
+    PRODUCTION = "https://dataverse.linkervision.ai"
+    LOCAL = "http://localhost:8000"
