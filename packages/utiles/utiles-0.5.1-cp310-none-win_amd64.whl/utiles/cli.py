040ea230b7752e5414ebaa2152bd5a86787beb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:04:08 +0000

    Add package utiles/0.5.1/utiles-0.5.1-cp310-none-win_amd64.whl

---
+++ packages/utiles/utiles-0.5.1-cp310-none-win_amd64.whl/utiles/cli.py
@@ -0,0 +1,27 @@
+"""Utiles cli"""
+
+from __future__ import annotations
+
+import logging
+import sys
+
+from utiles import ut_cli
+
+logger = logging.getLogger(__name__)
+
+
+def cli() -> None:
+    args = sys.argv[1:]
+
+    # if first arg is "utiles" then remove it
+    if args and (args[0] == "utiles" or args[0] == "ut"):
+        args = args[1:]
+    try:
+        ut_cli(["ut", *args])
+    except Exception as e:
+        logger.error(e)
+        raise e from e
+
+
+if __name__ == "__main__":
+    cli()
