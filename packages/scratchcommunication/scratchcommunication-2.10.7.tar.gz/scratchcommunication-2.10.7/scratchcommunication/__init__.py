995a9473b6d843001f98850eebca271f800f9e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:04:38 +0000

    Add package scratchcommunication/2.10.7/scratchcommunication-2.10.7.tar.gz

---
+++ packages/scratchcommunication/scratchcommunication-2.10.7.tar.gz/scratchcommunication-2.10.7/scratchcommunication/__init__.py
@@ -0,0 +1,17 @@
+"""
+Module for communicating with scratch projects.
+"""
+
+__version_number__ = '2.10.7'
+
+from .session import *
+from .cloud import *
+from .cloudrequests import RequestHandler
+
+from . import session
+from . import cloud
+from . import exceptions
+from . import headers
+from . import security
+from . import cloud_socket
+from . import cloudrequests
