5197f042a8903f03a4fc79c0e43bd3fd222835c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:53 +0000

    Add package musicmaster/1.2.11/musicmaster-1.2.11.tar.gz

---
+++ packages/musicmaster/musicmaster-1.2.11.tar.gz/musicmaster-1.2.11/src/music/context/context_.py
@@ -0,0 +1,25 @@
+import os
+from functools import cached_property
+
+from package_utils.context import Context as Context_
+
+from ..clients import spotify
+from ..models import Config, Options, Secrets
+from ..storage.storage import Storage
+
+
+class Context(Context_[Options, Config, Secrets]):
+    @cached_property
+    def spotify_client(self) -> spotify.Client:
+        return spotify.Client(self.secrets)
+
+    @cached_property
+    def storage(self) -> Storage:
+        return Storage()
+
+    @cached_property
+    def is_running_in_ci(self) -> bool:
+        return "GITHUB_ACTIONS" in os.environ
+
+
+context = Context(Options, Config, Secrets)
