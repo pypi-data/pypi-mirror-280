97dafc3bbe33638f1015baf05a2d58243fab4a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:09 +0000

    Add package lshmm/0.0.6/lshmm-0.0.6.tar.gz

---
+++ packages/lshmm/lshmm-0.0.6.tar.gz/lshmm-0.0.6/tests/test_api_fb_diploid.py
@@ -0,0 +1,123 @@
+import pytest
+
+from . import lsbase
+import lshmm as ls
+import lshmm.core as core
+import lshmm.fb_diploid as fbd
+
+
+class TestForwardBackwardDiploid(lsbase.ForwardBackwardAlgorithmBase):
+    def verify(self, ts, scale_mutation_rate, include_ancestors):
+        for n, m, H_vs, query, e_vs, r, mu in self.get_examples_pars(
+            ts,
+            ploidy=2,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+            include_extreme_rates=True,
+        ):
+            G_vs = core.convert_haplotypes_to_phased_genotypes(H_vs)
+            s = core.convert_haplotypes_to_unphased_genotypes(query)
+            num_alleles = core.get_num_alleles(H_vs, query)
+
+            F_vs, c_vs, ll_vs = fbd.forward_ls_dip_loop(
+                n=n,
+                m=m,
+                G=G_vs,
+                s=s,
+                e=e_vs,
+                r=r,
+                norm=True,
+            )
+            B_vs = fbd.backward_ls_dip_loop(
+                n=n,
+                m=m,
+                G=G_vs,
+                s=s,
+                e=e_vs,
+                c=c_vs,
+                r=r,
+            )
+            F, c, ll = ls.forwards(
+                reference_panel=G_vs,
+                query=s,
+                num_alleles=num_alleles,
+                prob_recombination=r,
+                prob_mutation=mu,
+                scale_mutation_rate=scale_mutation_rate,
+                normalise=True,
+            )
+            B = ls.backwards(
+                reference_panel=G_vs,
+                query=s,
+                num_alleles=num_alleles,
+                normalisation_factor_from_forward=c,
+                prob_recombination=r,
+                prob_mutation=mu,
+                scale_mutation_rate=scale_mutation_rate,
+            )
+
+            self.assertAllClose(F, F_vs)
+            self.assertAllClose(B, B_vs)
+            self.assertAllClose(ll_vs, ll)
+
+    @pytest.mark.parametrize("scale_mutation_rate", [True, False])
+    @pytest.mark.parametrize("include_ancestors", [True, False])
+    def test_ts_simple_n10_no_recomb(self, scale_mutation_rate, include_ancestors):
+        ts = self.get_ts_simple_n10_no_recomb()
+        self.verify(
+            ts,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+        )
+
+    @pytest.mark.parametrize("scale_mutation_rate", [True, False])
+    @pytest.mark.parametrize("include_ancestors", [True, False])
+    def test_ts_simple_n6(self, scale_mutation_rate, include_ancestors):
+        ts = self.get_ts_simple_n6()
+        self.verify(
+            ts,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+        )
+
+    @pytest.mark.parametrize("scale_mutation_rate", [True, False])
+    @pytest.mark.parametrize("include_ancestors", [True, False])
+    def test_ts_simple_n8(self, scale_mutation_rate, include_ancestors):
+        ts = self.get_ts_simple_n8()
+        self.verify(
+            ts,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+        )
+
+    @pytest.mark.parametrize("scale_mutation_rate", [True, False])
+    @pytest.mark.parametrize("include_ancestors", [True, False])
+    def test_ts_simple_n8_high_recomb(self, scale_mutation_rate, include_ancestors):
+        ts = self.get_ts_simple_n8_high_recomb()
+        self.verify(
+            ts,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+        )
+
+    @pytest.mark.parametrize("scale_mutation_rate", [True, False])
+    @pytest.mark.parametrize("include_ancestors", [True, False])
+    def test_ts_simple_n16(self, scale_mutation_rate, include_ancestors):
+        ts = self.get_ts_simple_n16()
+        self.verify(
+            ts,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+        )
+
+    @pytest.mark.parametrize("scale_mutation_rate", [True, False])
+    @pytest.mark.parametrize("include_ancestors", [True, False])
+    def test_ts_larger(self, scale_mutation_rate, include_ancestors):
+        ts = self.get_ts_custom_pars(
+            ref_panel_size=45, length=1e5, mean_r=1e-5, mean_mu=1e-5
+        )
+        self.verify(
+            ts,
+            scale_mutation_rate=scale_mutation_rate,
+            include_ancestors=include_ancestors,
+        )
