b01cbc7dfe63ead1c43c0d4bee665d3f2369f9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 14:03:21 +0000

    Add package quantaforge/0.1.1/quantaforge-0.1.1.tar.gz

---
+++ packages/quantaforge/quantaforge-0.1.1.tar.gz/quantaforge-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages, Extension
+import numpy as np
+
+setup(
+    name='quantaforge',
+    version='0.1.1',
+    author='Nicholas Davidson',
+    author_email='nrddodger@gmail.com',
+    description='A comprehensive library for creating and backtesting trading strategies.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ndavidson19/QuantaForge',
+    package_data = {
+        'quantaforge/cython_modules': ['cy_strategy.pyx','cy_portfolio.pyx'],
+    },
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    setup_requires=[
+        'Cython',
+        'numpy'
+    ],
+    install_requires=[
+        'scikit-learn',
+        'polars',
+        'Cython',
+        'numpy',
+        'ib_insync',
+        'confluent_kafka'
+    ],
+    include_dirs=[np.get_include()],
+)
