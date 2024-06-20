859394698b3a35d7dd7a718095fb02a92b9560c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:03:02 +0000

    Add package logger-local/0.0.155/logger_local-0.0.155-py3-none-any.whl

---
+++ packages/logger-local/logger_local-0.0.155-py3-none-any.whl/logger_local/Logger.py
@@ -0,0 +1,5 @@
+# backword compatibility
+from python_sdk_remote.utilities import deprecation_warning
+from .LoggerLocal import Logger  # noqa
+
+deprecation_warning(old_name="Logger", new_name="LoggerLocal")
