3bb4bfdfa01cd003951217d7c9e02b7ad9464fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:35 +0000

    Add package qpop/1.2/qpop-1.2.tar.gz

---
+++ packages/qpop/qpop-1.2.tar.gz/qpop-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qpop",
+    version="1.2",
+    description="qpop --queue queue.txt --gpus 0,1,2,3",
+    readme='README.md',
+    packages=find_packages(),
+    install_requires=['inputimeout'],
+    entry_points={
+        'console_scripts': [
+            'qpop = qpop.runner:main',
+        ],
+    }
+)
