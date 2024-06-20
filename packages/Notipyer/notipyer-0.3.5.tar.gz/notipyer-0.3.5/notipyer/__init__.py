89540c2cf74ea148f7a8c34a1b80eae03a627f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 15:03:31 +0000

    Add package Notipyer/0.3.5/notipyer-0.3.5.tar.gz

---
+++ packages/Notipyer/notipyer-0.3.5.tar.gz/notipyer-0.3.5/notipyer/__init__.py
@@ -0,0 +1,16 @@
+from .credential_handler import credentials
+
+__version__ = '0.3.5'
+__name__ = 'Notipyer'
+__short_desc__ = 'Notification Triggers for Python'
+__url__ = 'https://github.com/chirag-jn/notipyer'
+
+
+creds = None
+
+
+def _get_creds():
+    global creds
+    if creds is None:
+        creds = credentials()
+    return creds
