37edb46ede8c121b4adfdf48396808bd63aa1cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 06:03:31 +0000

    Add package WDai/0.2/wdai-0.2.tar.gz

---
+++ packages/WDai/wdai-0.2.tar.gz/wdai-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='WDai',
+    version='0.2',
+    license='MIT',
+    description='Herramienta para procesamiento distribuido de modelos de machine learning',
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    author='Nakato',
+    author_email='christianvelasces@gmail.com',
+    url='https://github.com/nakato156/dai',
+    keywords=['distribution', 'ia', 'server', "workers", "package", "machine", "learning", "parallel"],
+    packages=find_packages(),
+)
