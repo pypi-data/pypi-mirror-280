dd133983cff2f141e08a3a8ca37591aa80ae0bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:25 +0000

    Add package draive/0.16.3/draive-0.16.3.tar.gz

---
+++ packages/draive/draive-0.16.3.tar.gz/draive-0.16.3/src/draive/types/image.py
@@ -0,0 +1,29 @@
+from draive.parameters.model import DataModel
+
+__all__ = [
+    "ImageBase64Content",
+    "ImageContent",
+    "ImageDataContent",
+    "ImageURLContent",
+]
+
+
+class ImageURLContent(DataModel):
+    mime_type: str | None = None
+    image_url: str
+    image_description: str | None = None
+
+
+class ImageBase64Content(DataModel):
+    mime_type: str | None = None
+    image_base64: str
+    image_description: str | None = None
+
+
+class ImageDataContent(DataModel):
+    mime_type: str | None = None
+    image_data: bytes
+    image_description: str | None = None
+
+
+ImageContent = ImageURLContent | ImageBase64Content | ImageDataContent
