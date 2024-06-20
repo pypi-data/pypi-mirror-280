153e674044149324b025db67f2d99553a72e495f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:09 +0000

    Add package spacr/0.0.71/spacr-0.0.71-py3-none-any.whl

---
+++ packages/spacr/spacr-0.0.71-py3-none-any.whl/spacr/__main__.py
@@ -0,0 +1,13 @@
+"""
+Copyright © 2024 Something
+"""
+
+import sys, os, glob, pathlib, time
+import numpy as np
+from natsort import natsorted
+from tqdm import tqdm
+#from spacr import utils, io, version, timelapse, plot, core, mask_app, annotate_app
+import logging
+
+if __name__ == "__main__":
+    main()
