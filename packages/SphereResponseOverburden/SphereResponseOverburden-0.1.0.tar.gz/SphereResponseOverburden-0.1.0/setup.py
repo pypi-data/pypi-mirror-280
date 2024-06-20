dff2b7de470cfad02a5de7d28b57ed698c1689de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:02:58 +0000

    Add package SphereResponseOverburden/0.1.0/SphereResponseOverburden-0.1.0.tar.gz

---
+++ packages/SphereResponseOverburden/SphereResponseOverburden-0.1.0.tar.gz/SphereResponseOverburden-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SphereResponseOverburden",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A utility to calculate and plot sphere-overburden EM response",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/yourusername/sphereresponse",
+    packages=find_packages(),
+    install_requires=[
+        'PyQt5', 'numpy', 'matplotlib', 'pandas', 'scipy'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'sphere-response=sphere_package.sphere_main:main',
+        ],
+    },
+)
