8287f433d4506c3e3d13e29dd0bc2289324c631d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:03:58 +0000

    Add package ngstents/0.1.dev411/ngstents-0.1.dev411-cp310-cp310-win_amd64.whl

---
+++ packages/ngstents/ngstents-0.1.dev411-cp310-cp310-win_amd64.whl/ngstents/utils/_drawtents.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from matplotlib import colors as mcolors
+from matplotlib import pyplot as plt
+__all__ = ['DrawPitchedTentsPlt', 'mcolors', 'plt']
+def DrawPitchedTentsPlt(self, showtentnums = False):
+    """
+    
+        Draw 1D tents using Matplotlib
+        
+    """
