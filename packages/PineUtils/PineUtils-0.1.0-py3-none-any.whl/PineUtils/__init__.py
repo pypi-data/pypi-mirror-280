5aab7db25e7fda101cc552889458c1f605805901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:16 +0000

    Add package PineUtils/0.1.0/PineUtils-0.1.0-py3-none-any.whl

---
+++ packages/PineUtils/PineUtils-0.1.0-py3-none-any.whl/PineUtils/__init__.py
@@ -0,0 +1,10 @@
+from . import Logger
+from . import Input
+from . import Files
+from . import Array2D
+
+
+AUTHOR = "Plaraje (DMAM2) plaraje@proton.me"
+MAIL = "plaraje@proton.me"
+VERSION = "0.0.3"
+
