59c413b2d9e245f5f623efff2894f9722acc9a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:03:07 +0000

    Add package pydevd-pycharm/242.18071.12/pydevd-pycharm-242.18071.12.tar.gz

---
+++ packages/pydevd-pycharm/pydevd-pycharm-242.18071.12.tar.gz/pydevd-pycharm-242.18071.12/pydevd_pycharm.py
@@ -0,0 +1,6 @@
+
+# make settrace() and stoptrace() functions available for `pydevd_pycharm`
+from pydevd import settrace, stoptrace
+
+from _pydevd_bundle.pydevd_comm import VERSION_STRING
+__version__ = VERSION_STRING
