20eef0494c66ef00dbb1043b55bf1b9710cfcbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:50 +0000

    Add package python-cmethods/2.3.0/python_cmethods-2.3.0.tar.gz

---
+++ packages/python-cmethods/python_cmethods-2.3.0.tar.gz/python_cmethods-2.3.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 Benjamin Thomas Schwertfeger
+# GitHub: https://github.com/btschwertfeger
+#
+# This file is only used by sphinx for liking the package to the documentation.
+
+from setuptools import setup
+
+setup()
