8e34656c93c09e75045271cca45ab45b185815aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 04:03:17 +0000

    Add package thzabbix/0.1.3/thzabbix-0.1.3.tar.gz

---
+++ packages/thzabbix/thzabbix-0.1.3.tar.gz/thzabbix-0.1.3/thzabbix/httptest.py
@@ -0,0 +1,94 @@
+import json
+import requests
+
+from .zabbreq import ZabbReq
+
+
+class HTTPtest(object):
+    def __init__(self, creds):
+        self.authdata = dict(jsonrpc="2.0", method="user.login", params=dict(user=creds["user"], password=creds["passwd"]), id="1")
+        self.headers = {'content-type': 'application/json-rpc'}
+        self.url = creds["url"]
+        self.authtock = requests.post(self.url, data=json.dumps(self.authdata), headers=self.headers).json()["result"]
+        self.basedata = dict(jsonrpc="2.0", auth=self.authtock, id=1)
+        self.delay = str("3m")
+        self.timeout = str("30s")
+        self.retr = 3
+        self.follow_redirects = 0
+        self.zabb_req = ZabbReq(creds)
+
+    def _addparam(self, add_params):
+        if not add_params.get("delay"):
+            add_params["delay"] = self.delay
+        for step in add_params["steps"]:
+            if not step.get("follow_redirects"):
+                step["follow_redirects"] = self.follow_redirects
+            if not step.get("timeout"):
+                step["timeout"] = self.timeout
+        return add_params
+
+    def _httptest_addlist(self, add_params):
+        add_params = self._addparam(add_params)
+        steps_dict = dict(name=add_params["name"], url=add_params.pop("url"), follow_redirects=add_params.pop("follow_redirects"), timeout=add_params.pop("timeout"), status_codes=add_params.pop("status_codes"), no=str("1"))
+        steps_list = list()
+        steps_list.append(steps_dict)
+        add_params["hostid"] = self.zabb_req.hostidbyip(add_params.pop("host_ip"))
+        add_params["steps"] = steps_list
+        return add_params
+
+    def _httptest_updlist(self, upd_params):
+        upd_params = self._addparam(upd_params)
+        full_params = dict(httptestid=upd_params.pop("httptestid"), delay=upd_params.pop("delay"))
+        steps_dict = upd_params
+        steps_dict["no"] = str("1")
+        steps_list = list()
+        steps_list.append(steps_dict)
+        full_params["steps"] = steps_list
+        full_params["retries"] = self.retr
+        return full_params
+
+    def _trigg_add(self, name, host):
+        trigg_descr = str("Web scenario " + name + " failed: {ITEM.VALUE}")
+        trigg_expr = str("length(last(/" + host + "/web.test.error[" + name + "]))>0 and last(/" + host + "/web.test.fail[" + name + "])>0")
+        paramslst = dict(description=trigg_descr, expression=trigg_expr, priority=4)
+        full_data = self.basedata.copy()
+        full_data["params"] = paramslst
+        full_data["method"] = "trigger.create"
+        return self.zabb_req.req_post(full_data)
+
+    def _httptestfull(self, params, method):
+        check_name = params["name"]
+        params = self._addparam(params)
+        result = list()
+        if method == "httptest.create":
+            host_id = self.zabb_req.hostidbyip(params["host_ip"])
+            params["hostid"] = host_id
+            del params["host_ip"]
+        if method != "httptest.create" and method != "httptest.update":
+            return False
+        test_add_data = self.basedata.copy()
+        test_add_data["params"] = params
+        test_add_data["method"] = method
+        httptestret = self.zabb_req.req_post(test_add_data)
+        result.append(httptestret)
+        if method == "httptest.create":
+            hostname = self.zabb_req.hostdata(host_id)["name"]
+            triggadd_ret = self._trigg_add(check_name, hostname)
+            result.append(triggadd_ret)
+        return result
+
+    def httptestdel(self, del_testid):
+        paramslst = dict(httptestid=del_testid)
+        full_data = self.basedata.copy()
+        full_data["params"] = paramslst
+        full_data["method"] = "httptest.delete"
+        del_data = self.zabb_req.req_post(full_data)
+        return del_data
+
+    def httptestadd(self, add_params):
+        method = "httptest.create"
+        return self._httptestfull(add_params, method)
+
+    def httptestupd(self, upd_params):
+        method = "httptest.update"
+        return self._httptestfull(upd_params, method)
---
+++ packages/thzabbix/thzabbix-0.1.3.tar.gz/thzabbix-0.1.3/thzabbix/zabbreq.py
@@ -0,0 +1,104 @@
+import json
+import requests
+
+
+class ZabbReq:
+    def __init__(self, creds):
+        self.headers = {'content-type': 'application/json-rpc'}
+        self.url = creds["url"]
+        self.authdata = dict(jsonrpc="2.0", method="user.login",
+                             params=dict(user=creds["user"], password=creds["passwd"]), id="1")
+        self.authtock = requests.post(self.url, data=json.dumps(self.authdata), headers=self.headers).json()["result"]
+        self.basedata = dict(jsonrpc="2.0", auth=self.authtock, id=1)
+
+    def req_post(self, req_data):
+        full_data = self.basedata.copy()
+        full_data.update(req_data)
+        return requests.post(self.url, data=json.dumps(full_data), headers=self.headers).json()
+
+    def hostidbyip(self, host_ip):
+        find_data = dict(filter=dict(ip=host_ip))
+        res_req = self.req_post(dict(params=find_data, method="hostinterface.get"))["result"]
+        if len(res_req) > 0:
+            host_id = res_req[0]["hostid"]
+            return host_id
+        else:
+            print("Host not found")
+            return False
+
+    def hostipbyid(self, host_id):
+        find_data = dict(filter=dict(hostid=host_id))
+        res_req = self.req_post(dict(params=find_data, method="hostinterface.get"))["result"]
+        host_ip = res_req[0]["ip"]
+        return host_ip
+
+    def lf_data(self, method_get, param, param1):
+        paramslst = dict(output="extend")
+        paramsfilter = dict()
+        paramsfilter[param] = param1
+        if method_get == "host":
+            method_rst = "host.get"
+        elif method_get == "hostgroup":
+            method_rst = "hostgroup.get"
+        elif method_get == "template":
+            method_rst = "template.get"
+        elif method_get == "hostinterface":
+            method_rst = "hostinterface.get"
+        elif method_get == "httptest":
+            paramslst = dict(output="extend", selectSteps="extend", selectTags="extend")
+            method_rst = "httptest.get"
+        else:
+            return False
+        paramslst["filter"] = paramsfilter
+        host_get = dict(method=method_rst, params=paramslst)
+        return self.req_post(host_get)["result"]
+
+    def host_tmplt_list(self, hostid):
+        search_data = dict(method="host.get", params=dict(selectParentTemplates=list("templateid"), hostids=hostid))
+        result_data = self.req_post(search_data)["result"][0]['parentTemplates']
+        templt_lst = list()
+        for i in result_data:
+            templt_lst.append(i['templateid'])
+        return templt_lst
+
+    def host_tmplt_upd(self, hostid, tmplt_lst):
+        hupdttedata = dict(hostid=hostid)
+        tmplt_lst = list(dict.fromkeys(tmplt_lst))
+        hupdttedata["templates"] = tmplt_lst
+        updtmplt_req = dict(method="host.update", params=hupdttedata)
+        return self.req_post(updtmplt_req)
+
+    def host_tmplt_add(self, hostid, tmplt_id):
+        tmplts_list = self.host_tmplt_list(hostid)
+        tmplts_list.append(tmplt_id)
+        return self.host_tmplt_upd(hostid, tmplts_list)
+
+    def host_tmplt_del(self, hostid, tmplt_id):
+        tmplts_list = self.host_tmplt_list(hostid)
+        tmplts_list.remove(tmplt_id)
+        return self.host_tmplt_upd(hostid, tmplts_list)
+
+    def addhost(self, hcreatedata):
+        addhost_get = dict(method="host.create", params=hcreatedata)
+        return self.req_post(addhost_get)
+
+    def hostslist(self):
+        paramslst = dict(output="extend")
+        hosts_get = dict(method="host.get", params=paramslst)
+        hosts_get_data = self.req_post(hosts_get)["result"]
+        return hosts_get_data
+
+    def hostsidlist(self):
+        full_data_lst = self.hostslist()
+        res_lst = list()
+        for i in full_data_lst:
+            res_lst.append(i["hostid"])
+        return res_lst
+
+    def hostdata(self, hostid):
+        paramslst = dict(output="extend")
+        paramslst["filter"] = dict(hostid=hostid)
+        full_data = self.basedata.copy()
+        full_data["params"] = paramslst
+        full_data["method"] = "host.get"
+        return self.req_post(full_data)["result"][0]
