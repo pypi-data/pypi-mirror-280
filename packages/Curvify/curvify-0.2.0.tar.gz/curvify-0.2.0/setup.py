2c9b9c83936cc2934dbd020e5aad253bfab441e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 16:03:39 +0000

    Add package Curvify/0.2.0/curvify-0.2.0.tar.gz

---
+++ packages/Curvify/curvify-0.2.0.tar.gz/curvify-0.2.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = "Curvify",                                                                                                                              
+    version = "0.2.0",
+    author = "Cheng",
+    license='MIT license',
+    author_email="cjenf844747@gmail.com",
+    packages=setuptools.find_packages(),
+    description="a simple tool to plot curves",
+    url="https://github.com/Cjenf/Curve", 
+    python_requires='>=3.11.4'
+)
