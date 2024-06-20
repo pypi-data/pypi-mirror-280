3b86bf8ff500e4f8a407f1dcb65d1ebd6b1c8f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 05:03:30 +0000

    Add package algorand-python-testing/0.1.0/algorand_python_testing-0.1.0-py3-none-any.whl

---
+++ packages/algorand-python-testing/algorand_python_testing-0.1.0-py3-none-any.whl/algopy/__init__.py
@@ -0,0 +1,42 @@
+from algopy_testing.decorators.subroutine import subroutine
+from algopy_testing.enums import OnCompleteAction, TransactionType
+from algopy_testing.models import (
+    Account,
+    Application,
+    ARC4Contract,
+    Asset,
+    Contract,
+    Global,
+    GTxn,
+    ITxn,
+    Txn,
+)
+from algopy_testing.primitives import BigUInt, Bytes, String, UInt64
+from algopy_testing.state import GlobalState, LocalState
+
+from . import arc4, gtxn, itxn, op
+
+__all__ = [
+    "ARC4Contract",
+    "Account",
+    "Application",
+    "Asset",
+    "BigUInt",
+    "Bytes",
+    "Contract",
+    "Global",
+    "GlobalState",
+    "GTxn",
+    "ITxn",
+    "LocalState",
+    "OnCompleteAction",
+    "String",
+    "TransactionType",
+    "Txn",
+    "UInt64",
+    "subroutine",
+    "gtxn",
+    "itxn",
+    "arc4",
+    "op",
+]
---
+++ packages/algorand-python-testing/algorand_python_testing-0.1.0-py3-none-any.whl/algopy_testing/models/itxn.py
@@ -0,0 +1,34 @@
+import typing
+from collections.abc import Callable
+
+
+class _ITxn:
+    def __getattr__(self, name: str) -> Callable[[], typing.Any]:
+        from algopy_testing.context import get_test_context
+
+        context = get_test_context()
+        if not context:
+            raise ValueError(
+                "Test context is not initialized! Use `with algopy_testing_context()` to access "
+                "the context manager."
+            )
+        if not context._inner_transaction_groups:
+            raise ValueError(
+                "No inner transaction found in the context! Use `with algopy_testing_context()` "
+                "to access the context manager."
+            )
+        last_itxn_group = context._inner_transaction_groups[-1]
+
+        if not last_itxn_group:
+            raise ValueError("No inner transaction found in the testing context!")
+
+        last_itxn = last_itxn_group[-1]
+
+        value = getattr(last_itxn, name)
+        if value is None:
+            raise ValueError(f"'{name}' is not defined for {type(last_itxn).__name__} ")
+        # mimic the static functions on ITxn with a lambda
+        return lambda: value
+
+
+ITxn = _ITxn()
