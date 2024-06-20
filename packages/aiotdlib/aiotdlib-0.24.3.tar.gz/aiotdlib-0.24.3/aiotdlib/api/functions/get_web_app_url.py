3b7d910649a7ac81104ad94f9e8d6bf718e3b7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 03:03:21 +0000

    Add package aiotdlib/0.24.3/aiotdlib-0.24.3.tar.gz

---
+++ packages/aiotdlib/aiotdlib-0.24.3.tar.gz/aiotdlib-0.24.3/aiotdlib/api/functions/get_web_app_url.py
@@ -0,0 +1,37 @@
+# =============================================================================== #
+#                                                                                 #
+#    This file has been generated automatically!! Do not change this manually!    #
+#                                                                                 #
+# =============================================================================== #
+from __future__ import annotations
+
+import typing
+
+from pydantic import Field
+
+from ..types.base import *
+
+from ..types.all import (
+    ThemeParameters,
+)
+
+
+class GetWebAppUrl(BaseObject):
+    """
+    Returns an HTTPS URL of a Web App to open from the side menu, a keyboardButtonTypeWebApp button, an inlineQueryResultsButtonTypeWebApp button, or an internalLinkTypeSideMenuBot link
+
+    :param bot_user_id: Identifier of the target bot
+    :type bot_user_id: :class:`Int53`
+    :param url: The URL from a keyboardButtonTypeWebApp button, inlineQueryResultsButtonTypeWebApp button, an internalLinkTypeSideMenuBot link, or an empty when the bot is opened from the side menu
+    :type url: :class:`String`
+    :param application_name: Short name of the application; 0-64 English letters, digits, and underscores
+    :type application_name: :class:`String`
+    :param theme: Preferred Web App theme; pass null to use the default theme, defaults to None
+    :type theme: :class:`ThemeParameters`, optional
+    """
+
+    ID: typing.Literal["getWebAppUrl"] = Field("getWebAppUrl", validation_alias="@type", alias="@type")
+    bot_user_id: Int53
+    url: String
+    application_name: String
+    theme: typing.Optional[ThemeParameters] = None
