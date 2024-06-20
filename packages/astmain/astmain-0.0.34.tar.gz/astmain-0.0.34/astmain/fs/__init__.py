7bffe00c700dfc4b23ebd4c072af855fa49e5362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-20 17:03:18 +0000

    Add package astmain/0.0.34/astmain-0.0.34.tar.gz

---
+++ packages/astmain/astmain-0.0.34.tar.gz/astmain-0.0.34/astmain/fs/__init__.py
@@ -0,0 +1,4 @@
+from .delete import delete
+from .path import path
+from .read import read
+from .write import write
---
+++ packages/astmain/astmain-0.0.34.tar.gz/astmain-0.0.34/astmain/fs/path.py
@@ -0,0 +1,5 @@
+import os
+def path(my_path):
+    current_dir = os.getcwd()
+    return os.path.join(current_dir,path2)
+
