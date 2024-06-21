6f73306c77de00d9db83e69b48c7fe8b041082c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 03:04:19 +0000

    Add package villog/0.0.6/villog-0.0.6.tar.gz

---
+++ packages/villog/villog-0.0.6.tar.gz/villog-0.0.6/setup.py
@@ -0,0 +1,11 @@
+'''setup'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "villog",
+    version = "0.0.6",
+    description = "A simple python library for logging",
+    author = "Krisztián Villers",
+    packages = find_packages(),
+)
