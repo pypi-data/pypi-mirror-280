3e65ad6bc289e8620c3b99b57212095b96d6fedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 03:04:23 +0000

    Add package anaplan-sdk/0.1.8/anaplan_sdk-0.1.8-py3-none-any.whl

---
+++ packages/anaplan-sdk/anaplan_sdk-0.1.8-py3-none-any.whl/anaplan_sdk/exceptions.py
@@ -0,0 +1,62 @@
+from httpx import HTTPError, HTTPStatusError
+
+
+class AnaplanException(Exception):
+    """
+    Base class for all Anaplan SDK Exceptions.
+    """
+
+    def __init__(self, message: str):
+        self.message = f"\n\n\n{message}\n"
+        super().__init__(self.message)
+
+
+class InvalidCredentialsException(AnaplanException):
+    """
+    Exception raised when the provided credentials are invalid.
+    """
+
+    def __init__(self, message: str = "Invalid credentials."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidPrivateKeyException(InvalidCredentialsException):
+    """
+    Exception raised when the provided private key is invalid.
+    """
+
+    def __init__(self, message: str = "Invalid private key."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidIdentifierException(AnaplanException):
+    """
+    Exception raised when the provided identifier is invalid.
+    """
+
+    def __init__(self, message: str = "Invalid identifier."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class AnaplanActionError(AnaplanException):
+    """
+    Exception raised when an Anaplan Action fails.
+    """
+
+    def __init__(self, message: str = "Anaplan completed with errors."):
+        self.message = message
+        super().__init__(self.message)
+
+
+def raise_error(error: HTTPError) -> None:
+    """
+    Raise an appropriate exception based on the error.
+    :param error: The error to raise an exception for.
+    """
+    if isinstance(error, HTTPStatusError):
+        if error.response.status_code == 404:
+            raise InvalidIdentifierException from error
+    raise AnaplanException(str(error))
