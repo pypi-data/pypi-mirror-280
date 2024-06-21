16e4fd9ca5eaa4dc6a64f35b31d06a5fd8398396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:32 +0000

    Add package ngstents/0.1.dev420/ngstents-0.1.dev420-cp310-cp310-win_amd64.whl

---
+++ packages/ngstents/ngstents-0.1.dev420-cp310-cp310-win_amd64.whl/ngstents/utils/__init__.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from ngstents.utils._drawtents import DrawPitchedTentsPlt
+from ngstents.utils._drawtents2d import DrawPitchedTents
+from . import _drawtents
+from . import _drawtents2d
+__all__: list = ['DrawPitchedTentsPlt']
