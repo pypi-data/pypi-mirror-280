11529001c6acfb52380092e82175549a0e511024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:04:30 +0000

    Add package CVE-2012-5321/0.0.1/CVE-2012-5321-0.0.1.tar.gz

---
+++ packages/CVE-2012-5321/CVE-2012-5321-0.0.1.tar.gz/CVE-2012-5321-0.0.1/cve20125321/includes/writefile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+ * CVE-2012-5321
+ * CVE-2012-5321 Bug scanner for WebPentesters and Bugbounty Hunters
+ *
+ * @Developed By Cappricio Securities <https://cappriciosec.com>
+ */
+
+
+"""
+
+
+def writedata(output, data):
+    with open(output, 'a') as file:
+        file.write(data)
