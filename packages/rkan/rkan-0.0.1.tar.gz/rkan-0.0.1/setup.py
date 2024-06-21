5715e8ceff51f92df85f4eb89655177d11566b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:05:09 +0000

    Add package rkan/0.0.1/rkan-0.0.1.tar.gz

---
+++ packages/rkan/rkan-0.0.1.tar.gz/rkan-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='rkan',
+    version='0.0.1',
+    packages=["rkan"],
+    description="Rational Kolmogorov-Arnold Network (rKAN)",
+    author='Alireza Afzal Aghaei',
+    author_email='alirezaafzalaghaei@gmail.com',
+    url='https://github.com/alirezaafzalaghaei/rkan',
+    python_needed='>=3.9',
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: BSD License",
+    ],
+    license='BSD',
+)
