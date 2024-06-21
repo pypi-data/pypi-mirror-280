3d8fd36cb9e6429881a74e29a01cd240e701ae76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 15:09:43 +0000

    Add package sparrow-parse/0.2.7/sparrow_parse-0.2.7.tar.gz

---
+++ packages/sparrow-parse/sparrow_parse-0.2.7.tar.gz/sparrow_parse-0.2.7/sparrow_parse/temp.py
@@ -0,0 +1,16 @@
+# content, table_content = processor.extract_data(
+    #     '/Users/andrejb/Documents/work/schreiber/invoice_data/test/2618407.pdf',
+    #     'hi_res',
+    #     'yolox',
+    #     # 'detectron2_onnx',
+    #     ['tables', 'unstructured'],
+    #     True,
+    #     True)
+
+    # content, table_content = processor.extract_data(
+    #     '/Users/andrejb/Documents/work/epik/bankstatement/OCBC_1_1.pdf',
+    #     'hi_res',
+    #     'yolox',
+    #     ['tables', 'unstructured'],
+    #     True,
+    #     True)
