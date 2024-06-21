cecf25e74ec83aaabf2bd340197a12f693764673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 07:04:05 +0000

    Add package meerschaum/2.2.2rc3/meerschaum-2.2.2rc3.tar.gz

---
+++ packages/meerschaum/meerschaum-2.2.2rc3.tar.gz/meerschaum-2.2.2rc3/meerschaum/api/_oauth2.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Define JWT authorization here.
+"""
+
+import os
+from meerschaum.api import app, endpoints, CHECK_UPDATE
+from meerschaum.utils.packages import attempt_import
+fastapi = attempt_import('fastapi', lazy=False, check_update=CHECK_UPDATE)
+fastapi_responses = attempt_import('fastapi.responses', lazy=False, check_update=CHECK_UPDATE)
+fastapi_login = attempt_import('fastapi_login', check_update=CHECK_UPDATE)
+
+LoginManager = fastapi_login.LoginManager
+def generate_secret_key() -> str:
+    """
+    Read or generate the secret keyfile.
+    """
+    from meerschaum.config._paths import API_SECRET_KEY_PATH
+    if not API_SECRET_KEY_PATH.exists():
+        secret_key = os.urandom(24).hex()
+        with open(API_SECRET_KEY_PATH, 'w+') as f:
+            f.write(secret_key)
+    else:
+        with open(API_SECRET_KEY_PATH, 'r') as f:
+            secret_key = f.read()
+
+    return secret_key.encode('utf-8')
+
+SECRET = generate_secret_key()
+manager = LoginManager(SECRET, token_url=endpoints['login'])
