1b7fdec6cc4443c5e30a90534481a93b2c10b34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 06:04:33 +0000

    Add package metaflow-stubs/2.12.5/metaflow_stubs-2.12.5.tar.gz

---
+++ packages/metaflow-stubs/metaflow_stubs-2.12.5.tar.gz/metaflow_stubs-2.12.5/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,14 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.5                                                             #
+# Generated on 2024-06-20T19:51:33.994875                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
