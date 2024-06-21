0ba3e57b10a003020f7811e429ea525d784dc6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 06:05:05 +0000

    Add package lollmsvectordb/0.5.0/lollmsvectordb-0.5.0.tar.gz

---
+++ packages/lollmsvectordb/lollmsvectordb-0.5.0.tar.gz/lollmsvectordb-0.5.0/lollmsvectordb/__init__.py
@@ -0,0 +1,5 @@
+
+from lollmsvectordb.vector_database import VectorDatabase
+from lollmsvectordb.vectorizer import Vectorizer
+from lollmsvectordb.lollms_vectorizers.tfidf_vectorizer import TFIDFVectorizer
+#from lollmsvectordb.vectorizers.word2vec_vectorizer import Word2VecVectorizer
