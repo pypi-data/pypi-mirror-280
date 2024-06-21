4fe7c8a2d80dc5284f84650b48d6a685a7564bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 03:04:38 +0000

    Add package django-tabbed-admin-u/1.0.4.2/django_tabbed_admin_u-1.0.4.2.tar.gz

---
+++ packages/django-tabbed-admin-u/django_tabbed_admin_u-1.0.4.2.tar.gz/django_tabbed_admin_u-1.0.4.2/setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+import sys
+
+# python2 and python3 support
+try:
+    reload
+except NameError:
+    # py3k has unicode by default
+    pass
+else:
+    reload(sys).setdefaultencoding('utf-8')
+
+
+setup(
+    name='django-tabbed-admin-u',
+    version='1.0.4.2',
+    author='intp1',
+    author_email='intp1@protonmail.com',
+    description="Patched with 'ugettext/gettext' support for django-tabbed-admin",
+    long_description=open('README.rst').read(),
+    url='http://www.revsquare.com',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages("./tabbed_admin_u", include=['*.py','*.txt','static.*','templates.*','templatetags.*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
