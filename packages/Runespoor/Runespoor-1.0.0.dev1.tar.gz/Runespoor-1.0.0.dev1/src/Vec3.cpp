b9ea06cc217af8b9c1273d12228ece9720c14f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:05:15 +0000

    Add package Runespoor/1.0.0.dev1/Runespoor-1.0.0.dev1.tar.gz

---
+++ packages/Runespoor/Runespoor-1.0.0.dev1.tar.gz/Runespoor-1.0.0.dev1/src/Vec3.cpp
@@ -0,0 +1,18 @@
+#include "Vec3.h"
+#include "Camera.h"
+#include "Tup.h"
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+
+quaternion vec3::to_quaternion() {
+    return quaternion(axis);
+}
+
+vec3 vec3::cross(quaternion const& other) {
+    return vec3(glm::cross(other.quat, axis));
+}
+
+vec3 vec3::operator*(quaternion const& other) {
+    return vec3(axis * other.quat);
+}
