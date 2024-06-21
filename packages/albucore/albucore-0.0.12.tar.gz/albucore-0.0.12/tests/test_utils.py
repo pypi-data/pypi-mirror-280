3f5e02a57c1ff72431a839f48484fa85bc45a81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 14:04:05 +0000

    Add package albucore/0.0.12/albucore-0.0.12.tar.gz

---
+++ packages/albucore/albucore-0.0.12.tar.gz/albucore-0.0.12/tests/test_utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pytest
+import cv2
+from albucore.utils import MAX_VALUES_BY_DTYPE, NPDTYPE_TO_OPENCV_DTYPE, clip, convert_value, get_opencv_dtype_from_numpy
+import albucore
+
+
+@pytest.mark.parametrize("input_img, dtype, expected", [
+    (np.array([[-300, 0], [100, 400]], dtype=np.float32), np.uint8, np.array([[0, 0], [100, 255]], dtype=np.float32)),
+    (np.array([[-0.02, 0], [0.5, 2.2]], dtype=np.float32), np.float32, np.array([[0, 0], [0.5, 1.0]], dtype=np.float32))
+])
+def test_clip(input_img, dtype, expected):
+    clipped = clip(input_img, dtype=dtype)
+    assert np.array_equal(clipped, expected)
+
+valid_cv2_types = {
+    cv2.CV_8U, cv2.CV_16U, cv2.CV_32F, cv2.CV_64F, cv2.CV_32S
+}
+
+@pytest.mark.parametrize("cv_type", NPDTYPE_TO_OPENCV_DTYPE.values())
+def test_valid_cv2_types(cv_type):
+    assert cv_type in valid_cv2_types, f"{cv_type} is not a valid cv2 type"
+
+
+def test_cv_dtype_from_np():
+    assert get_opencv_dtype_from_numpy(np.uint8) == cv2.CV_8U
+    assert get_opencv_dtype_from_numpy(np.uint16) == cv2.CV_16U
+    assert get_opencv_dtype_from_numpy(np.float32) == cv2.CV_32F
+    assert get_opencv_dtype_from_numpy(np.float64) == cv2.CV_64F
+    assert get_opencv_dtype_from_numpy(np.int32) == cv2.CV_32S
+
+    assert get_opencv_dtype_from_numpy(np.dtype("uint8")) == cv2.CV_8U
+    assert get_opencv_dtype_from_numpy(np.dtype("uint16")) == cv2.CV_16U
+    assert get_opencv_dtype_from_numpy(np.dtype("float32")) == cv2.CV_32F
+    assert get_opencv_dtype_from_numpy(np.dtype("float64")) == cv2.CV_64F
+    assert get_opencv_dtype_from_numpy(np.dtype("int32")) == cv2.CV_32S
+
+
+@pytest.mark.parametrize(
+    "value, num_channels, expected",
+    [
+        ((1.5), 1, 1.5),
+        (np.array([1.5]), 3, 1.5),
+        (np.array([1.5, 2.5]), 1, 1.5),
+        (np.array([1.5, 2.5, 0.5]), 2, np.array([1.5, 2.5,], dtype=np.float32)),
+        (3, 2, 3),
+        (np.array((1.5)), 2, 1.5),
+        (np.reciprocal(np.array(0.2, dtype=np.float64)), 2, 5),
+    ]
+)
+def test_convert_value(value, num_channels, expected):
+    result = convert_value(value, num_channels)
+    if isinstance(expected, np.ndarray):
+        assert np.array_equal(result, expected)
+    else:
+         assert result == expected
