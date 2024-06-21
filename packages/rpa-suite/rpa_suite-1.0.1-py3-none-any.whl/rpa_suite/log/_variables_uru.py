36971d4186b467946247973e80f807d0eada3c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:52 +0000

    Add package rpa-suite/1.0.1/rpa_suite-1.0.1-py3-none-any.whl

---
+++ packages/rpa-suite/rpa_suite-1.0.1-py3-none-any.whl/rpa_suite/log/_variables_uru.py
@@ -0,0 +1,9 @@
+from loguru import logger
+from typing import Optional
+from typing import Any
+
+# Variável global para o manipulador de arquivo
+file_handler: Optional[str] = None
+
+# Variável global para o manipulador de stream stdout/stdin/buffer
+stream_handler: Optional[Any] = logger
