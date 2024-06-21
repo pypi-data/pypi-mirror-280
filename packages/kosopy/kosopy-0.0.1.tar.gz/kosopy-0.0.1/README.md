97f6ca44e5b9c80a3961a39202bd47edf6da0011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 15:10:13 +0000

    Add package kosopy/0.0.1/kosopy-0.0.1.tar.gz

---
+++ packages/kosopy/kosopy-0.0.1.tar.gz/kosopy-0.0.1/README.md
@@ -0,0 +1,17 @@
+# kosopy
+
+
+[![image](https://img.shields.io/pypi/v/kosopy.svg)](https://pypi.python.org/pypi/kosopy)
+[![image](https://img.shields.io/conda/vn/conda-forge/kosopy.svg)](https://anaconda.org/conda-forge/kosopy)
+
+
+**A python pacakge to handle KoSO data.**
+
+
+-   Free software: MIT License
+-   Documentation: https://bibhuraushan.github.io/kosopy
+    
+
+## Features
+
+-   TODO
---
+++ packages/kosopy/kosopy-0.0.1.tar.gz/kosopy-0.0.1/tests/test_kosopy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `kosopy` package."""
+
+
+import unittest
+
+from kosopy import kosopy
+
+
+class TestKosopy(unittest.TestCase):
+    """Tests for `kosopy` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
