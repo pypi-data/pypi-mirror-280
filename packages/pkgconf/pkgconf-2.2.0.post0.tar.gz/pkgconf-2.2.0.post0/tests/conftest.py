6bf22ba8bc58613190c83abdff66f660651a23c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 14:04:04 +0000

    Add package pkgconf/2.2.0.post0/pkgconf-2.2.0.post0.tar.gz

---
+++ packages/pkgconf/pkgconf-2.2.0.post0.tar.gz/pkgconf-2.2.0.post0/tests/conftest.py
@@ -0,0 +1,45 @@
+import pathlib
+import sys
+
+import environment_helpers
+import environment_helpers.build
+import pytest
+
+
+ROOT = pathlib.Path(__file__).parent.parent
+
+sys.path.insert(0, str(ROOT / 'src'))
+
+
+@pytest.fixture
+def root():
+    return ROOT
+
+
+@pytest.fixture
+def packages():
+    return ROOT / 'tests' / 'packages'
+
+
+@pytest.fixture
+def data():
+    return ROOT / 'tests' / 'data'
+
+
+@pytest.fixture
+def examples():
+    return ROOT / 'examples'
+
+
+@pytest.fixture(scope='session')
+def self_wheel(tmp_path_factory):
+    tmpdir = tmp_path_factory.mktemp('wheel')
+    return environment_helpers.build.build_wheel(ROOT, tmpdir)
+
+
+@pytest.fixture
+def env(tmpdir, self_wheel):
+    """Make a virtual environment with our project installed."""
+    env = environment_helpers.create_venv(tmpdir, system_site_packages=True)
+    env.install_wheel(self_wheel)
+    return env
---
+++ packages/pkgconf/pkgconf-2.2.0.post0.tar.gz/pkgconf-2.2.0.post0/tests/test_entrypoints.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import subprocess
+import sys
+
+import pytest
+
+import pkgconf
+import pkgconf.__main__
+
+
+@pytest.mark.parametrize('name', ['pkgconf', 'pkg-config'])
+def test_entries(env, name):
+    # TODO: Check PKG_CONFIG_PATH is correctly set
+    path = shutil.which(name, path=env.scripts)
+    help_text = subprocess.check_output([os.fspath(path), '--help'])
+    assert help_text.decode().startswith('usage: pkgconf')
+
+
+def test_fallback(mocker, monkeypatch):
+    """Test that we fallback to the system pkgconf if ours fails."""
+    mocker.patch('pkgconf.run_pkgconf', side_effect=subprocess.CalledProcessError(1, '(cmd)'))
+    mocker.patch('subprocess.run')
+
+    mocker.patch('shutil.which', return_value='(pkgconf-path)')
+
+    args = ['--libs', 'py-test-inexistent']
+    monkeypatch.setattr(sys, 'argv', ['(argv0)', *args])
+
+    pkgconf.__main__._entrypoint()
+
+    subprocess.run.assert_called_with(['(pkgconf-path)', *args])
+
+
+def test_main(env):
+    output = env.run_interpreter('-m', 'pkgconf', '--help')
+    assert output.decode().startswith('usage: pkgconf')
