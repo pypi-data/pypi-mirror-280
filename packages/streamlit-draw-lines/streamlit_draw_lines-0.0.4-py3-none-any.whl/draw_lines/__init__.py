6d4e94a33984e76b5b5de7da0625159291b5a538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:29 +0000

    Add package streamlit-draw-lines/0.0.4/streamlit_draw_lines-0.0.4-py3-none-any.whl

---
+++ packages/streamlit-draw-lines/streamlit_draw_lines-0.0.4-py3-none-any.whl/draw_lines/__init__.py
@@ -0,0 +1,18 @@
+import streamlit.components.v1 as components
+import os
+
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+frontend_dir = os.path.join(parent_dir, "frontend", "build")
+
+draw_lines_func = components.declare_component(
+        "draw_lines",
+        path=frontend_dir,
+        #url=frontend_dir
+        #url="http://localhost:3001",
+    )
+
+# Create a wrapper function that calls the custom component
+def draw_lines(image, width, height, lines=[], key=None):
+    component_value = draw_lines_func(image=image, width=width, height=height, lines=lines, key=key)
+    return component_value
