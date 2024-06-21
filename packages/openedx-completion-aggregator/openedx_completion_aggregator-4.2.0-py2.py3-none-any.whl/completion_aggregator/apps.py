42d1c1eb512ba55de6d0e4604e6e3e957eeb75cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 14:04:04 +0000

    Add package openedx-completion-aggregator/4.2.0/openedx_completion_aggregator-4.2.0-py2.py3-none-any.whl

---
+++ packages/openedx-completion-aggregator/openedx_completion_aggregator-4.2.0-py2.py3-none-any.whl/completion_aggregator/apps.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+completion_aggregator Django application initialization.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class CompletionAggregatorAppConfig(AppConfig):
+    """
+    Configuration for the completion_aggregator Django application.
+    """
+
+    name = 'completion_aggregator'
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': 'completion_aggregator',
+                'regex': r'^completion-aggregator/',
+                'relative_path': 'urls',
+            },
+        },
+        'settings_config': {
+            'lms.djangoapp': {
+                'aws': {'relative_path': 'settings.aws'},
+                'common': {'relative_path': 'settings.common'},
+            },
+            'cms.djangoapp': {
+                'aws': {'relative_path': 'settings.aws'},
+                'common': {'relative_path': 'settings.common'},
+            },
+        },
+    }
+
+    def ready(self):
+        """
+        Load signal handlers when the app is ready.
+        """
+        # pylint: disable=import-outside-toplevel
+        from . import signals
+        signals.register()
+
+        # pylint: disable=unused-import
+        from . import xapi
+        from .tasks import aggregation_tasks, handler_tasks
