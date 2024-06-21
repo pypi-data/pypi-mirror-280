5397d01a9e6c67135ed7fe50092f3611066a13ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:47 +0000

    Add package tpp7/2024.6.19/tpp7-2024.6.19.tar.gz

---
+++ packages/tpp7/tpp7-2024.6.19.tar.gz/tpp7-2024.6.19/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="tpp7", # Replace with your own username
+	version="2024.06.19",
+	author="Olivier Cardoso",
+	author_email="Olivier.Cardoso@univ-paris-diderot.fr",
+	description="Un paquetage pour les TP de Physique d'Université Paris-Cité",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/cardosoo/tp",
+	package_dir={'': 'src'},
+	include_package_data=True,
+	packages=setuptools.find_packages(where='src'),
+	scripts=['tests/minimalTPUsage.py', 'tests/minimalTPUsage.ipynb'],
+	#scripts=['tests/*'],
+	package_data = {"cmpl": ["*.rules", "*.png", "*.desktop"]},
+	# data_files=[('cmpl', ['*'])],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	install_requires=[
+		'numpy', 'scipy', 'matplotlib', 'python-usbtmc', 'pandas', 'pyusb', 'fitutils'
+	],
+	python_requires='>=3.6',
+)
