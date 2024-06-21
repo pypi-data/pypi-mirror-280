7d27b89a24c07497e6a0bc99946f08c5469683d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 07:04:05 +0000

    Add package czv/0.0.0/czv-0.0.0-cp310-cp310-manylinux_2_34_x86_64.whl

---
+++ packages/czv/czv-0.0.0-cp310-cp310-manylinux_2_34_x86_64.whl/czv/__init__.pyi
@@ -0,0 +1,56 @@
+"""
+# czv
+
+Python library for [czv](https://github.com/rzmk/czv). CSV operations library for data engineering/analysis tasks.
+
+## Install
+
+```bash
+pip install czv
+```
+
+## Example
+
+```python
+import czv
+
+data: str = \"""fruits,price
+apple,2.50
+banana,3.00
+strawberry,1.50\"""
+
+output: int = czv.row_count(file_data=data, include_header_row=True)
+
+print(output) # 4
+```
+
+## Links
+
+- czv GitHub repository: <https://github.com/rzmk/czv>
+- Rust: [crates.io/crates/czv](https://crates.io/crates/czv) ([source code](https://github.com/rzmk/czv/tree/main/czv))
+- WebAssembly (JavaScript/TypeScript): [npmjs.com/package/czv](https://www.npmjs.com/package/czv) ([source code](https://github.com/rzmk/czv/tree/main/czv-wasm))
+- Python: [pypi.org/project/czv](https://pypi.org/project/czv/) ([source code](https://github.com/rzmk/czv/tree/main/czv-python))
+
+"""
+
+from typing import Optional
+from pathlib import Path
+
+def row_count(file_path: Optional[Path], file_data: Optional[str], include_header_row: Optional[bool]) -> int:
+    """Returns a count of the total number of rows.
+    
+    ## Arguments
+     
+    * `file_path` - CSV file path.
+    * `file_data` - CSV file data.
+    * `include_header_row` - Specify whether to include the header row (first row) in the row count. Default is false.
+    """
+
+def column_count(file_path: Optional[Path], file_data: Optional[str]) -> int:
+    """Returns a count of the total number of columns (fields).
+    
+    ## Arguments
+    
+    * `file_path` - CSV file path.
+    * `file_data` - CSV file data.
+    """
