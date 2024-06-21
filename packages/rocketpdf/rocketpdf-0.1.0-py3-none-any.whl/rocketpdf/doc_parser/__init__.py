78fc8877d3195aacb706231bf349f10f7c4d8423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 15:10:30 +0000

    Add package rocketpdf/0.1.0/rocketpdf-0.1.0-py3-none-any.whl

---
+++ packages/rocketpdf/rocketpdf-0.1.0-py3-none-any.whl/rocketpdf/doc_parser/__init__.py
@@ -0,0 +1,21 @@
+from .doc_parser import (
+    to_PDF,
+    convert_docx_pdf,
+    convert_pdf_docx,
+    merge_pdfs,
+    extract_pages,
+    compress_pdf,
+    parse_dir,
+    merge_dir,
+)
+
+__all__ = [
+    "to_PDF",
+    "convert_docx_pdf",
+    "convert_pdf_docx",
+    "merge_pdfs",
+    "extract_pages",
+    "compress_pdf",
+    "parse_dir",
+    "merge_dir",
+]
