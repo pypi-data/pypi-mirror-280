0d7e56670b94a09ee5f38462da60e92e0e0e0dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:05:00 +0000

    Add package seg_tgce/0.1.5.dev1/seg_tgce-0.1.5.dev1-py3-none-any.whl

---
+++ packages/seg_tgce/seg_tgce-0.1.5.dev1-py3-none-any.whl/seg_tgce/data/crowd_seg/__main__.py
@@ -0,0 +1,16 @@
+from seg_tgce.data.crowd_seg import get_all_data
+
+
+def main() -> None:
+    train, val, test = get_all_data(batch_size=8, with_sparse_data=True)
+    val.visualize_sample(batch_index=138, sample_indexes=[2, 3, 4, 5])
+    print(f"Train: {len(train)} batches, {len(train) * train.batch_size} samples")
+    print(f"Val: {len(val)} batches, {len(val) * val.batch_size} samples")
+    print(f"Test: {len(test)} batches, {len(test) * test.batch_size} samples")
+
+    for i in range(1):
+        img, mask = train[i]
+        print(f"Batch {i}: {img.shape}, {mask.shape}")
+
+
+main()
