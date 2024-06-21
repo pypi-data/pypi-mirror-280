207553a6319876f5e8f42151f2648c4b61a30622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:05:02 +0000

    Add package youqu-button-center/2024.6.21/youqu_button_center-2024.6.21.tar.gz

---
+++ packages/youqu-button-center/youqu_button_center-2024.6.21.tar.gz/youqu_button_center-2024.6.21/README.md
@@ -0,0 +1,2 @@
+# youqu-button-center
+相对位移定位插件
---
+++ packages/youqu-button-center/youqu_button_center-2024.6.21.tar.gz/youqu_button_center-2024.6.21/youqu_button_center/conf.py
@@ -0,0 +1,31 @@
+import enum
+import os
+import platform
+
+
+@enum.unique
+class DisplayServer(enum.Enum):
+    wayland = "wayland"
+    x11 = "x11"
+
+
+class Config:
+    ARCH = platform.machine()
+
+    PASSWORD = "1"
+
+    if os.path.exists(os.path.expanduser("~/.xsession-errors")):
+        DISPLAY_SERVER = (
+            os.popen("cat ~/.xsession-errors | grep XDG_SESSION_TYPE | head -n 1")
+            .read()
+            .split("=")[-1]
+            .strip("\n")
+        )
+    else:
+        DISPLAY_SERVER = "x11" if os.popen("ps -ef | grep -v grep | grep kwin_x11").read() else "wayland"
+
+    IS_X11 = DISPLAY_SERVER == DisplayServer.x11.value
+    IS_WAYLAND = DISPLAY_SERVER == DisplayServer.wayland.value
+
+
+conf = Config()
