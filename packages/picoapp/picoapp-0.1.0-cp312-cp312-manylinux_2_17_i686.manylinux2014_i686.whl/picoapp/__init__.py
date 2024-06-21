23271b2b04bd0485e81119ceea3fafccbbb8d577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 04:05:50 +0000

    Add package picoapp/0.1.0/picoapp-0.1.0-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/picoapp/picoapp-0.1.0-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl/picoapp/__init__.py
@@ -0,0 +1,8 @@
+from .core import run as run
+from .types import Input as Input
+from .types import Inputs as Inputs
+from .types import IntSlider as IntSlider
+from .types import Output as Output
+from .types import Outputs as Outputs
+from .types import Plot as Plot
+from .types import Slider as Slider
