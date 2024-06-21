bb2e0ce88de3fad030009e67d37e82dc3da8313f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 03:04:28 +0000

    Add package BlackJackTable/0.2.7/BlackJackTable-0.2.7.tar.gz

---
+++ packages/BlackJackTable/BlackJackTable-0.2.7.tar.gz/BlackJackTable-0.2.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open ("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name = "BlackJackTable",
+    version="0.2.7",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="zedMar65",
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
