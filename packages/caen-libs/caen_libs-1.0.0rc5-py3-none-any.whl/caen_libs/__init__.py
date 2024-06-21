01efa8bf8e5278146e4679185af35c9604fbe445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 03:04:42 +0000

    Add package caen-libs/1.0.0rc5/caen_libs-1.0.0rc5-py3-none-any.whl

---
+++ packages/caen-libs/caen_libs-1.0.0rc5-py3-none-any.whl/caen_libs/__init__.py
@@ -0,0 +1,12 @@
+"""
+@defgroup Python Python
+@brief Python binding
+
+@pre This module requires CAEN libraries to be installed in the system.
+"""
+
+__author__ = 'Giovanni Cerretani'
+__copyright__ = 'Copyright (C) 2024 CAEN SpA'
+__license__ = 'LGPL-3.0-or-later'  # SPDX-License-Identifier
+__version__ = '1.0.0-rc5'
+__contact__ = 'https://www.caen.it/'
