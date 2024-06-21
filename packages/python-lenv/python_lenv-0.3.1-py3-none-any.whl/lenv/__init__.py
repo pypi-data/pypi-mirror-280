3d22267d3b0390225050f689930b08771608832e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 03:05:32 +0000

    Add package python-lenv/0.3.1/python_lenv-0.3.1-py3-none-any.whl

---
+++ packages/python-lenv/python_lenv-0.3.1-py3-none-any.whl/lenv/__init__.py
@@ -0,0 +1,16 @@
+# Copyright © 2024 pkeorley
+#
+# This source code is licensed under the MIT license found in the LICENSE
+# file in the root directory of this source tree.
+
+from lenv.meta import MetaEnvironmentVariablesLoader, Metadata
+
+
+__version__ = "0.3.1"
+__description__ = "Quickly and efficiently load environment variables into a class"
+__author__ = "pkeorley"
+
+__all__ = [
+    "MetaEnvironmentVariablesLoader",
+    "Metadata",
+]
