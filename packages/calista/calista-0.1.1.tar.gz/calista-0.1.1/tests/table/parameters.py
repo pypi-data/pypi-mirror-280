a6989a6a337705d6408d8908b354b51ddbe9a398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 04:03:57 +0000

    Add package calista/0.1.1/calista-0.1.1.tar.gz

---
+++ packages/calista/calista-0.1.1.tar.gz/calista-0.1.1/tests/table/parameters.py
@@ -0,0 +1,12 @@
+SNOWFLAKE_CONN_PARAMS = {
+            "credentials": {
+            "account": "nj69313.eu-west-3.aws",
+            "user": "mioras",
+            "password": "Aubay2024@92",
+            }
+        }
+
+BIGQUERY_CONN_PARAMS = {
+        "connection_string": "bigquery://test-calista/RH",
+        "credentials_path": "bigquery_private_key.json",
+}
