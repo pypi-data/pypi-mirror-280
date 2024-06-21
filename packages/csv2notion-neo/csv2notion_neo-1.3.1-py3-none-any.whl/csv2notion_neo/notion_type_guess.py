c127e9753267027393790b8f63dc4b77bae4e5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 14:03:57 +0000

    Add package csv2notion-neo/1.3.1/csv2notion_neo-1.3.1-py3-none-any.whl

---
+++ packages/csv2notion-neo/csv2notion_neo-1.3.1-py3-none-any.whl/csv2notion_neo/notion_type_guess.py
@@ -0,0 +1,51 @@
+import math
+import re
+from typing import List
+from icecream import ic
+
+def guess_type_by_values(values_str: List[str]) -> str:
+
+    if type(values_str[0]) == list:
+        unique_values = set()
+        for value in values_str:
+            unique_values.update(value)
+    else:   
+        unique_values = set(filter(None, values_str))
+
+    match_map = {
+        "text": is_empty,
+        "checkbox": is_checkbox,
+        "number": is_number,
+        "url": is_url,
+        "email": is_email,
+    }
+
+    matches = (
+        value_type
+        for value_type, match_func in match_map.items()
+        if all(map(match_func, unique_values))
+    )
+    return next(matches, "text")
+
+
+def is_number(s: str) -> bool:
+    try:
+        return not math.isnan(float(s))
+    except ValueError:
+        return False
+
+
+def is_url(s: str) -> bool:
+    return re.match("^https?://", s) is not None
+
+
+def is_email(s: str) -> bool:
+    return re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", s) is not None
+
+
+def is_checkbox(s: str) -> bool:
+    return s in {"true", "false"}
+
+
+def is_empty(s: str) -> bool:
+    return not s.strip()
