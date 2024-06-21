fce45bae9ca00adf585fddd8122b04a4cd008f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 04:04:09 +0000

    Add package tgclients/0.17.0/tgclients-0.17.0-py3-none-any.whl

---
+++ packages/tgclients/tgclients-0.17.0-py3-none-any.whl/tgclients/databinding/tgpublish/__init__.py
@@ -0,0 +1,29 @@
+from tgclients.databinding.tgpublish.tgpub import (
+    ErrorType,
+    Module,
+    ProcessStatusType,
+    PublishError,
+    PublishObject,
+    PublishResponse,
+    PublishStatus,
+    PublishWarning,
+    ReferencedUris,
+    StatusType,
+    WarningType,
+    WorldReadableMimetypes,
+)
+
+__all__ = [
+    "ErrorType",
+    "Module",
+    "ProcessStatusType",
+    "PublishError",
+    "PublishObject",
+    "PublishResponse",
+    "PublishStatus",
+    "PublishWarning",
+    "ReferencedUris",
+    "StatusType",
+    "WarningType",
+    "WorldReadableMimetypes",
+]
