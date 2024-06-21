a4ef969b5386037564a2783933fae5baa9f3b213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:04:54 +0000

    Add package pynguin/0.38.0/pynguin-0.38.0.tar.gz

---
+++ packages/pynguin/pynguin-0.38.0.tar.gz/pynguin-0.38.0/src/pynguin/__version__.py
@@ -0,0 +1,8 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2024 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
+"""Specifies the version of Pynguin."""
+__version__ = "0.38.0"
