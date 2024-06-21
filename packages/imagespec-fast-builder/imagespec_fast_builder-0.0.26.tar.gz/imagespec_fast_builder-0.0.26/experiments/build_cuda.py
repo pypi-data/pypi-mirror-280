5961d6898e0248f5fb91b62d0443509f72d0f0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 06:05:03 +0000

    Add package imagespec-fast-builder/0.0.26/imagespec_fast_builder-0.0.26.tar.gz

---
+++ packages/imagespec-fast-builder/imagespec_fast_builder-0.0.26.tar.gz/imagespec_fast_builder-0.0.26/experiments/build_cuda.py
@@ -0,0 +1,17 @@
+from flytekit import ImageSpec, Resources, task
+
+# image = ImageSpec(
+#     name="pytorch",
+#     packages=["torch==2.3.1"],
+#     builder="fast-builder",
+#     registry="ghcr.io/thomasjpfan",
+# )
+# This is the image from the above image spec
+image = "ghcr.io/thomasjpfan/pytorch:fpKDvHgYK_C3ukST8iGGAQ"
+
+
+@task(container_image=image, requests=Resources(gpu="1"))
+def check_torch() -> bool:
+    import torch
+
+    return torch.cuda.is_available()
