846b59c0c37a643f595fd711acef68e7d3ed2c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 14:04:14 +0000

    Add package cibercca/0.2.4/cibercca-0.2.4.tar.gz

---
+++ packages/cibercca/cibercca-0.2.4.tar.gz/cibercca-0.2.4/cibercca/c_alive_command.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+try:
+    from .c_typer import app, typer
+except Exception:
+    from c_typer import app, typer  # noqa
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    from .c_alive import Alive
+except Exception:
+    from c_alive import Alive
+
+
+@app.command()
+def alive(
+    path: Optional[Path] = typer.Option(
+        ...,
+        help="The path to inventory",
+    ),
+    group: Optional[str] = typer.Option(
+        ...,
+        help="The groups to filter inventory"
+    ),
+    workers: Optional[int] = typer.Option(
+        2,
+        help="The parallel execution"
+    ),
+    output: Optional[str] = typer.Option(
+        "json",
+        help="The type to print report",
+    ),
+):
+    """
+    Alive for all device filter with groups
+    """
+
+    if path is None:
+        path_inventory = BASE_DIR + "/inventory/"
+    else:
+        path_inventory = path
+
+    alive = Alive(path=path_inventory, filter=group, workers=workers)
+    alive.run()
+
+    if output == 'json':
+        print(alive.data_json())
+
+    if output == 'table':
+        print(alive.data_table())
+
+    if output == 'database':
+        print(alive.data_database())
