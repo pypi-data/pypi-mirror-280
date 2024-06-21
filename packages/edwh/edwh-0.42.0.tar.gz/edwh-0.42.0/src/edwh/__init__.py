d1e9ef22d1e4f225cc432e892e6f1d0879dde595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 04:03:43 +0000

    Add package edwh/0.42.0/edwh-0.42.0.tar.gz

---
+++ packages/edwh/edwh-0.42.0.tar.gz/edwh-0.42.0/src/edwh/__init__.py
@@ -0,0 +1,20 @@
+"""
+This file exposes some functions so this tool can be used as a library.
+"""
+
+# SPDX-FileCopyrightText: 2023-present Remco Boerma <remco.b@educationwarehouse.nl>
+#
+# SPDX-License-Identifier: MIT
+
+from . import tasks  # noqa F401: this exposes it as a library
+from .helpers import *  # noqa F401: this exposes it as a library
+from .improved_invoke import ImprovedTask, improved_task  # noqa F401
+from .tasks import (  # noqa F401: this exposes it as a library
+    TomlConfig,
+    check_env,
+    get_env_value,
+    get_task,
+    read_dotenv,
+    set_env_value,
+    task_for_namespace,
+)
