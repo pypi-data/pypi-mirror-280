927bb170276330cf04d966b492547da26b125d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:04:45 +0000

    Add package larq-compute-engine/0.16.0/larq_compute_engine-0.16.0-cp312-cp312-macosx_10_15_x86_64.whl

---
+++ packages/larq-compute-engine/larq_compute_engine-0.16.0-cp312-cp312-macosx_10_15_x86_64.whl/larq_compute_engine/__init__.py
@@ -0,0 +1,18 @@
+from larq_compute_engine.mlir.python.converter import (
+    convert_keras_model,
+    convert_saved_model,
+)
+from larq_compute_engine.tflite.python import interpreter as testing
+
+try:
+    from importlib import metadata  # type: ignore
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
+
+try:
+    __version__ = metadata.version("larq_compute_engine")
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["convert_keras_model", "convert_saved_model", "testing"]
