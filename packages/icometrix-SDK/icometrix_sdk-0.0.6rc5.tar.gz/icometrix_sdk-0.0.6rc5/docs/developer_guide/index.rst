fbb0e387b3991b34de7158e8f59cb5dbd31bd192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:04:45 +0000

    Add package icometrix-SDK/0.0.6rc5/icometrix_sdk-0.0.6rc5.tar.gz

---
+++ packages/icometrix-SDK/icometrix_sdk-0.0.6rc5.tar.gz/icometrix_sdk-0.0.6rc5/docs/developer_guide/index.rst
@@ -0,0 +1,11 @@
+Developer Guide
+===============
+
+.. toctree::
+
+   paginators
+   session
+   models
+   upload
+   anonymization
+   data_validation
---
+++ packages/icometrix-SDK/icometrix_sdk-0.0.6rc5.tar.gz/icometrix_sdk-0.0.6rc5/icometrix_sdk/models/customer_report_entity.py
@@ -0,0 +1,34 @@
+from typing import Optional, Union, List
+
+from pydantic import BaseModel
+
+from icometrix_sdk.models.base import BackendEntity
+
+
+class CustomerReportFile(BaseModel):
+    uri: str
+    name: str
+    type: str
+
+
+class CustomerReportEntity(BackendEntity):
+    cost: Optional[Union[str, float]] = None
+    status: str
+    study_id: str
+    upload_id: Optional[str] = None
+    patient_id: str
+    project_id: str
+    study_date: Optional[str] = None
+    report_type: Optional[str] = None
+    patient_name: Optional[str] = None
+    project_name: str
+    report_files: List[CustomerReportFile]
+    report_status: Optional[str] = None
+    project_country: str
+    accession_number: Optional[str] = None
+    dicom_patient_id: Optional[str] = None
+    customer_result_id: Optional[str] = None
+    pipeline_result_id: Optional[str] = None
+    study_instance_uid: str
+    icobrain_report_type: str
+    report_translated_remarks: Optional[str] = None
