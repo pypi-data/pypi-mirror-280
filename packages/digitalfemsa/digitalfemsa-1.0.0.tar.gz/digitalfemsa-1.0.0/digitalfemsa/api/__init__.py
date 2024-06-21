a10a2904cb3cc48550f1805fadf48edb8407e0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:43 +0000

    Add package digitalfemsa/1.0.0/digitalfemsa-1.0.0.tar.gz

---
+++ packages/digitalfemsa/digitalfemsa-1.0.0.tar.gz/digitalfemsa-1.0.0/digitalfemsa/api/__init__.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+
+# import apis into api package
+from digitalfemsa.api.api_keys_api import ApiKeysApi
+from digitalfemsa.api.balances_api import BalancesApi
+from digitalfemsa.api.charges_api import ChargesApi
+from digitalfemsa.api.companies_api import CompaniesApi
+from digitalfemsa.api.customers_api import CustomersApi
+from digitalfemsa.api.discounts_api import DiscountsApi
+from digitalfemsa.api.events_api import EventsApi
+from digitalfemsa.api.logs_api import LogsApi
+from digitalfemsa.api.orders_api import OrdersApi
+from digitalfemsa.api.payment_link_api import PaymentLinkApi
+from digitalfemsa.api.payment_methods_api import PaymentMethodsApi
+from digitalfemsa.api.products_api import ProductsApi
+from digitalfemsa.api.shipping_contacts_api import ShippingContactsApi
+from digitalfemsa.api.shippings_api import ShippingsApi
+from digitalfemsa.api.taxes_api import TaxesApi
+from digitalfemsa.api.transactions_api import TransactionsApi
+from digitalfemsa.api.transfers_api import TransfersApi
+from digitalfemsa.api.webhook_keys_api import WebhookKeysApi
+from digitalfemsa.api.webhooks_api import WebhooksApi
+
