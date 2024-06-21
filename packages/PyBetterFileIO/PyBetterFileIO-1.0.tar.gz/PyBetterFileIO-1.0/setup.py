7c9d63384199ff8e2a4f53d9eb316b96ea0bc3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:33 +0000

    Add package PyBetterFileIO/1.0/PyBetterFileIO-1.0.tar.gz

---
+++ packages/PyBetterFileIO/PyBetterFileIO-1.0.tar.gz/PyBetterFileIO-1.0/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'PyBetterFileIO',         
+  packages = ['PyBetterFileIO'],   
+  version = '1.0',     
+  license='NCSA',        
+  description = 'A faster, more efficient Python file input and output framework',  
+  author = 'Keith Burroughs',                 
+  author_email = 'keithburroughs33@gmail.com',      
+  url = 'https://github.com/keithhb33/PyBetterFileIO',  
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',   
+  keywords = ['python', 'files', 'file', 'folder', 'folders', 'input', 'output', 'fileio', 'file management', 'script'],  
+  install_requires=[            
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',      
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
