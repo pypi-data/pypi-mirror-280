3449b58c5dbb4d74cb7d681c4946e13b4f0ce08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 16:04:22 +0000

    Add package poe-api-wrapper/1.5.4/poe-api-wrapper-1.5.4.tar.gz

---
+++ packages/poe-api-wrapper/poe-api-wrapper-1.5.4.tar.gz/poe-api-wrapper-1.5.4/poe_api_wrapper/openai/type.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Any, Optional
+
+class ChatData(BaseModel):
+    model: Any
+    messages: Any
+    stream: Optional[bool] = False
+    max_tokens: Optional[int] = 4000
+    presence_penalty: Optional[float] = 1.0
+    temperature: Optional[float] = 1.0
+    top_p: Optional[float] = 1.0
+    
+class ImagesData(BaseModel):
+    prompt: Any
+    model: Any
+    n: Optional[int] = 1
+    size: Optional[str] = '1024x1024'
