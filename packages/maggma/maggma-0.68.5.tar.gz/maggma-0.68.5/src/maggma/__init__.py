5d46d72c9dd76faa17ddbec171fdff62d690f819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-21 05:04:55 +0000

    Add package maggma/0.68.5/maggma-0.68.5.tar.gz

---
+++ packages/maggma/maggma-0.68.5.tar.gz/maggma-0.68.5/src/maggma/__init__.py
@@ -0,0 +1,8 @@
+"""Primary Maggma module."""
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("maggma")
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    pass
