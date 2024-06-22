35cad46231a7f15a366c3c895333afa336b44468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:05:25 +0000

    Add package NeoCogs/0.4.5a0/neocogs-0.4.5a0.tar.gz

---
+++ packages/NeoCogs/neocogs-0.4.5a0.tar.gz/neocogs-0.4.5a0/demo/02-hello-extension/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Cogs-Hello',
+    version='0.1',
+    description="""A Cogs task to greet somebody""",
+    packages=['cogs.hello'],
+    package_dir={'': 'src'},
+    install_requires=['Cogs'],
+    entry_points={ 'cogs.extensions': ['Hello = cogs.hello'] },
+)
