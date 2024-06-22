e586cb0dd22cbcc6c854a02c63009c85a16738b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:56 +0000

    Add package url-remote/0.0.106/url_remote-0.0.106-py3-none-any.whl

---
+++ packages/url-remote/url_remote-0.0.106-py3-none-any.whl/url_remote/url_circlez.py
@@ -0,0 +1,5 @@
+from .our_url import OurUrl  # noqa: backward compatibility
+import warnings
+
+warnings.warn("Deprecated: Please use 'from url_remote.our_url import OurUrl' instead of "
+              "'from url_remote.url_circlez import OurUrl'", DeprecationWarning)
