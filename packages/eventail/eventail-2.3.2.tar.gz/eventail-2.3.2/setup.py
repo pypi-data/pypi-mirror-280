8620b8a8361e7e9ea7e8af61914432ff817d108a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:35 +0000

    Add package eventail/2.3.2/eventail-2.3.2.tar.gz

---
+++ packages/eventail/eventail-2.3.2.tar.gz/eventail-2.3.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="eventail",
+    version="2.3.2",
+    url="https://github.com/allo-media/eventail",
+    author="Allo-Media",
+    author_email="dev@allo-media.fr",
+    description="A base class and utilities for AM service architecture",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development",
+    ],
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    scripts=[
+        "scripts/inspect_queue.py",
+        "scripts/logger.py",
+        "scripts/monitor.py",
+        "scripts/publish_configuration.py",
+        "scripts/publish_event.py",
+        "scripts/resurrect.py",
+        "scripts/send_command.py",
+    ],
+    install_requires=["pika>=1.2.0", "cbor2", "redis>=4.5.4"],
+    extras_require={
+        "synchronous": ["kombu"],
+        "test": ["tox"],
+    },
+    python_requires=">=3.8",
+)
