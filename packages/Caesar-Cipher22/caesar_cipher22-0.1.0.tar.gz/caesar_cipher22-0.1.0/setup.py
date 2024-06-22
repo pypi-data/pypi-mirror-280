f186bdcafaa7bb05f94f34daf152b6c715063e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 14:04:26 +0000

    Add package Caesar-Cipher22/0.1.0/caesar_cipher22-0.1.0.tar.gz

---
+++ packages/Caesar-Cipher22/caesar_cipher22-0.1.0.tar.gz/caesar_cipher22-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Caesar_Cipher22",
+    version="0.1.0",
+    description="A package for Caesar Cipher encryption and decryption",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/CaesarCipher",  
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
