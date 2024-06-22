77a47a495f46da743a6bb635c4fd9d59d4d5a57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:05:31 +0000

    Add package wocecolormap/0.0.1/wocecolormap-0.0.1-py3-none-any.whl

---
+++ packages/wocecolormap/wocecolormap-0.0.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,33 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "998bd34e-1256-4484-8691-ddf9d33afbe8",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.18"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
