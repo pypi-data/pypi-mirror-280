5be9ffa4cd14a42ec1d7759bc8d04c84095e5e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 05:05:27 +0000

    Add package randomchessgame/4.0.7/randomchessgame-4.0.7.tar.gz

---
+++ packages/randomchessgame/randomchessgame-4.0.7.tar.gz/randomchessgame-4.0.7/randomchessgame/randomchessgame.py
@@ -0,0 +1,61 @@
+import random
+import pkg_resources
+
+def random_chess_game(length: int = None, min_length: int = None, startswith: str = None, endswith_move: str = None, endswith_mate: bool = False,
+                   cleargame: bool = False) -> str:
+    """
+    Returns a random chess game from a file.
+
+    Params:
+    - length (int): The desired length of the chess game in number of moves.
+    - min_length (int): The minimum length of the chess game in number of moves.
+    - startswith (str): The desired starting move(s) of the chess game.
+    - endswith_move (str): The desired ending move(s) of the chess game.
+    - endswith_mate (bool): Whether the chess game should end with a checkmate.
+    - file (str): The path to the file containing the chess games (default: 'games.pgn').
+    - cleargame (bool): Whether to remove move numbers from the selected game (default: False).
+
+
+    """
+    def no_movechars(game : str):
+        """
+        Removes move numbers from a chess game.
+
+        Params:
+        
+            game (str): The chess game to remove move chars from.
+
+        """
+        return ' '.join([el for el in game.split() if not el.endswith('.')])
+
+    file = pkg.resources.resource_filename('randomchessgame', 'GAMES.pgn')
+
+    with open(file, 'r') as plik:
+        games = [game.strip() for game in plik.readlines() if game.strip()]
+
+    if length:
+        games = [game for game in games if len(no_movechars(game).split()) == length]
+
+    if min_length:
+        games = [game for game in games if len(no_movechars(game).split()) >= min_length]
+
+    if startswith:
+        games = [game for game in games if no_movechars(game).startswith(startswith)]
+
+    if endswith_move:
+        games = [game for game in games if no_movechars(game).endswith(endswith_move)]
+
+    if endswith_mate:
+        games = [game for game in games if game.endswith('#')]
+
+    try:
+        random_game = random.choice(games)
+    
+    except IndexError:
+        return 'No games found with the given criteria.'
+
+    if cleargame:
+        return no_movechars(random_game)
+
+    return random_game
+
---
+++ packages/randomchessgame/randomchessgame-4.0.7.tar.gz/randomchessgame-4.0.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="randomchessgame",
+    author="lot022",
+    version="4.0.7",
+    license="MIT",
+    keywords="chess",
+    python_requires=">=3.7",
+    url="https://github.com/lot022/randomcg",
+    description="Python library to come in handy when random chess game is neeed.",
+    long_description=long_description,
+    include_package_data=True,
+    package_data={
+        'randomchessgame': ['data/*.pgn', '*.txt'],
+        'randomchessgame' : ['*/.pgn'],
+        'randomchessgame' : ['GAMES.pgn'] 
+    },
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe = False,
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
