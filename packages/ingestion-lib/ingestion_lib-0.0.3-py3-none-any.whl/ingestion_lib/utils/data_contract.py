d30eadf3a72a2b308ffda0967a68e0fdd39041c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:59 +0000

    Add package ingestion-lib/0.0.3/ingestion_lib-0.0.3-py3-none-any.whl

---
+++ packages/ingestion-lib/ingestion_lib-0.0.3-py3-none-any.whl/ingestion_lib/utils/data_contract.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+
+class TableContract(BaseModel):
+    db_name: str
+    table_name: str
+    schema_: str
+    batch_timestamp: str
+    watermark_columns: List[str]
+    lower_bound: Optional[str] = None
+    upper_bound: Optional[str] = None
+    full_load: bool = False
+    load_type: str = 'incremental'
+    target_schema: str
+    mount_point: str
+
+    class Config:
+        anystr_lower = True
+        allow_population_by_field_name = True
+        fields = {
+            'schema_': {'alias': 'schema'}
+        }
