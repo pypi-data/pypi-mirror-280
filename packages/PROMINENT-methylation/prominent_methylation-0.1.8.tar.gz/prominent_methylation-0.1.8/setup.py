a10a32dd2c6fe21be117da83e2d5ad4558df779a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:05:54 +0000

    Add package PROMINENT-methylation/0.1.8/prominent_methylation-0.1.8.tar.gz

---
+++ packages/PROMINENT-methylation/prominent_methylation-0.1.8.tar.gz/prominent_methylation-0.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='PROMINENT_methylation',
+    version='0.1.8',
+    packages=['PROMINENT_methylation'],
+    description='PROMINENT: AN INTERPRETABLE DEEP LEARNING METHOD TO PREDICT PHENOTYPES USING DNA METHYLATION',
+    url='https://github.com/cloudmacchiato/dlmethylation',
+    author='Laizhi Zhang',
+    author_email='laz64@pitt.edu',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'PROMINENT-data_prepare = PROMINENT_methylation.dataprep:dataprep',
+            'PROMINENT-train_test = PROMINENT_methylation.train_test:train',
+            'PROMINENT-scores = PROMINENT_methylation.scores:get_scores',
+            'PROMINENT-model_interpret = PROMINENT_methylation.interpret:get_feature_importance'
+        ]
+    },
+    install_requires=['numpy',
+                      'shap',
+                      'seaborn',
+                      'matplotlib',
+                      'torch',
+                      'imblearn'
+    ]
+)
+
+# functions: independent testing, input for model explanation (rownames)
