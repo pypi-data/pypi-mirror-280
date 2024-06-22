9711dea5cad59bf2d38c4f7d34349fae3e86bd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 05:04:52 +0000

    Add package EAGM/1.1.0/EAGM-1.1.0.tar.gz

---
+++ packages/EAGM/EAGM-1.1.0.tar.gz/EAGM-1.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+def load_readme() -> str:
+    with open("README.md",encoding="utf-8_sig") as fin:
+        return fin.read()
+setup(
+    name='EAGM',
+    version='1.1.0',
+    keywords = ("discord","add member"),
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    author='taka4602',
+    author_email='shun4602@gmail.com',
+    url='https://github.com/taka-4602/Discord-Easy-Add-Guild-Member',
+    description='A simple API wrapper for Discord Add Guild Member',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        "requests",
+    ],
+    python_requires='>=3.6',
+)
