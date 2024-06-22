19b436e654b495e6b2da01ae4d6efed77dfe30c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:04:47 +0000

    Add package evidi-fabric/0.0.82/evidi_fabric-0.0.82-py3-none-any.whl

---
+++ packages/evidi-fabric/evidi_fabric-0.0.82-py3-none-any.whl/evidi_fabric/__init__.py
@@ -0,0 +1,14 @@
+"""
+This package provides a collection of modules for data processing and manipulation.
+
+Submodules:
+- clean: Contains functions for cleaning dataframes.
+- fs: Provides functions for file system operations.
+- sql: Includes helper functions for working with SQL.
+- utils: Provides utility functions for handling and manipulating data structures.
+
+You can import these modules like so:
+
+```python
+from my_package import clean, fs, sql, utils
+"""
