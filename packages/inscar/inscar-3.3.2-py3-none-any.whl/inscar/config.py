f6ce9d7255e4de7420ae1cb48189ac096287b36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:04:52 +0000

    Add package inscar/3.3.2/inscar-3.3.2-py3-none-any.whl

---
+++ packages/inscar/inscar-3.3.2-py3-none-any.whl/inscar/config.py
@@ -0,0 +1,231 @@
+"""Objects used to configure the physical parameters of the system."""
+
+from typing import Union
+
+import attr
+import numpy as np
+import scipy.constants as const
+
+
+def is_odd(_, attribute, value):
+    """Verify that a value is odd."""
+    if value % 2 == 0:
+        raise ValueError(f"{attribute} must be odd")
+
+
+def is_positive(_, attribute, value):
+    """Verify that a value is positive."""
+    if not isinstance(value, (int, float)):
+        raise ValueError(f"{attribute} must be a positive number")
+    if value <= 0:
+        raise ValueError(f"{attribute} must be positive")
+
+
+def is_nonnegative(_, attribute, value):
+    """Verify that a value is non-negative."""
+    if not isinstance(value, (int, float)):
+        raise ValueError(f"{attribute} must be a non-negative number")
+    if value < 0:
+        raise ValueError(f"{attribute} must be non-negative")
+
+
+def is_range_tuple(_, attribute, value):
+    """Verify that a value is a tuple of two numbers."""
+    two_tuple = 2
+    if not isinstance(value, tuple):
+        raise ValueError(f"{attribute} must be a tuple")
+    if len(value) != two_tuple:
+        raise ValueError(f"{attribute} must be a tuple of length 2")
+    if isinstance(value[0], (int, float)) and isinstance(value[1], (int, float)):
+        if value[0] >= value[1]:
+            raise ValueError(f"{attribute} must be a tuple of increasing values")
+    else:
+        raise ValueError(f"{attribute} must be a tuple of int/floats")
+
+
+def to_radians(value) -> float:
+    """Convert degrees to radians."""
+    return value * np.pi / 180
+
+
+@attr.s(auto_attribs=True)
+class Particle:
+    """Object used to configure the physical parameters of a particle.
+
+    See Also
+    --------
+    Parameters
+    """  # noqa: D410, D407, D411, D414
+
+    gordeyev_upper_lim: Union[float, int] = attr.ib(
+        default=1.5e-4,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    gordeyev_size: int = attr.ib(
+        default=int(8e4 + 1),
+        validator=is_odd,
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=int,
+    )
+    gordeyev_exp: int = attr.ib(
+        default=3,
+        validator=is_odd,
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=int,
+    )
+    velocity_upper_lim: Union[float, int] = attr.ib(
+        default=6e6,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    velocity_size: int = attr.ib(
+        default=int(4e4 + 1),
+        validator=is_odd,
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=int,
+    )
+    velocity_exp: int = attr.ib(
+        default=3,
+        validator=is_odd,
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=int,
+    )
+    temperature: float = attr.ib(
+        default=5000,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    collision_frequency: float = attr.ib(
+        default=0,
+        validator=is_nonnegative,
+        on_setattr=attr.setters.validate,
+    )
+    mass: float = attr.ib(
+        default=const.m_e,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    number_density: float = attr.ib(
+        default=2e11,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    kappa: float = attr.ib(
+        default=20,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+
+    @property
+    def velocity_axis(self) -> np.ndarray:
+        """Return the axis for the velocity integral."""
+        return (
+            np.linspace(
+                0,
+                self.velocity_upper_lim ** (1 / self.velocity_exp),
+                self.velocity_size,
+            )
+        ) ** self.velocity_exp
+
+    @property
+    def gordeyev_axis(self) -> np.ndarray:
+        """Return the axis for the Gordeyev integral."""
+        return (
+            np.linspace(
+                0,
+                self.gordeyev_upper_lim ** (1 / self.gordeyev_exp),
+                self.gordeyev_size,
+                dtype=np.double,
+            )
+        ) ** self.gordeyev_exp
+
+
+@attr.s(auto_attribs=True)
+class Parameters:
+    """Object used to configure the physical parameters of the system.
+
+    See Also
+    --------
+    Particle
+
+    Examples
+    --------
+    >>> from inscar import config
+
+    Setting aspect angle to 45 degrees.
+
+    >>> p = config.Parameters(aspect_angle=45)
+    >>> print(f"p.aspect_angle = {p.aspect_angle:.4f}")
+    p.aspect_angle = 0.7854
+
+    It's automatically converted to radians! Similarly, setting radar frequency to 430
+    MHz will automatically update radar wave number:
+
+    >>> print(f"p.radar_wavenumber = {p.radar_wavenumber:.4f}")
+    p.radar_wavenumber = -18.0243
+    >>> # Changing radar frequency...
+    >>> p.radar_frequency = 430e5
+    >>> print(f"p.radar_wavenumber = {p.radar_wavenumber:.4f}")
+    p.radar_wavenumber = -1.8024
+
+    Setting aspect angle to 360.5 degrees.
+
+    >>> p.aspect_angle = 360.5
+    >>> print(f"p.aspect_angle = {p.aspect_angle:.4f}")
+    p.aspect_angle = 6.2919
+    """
+
+    radar_frequency: float = attr.ib(
+        default=430e6,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    frequency_range: tuple = attr.ib(
+        default=(-2e6, 2e6),
+        validator=is_range_tuple,
+        on_setattr=attr.setters.validate,
+    )
+    frequency_size: int = attr.ib(
+        default=int(1e4 + 1),
+        validator=is_odd,
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=int,
+    )
+    frequency_exp: int = attr.ib(
+        default=1,
+        validator=is_odd,
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=int,
+    )
+    magnetic_field_strength: float = attr.ib(
+        default=35000e-9,
+        validator=is_positive,
+        on_setattr=attr.setters.validate,
+    )
+    aspect_angle: Union[int, float] = attr.ib(
+        default=45,
+        validator=attr.validators.instance_of((int, float)),
+        on_setattr=[attr.setters.convert, attr.setters.validate],
+        converter=to_radians,
+    )
+
+    @property
+    def linear_frequency(self) -> np.ndarray:
+        """Return the linear frequency used for the power spectrum."""
+        return (
+            np.linspace(
+                self.frequency_range[0], self.frequency_range[1], self.frequency_size
+            )
+            / self.frequency_range[1]
+        ) ** self.frequency_exp * self.frequency_range[1]
+
+    @property
+    def angular_frequency(self) -> np.ndarray:
+        """Return the angular frequency used for the power spectrum."""
+        return self.linear_frequency * 2 * np.pi
+
+    @property
+    def radar_wavenumber(self) -> float:
+        """Return the radar wave number."""
+        return -2 * self.radar_frequency * 2 * np.pi / const.c
---
+++ packages/inscar/inscar-3.3.2-py3-none-any.whl/inscar/numba_integration.py
@@ -0,0 +1,192 @@
+"""Implementation of integrals using ``numba`` for parallelization."""
+
+import math
+from typing import Optional
+
+import numba as nb
+import numpy as np
+import scipy.constants as const
+
+from inscar import config
+
+
+@nb.njit(parallel=True, cache=True)
+def trapz(y, x):
+    """Pure python version of trapezoid rule.
+
+    Parameters
+    ----------
+    y : np.ndarray
+        Function to integrate over.
+    x : np.ndarray
+        The axis to integrate along.
+
+    Returns
+    -------
+    float
+        The value of the integral.
+    """
+    s = 0
+    for i in nb.prange(1, len(x)):
+        s += (x[i] - x[i - 1]) * (y[i] + y[i - 1])
+    return s / 2
+
+
+@nb.njit(cache=True)
+def inner_int(w, x, function):
+    """Calculate the Gordeyev integral of the F function.
+
+    Parameters
+    ----------
+    w : np.ndarray
+        Angular frequency array.
+    x : np.ndarray
+        The axis of the :math:`F` function.
+    function : np.ndarray
+        Function to integrate over.
+
+    Returns
+    -------
+    np.ndarray
+        The integrated function.
+    """
+    array = np.zeros_like(w, dtype=np.complex128)
+    for idx in nb.prange(len(w)):
+        array[idx] = trapz(np.exp(-1j * w[idx] * x) * function, x)
+    return array
+
+
+def integrate(
+    params: config.Parameters,
+    particle: config.Particle,
+    integrand: np.ndarray,
+    the_type: str,
+    char_vel: Optional[float] = None,
+) -> np.ndarray:
+    r"""Calculate the Gordeyev integral for each frequency.
+
+    This locates the Gordeyev axis of the particle object, and for each frequency
+    calculates the Gordeyev integral corresponding to it, returning an array of the same
+    length as the frequency array.
+
+    Parameters
+    ----------
+    params : Parameters
+        ``Parameters`` object with simulation parameters.
+    particle : Particle
+        ``Particle`` object with particle parameters.
+    integrand : np.ndarray
+        Function to integrate over.
+    the_type : str
+        Defines which type of ``Integrand`` class that is used.
+    char_vel : float, optional
+        Characteristic velocity of the particle.
+
+    Returns
+    -------
+    np.ndarray
+        The integral evaluated along the whole Gordeyev frequency axis of the particle.
+
+    See Also
+    --------
+    inscar.spectrum_calculation.SpectrumCalculation.set_calculate_f_function
+
+    Notes
+    -----
+    The :math:`F` function here refers to the equation found in Hagfors [6]_:
+
+    .. math::
+
+       \begin{aligned}
+       F_{\mathrm{e}}(\boldsymbol{k}, \omega)
+       =1-\left(i\frac{X(\omega)}{X_{\mathrm{e}}}+\Lambda_{\mathrm{e}}\right)\int_0^{\infty}\exp\left
+       \{-iy\frac{X(\omega)}{X_{\mathrm{e}}}-y\Lambda_{\mathrm{e}}\right.\\
+       \left.-\frac{1}{2X_{\mathrm{e}}^2}\left[\sin^2\theta(1-\cos
+       y)+\frac{1}{2}y^2\cos^2\theta\right]\right \}\textnormal{d}y
+       \end{aligned}
+
+    References
+    ----------
+    .. [6] T. Hagfors, "Density Fluctuations in a Plasma in a Magnetic Field, with
+        Applications to the Ionosphere," Journal of Geophysical Research, vol. 66,
+        no. 9, pp. 1699-1712, 1961.
+    """
+    y = particle.gordeyev_axis
+    temp = particle.temperature
+    mass = particle.mass
+    w = params.angular_frequency
+    nu = particle.collision_frequency
+    array = inner_int(w, y, integrand)
+    if the_type == "a_vdf":
+        # Characteristic velocity scaling
+        a = 4 * np.pi * temp * const.k * array / mass * char_vel
+    elif the_type == "kappa":
+        a = array / (2 ** (particle.kappa - 1 / 2) * math.gamma(particle.kappa + 1 / 2))
+    else:
+        a = array
+    return a if the_type == "a_vdf" else 1 - (1j * w + nu) * a
+
+
+@nb.njit(cache=True)
+def integrate_velocity(y, v, f, k_r, theta, w_c):  # noqa: PLR0913
+    """Calculate the velocity integral.
+
+    Parameters
+    ----------
+    y : np.ndarray
+        The axis in frequency space.
+    v : np.ndarray
+        The axis in velocity space.
+    f : np.ndarray
+        The function to integrate.
+    k_r : float
+        The radar wavenumber.
+    theta : float
+        The radar aspect angle.
+    w_c : float
+        The gyro frequency.
+
+    Returns
+    -------
+    np.ndarray
+        The integrated function.
+    """
+    array = np.zeros_like(y)
+    for idx in nb.prange(len(y)):
+        array[idx] = trapz(v * np.sin(p(y[idx], k_r, theta, w_c) * v) * f, v)
+    return array
+
+
+@nb.njit(cache=True)
+def p(y, k_r, theta, w_c):
+    """Calculate the :math:`p` function.
+
+    Parameters
+    ----------
+    y : np.ndarray
+        Parameter from Gordeyev integral
+    k_r : float
+        The radar wave number.
+    theta : float
+        The radar aspect angle.
+    w_c : float
+        The gyro frequency.
+
+    Returns
+    -------
+    np.ndarray
+        Value of the :math:`p` function
+
+    Notes
+    -----
+    Implementation of the :math:`p` function from Mace [7]_.
+
+    References
+    ----------
+    .. [7] R. L. Mace, "A Gordeyev integral for electrostatic waves in a magnetized
+       plasma with a kappa velocity distribution," Physics of plasmas, vol. 10, no. 6,
+       pp. 2101-2193, 2003.
+    """
+    k_perp = k_r * np.sin(theta)
+    k_par = k_r * np.cos(theta)
+    return (2 * k_perp**2 / w_c**2 * (1 - np.cos(y * w_c)) + k_par**2 * y**2) ** 0.5
