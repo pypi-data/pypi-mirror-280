9a64936a9737bdef4ec4d6049a4223867ed5e9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 14:04:29 +0000

    Add package himalaya/0.4.6/himalaya-0.4.6-py3-none-any.whl

---
+++ packages/himalaya/himalaya-0.4.6-py3-none-any.whl/himalaya/__init__.py
@@ -0,0 +1,21 @@
+from . import backend
+from . import kernel_ridge
+from . import lasso
+from . import ridge
+from . import progress_bar
+from . import scoring
+from . import utils
+from . import viz
+
+__version__ = '0.4.6'
+
+__all__ = [
+    backend,
+    kernel_ridge,
+    lasso,
+    ridge,
+    progress_bar,
+    scoring,
+    utils,
+    viz,
+]
