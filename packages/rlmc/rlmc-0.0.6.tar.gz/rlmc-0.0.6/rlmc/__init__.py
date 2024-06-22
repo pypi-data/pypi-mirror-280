82bdb8c21dcc1f97a820effe2fe6df6487b9c6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:05:11 +0000

    Add package rlmc/0.0.6/rlmc-0.0.6.tar.gz

---
+++ packages/rlmc/rlmc-0.0.6.tar.gz/rlmc-0.0.6/rlmc/__init__.py
@@ -0,0 +1,54 @@
+"""
+@File    :   __init__.py
+@Time    :   2024/06/19 13:38:17
+@Author  :   RayLam
+@Contact :   1027196450@qq.com
+"""
+
+__version__ = "0.0.6"
+
+import sys
+
+sys.path.append(".")
+
+from rlmc.utils.logger import Logger
+from rlmc.utils.mc import MetaList
+from rlmc.utils.register import Register
+from rlmc.utils.sudict import SuDict
+from rlmc.utils.multiprocess import MultiProcess
+from rlmc.utils.multithread import MultiThread
+from rlmc.utils.asynctask import AsyncTasks
+
+from rlmc.fileop import file_processor
+
+# from rlmc.configs.cfg import Configs
+
+
+reg = Register()
+reg.register(MetaList)
+reg.register(Logger)
+reg.register(SuDict)
+reg.register(MultiProcess)
+reg.register(MultiThread)
+reg.register(AsyncTasks)
+reg.register(file_processor)
+
+__all__ = [
+    "__version__",
+    "Logger",
+    "MetaList",
+    "Register",
+    "SuDict",
+    "MultiProcess",
+    "MultiThread",
+    "AsyncTasks",
+    "file_processor",
+    "reg",
+]
+
+
+if __name__ == "__main__":
+    print(reg.keys())
+    yamlobj = reg['file_processor']("rlmc/configs/cfg.yaml")
+    yaml_content = yamlobj.read()
+    print(yaml_content)
