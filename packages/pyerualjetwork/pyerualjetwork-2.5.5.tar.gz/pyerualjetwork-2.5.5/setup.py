76b62dfb39d43e3075b510a38cc4552627f80096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:04:57 +0000

    Add package pyerualjetwork/2.5.5/pyerualjetwork-2.5.5.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.5.5.tar.gz/pyerualjetwork-2.5.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.5.5",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "standard_scaler function improved. Changes possibily nan values to 0",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
