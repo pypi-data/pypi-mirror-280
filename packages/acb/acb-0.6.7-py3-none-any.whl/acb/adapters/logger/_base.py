19cfa59c75bb876f974400ed7be506ce1c4f55e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 05:04:04 +0000

    Add package acb/0.6.7/acb-0.6.7-py3-none-any.whl

---
+++ packages/acb/acb-0.6.7-py3-none-any.whl/acb/adapters/logger/_base.py
@@ -0,0 +1,10 @@
+from acb.adapters import AdapterBase
+from acb.config import Settings
+
+
+class LoggerBaseSettings(Settings):
+    verbose: bool = False
+    deployed_level: str = "ERROR"
+
+
+class LoggerBase(AdapterBase): ...
