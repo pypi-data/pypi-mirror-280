59c2dea8fb87ca053fdc6e2ca36fcc7e0c309c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 05:05:08 +0000

    Add package tasktiger/0.19.4/tasktiger-0.19.4.tar.gz

---
+++ packages/tasktiger/tasktiger-0.19.4.tar.gz/tasktiger-0.19.4/tasktiger/__init__.py
@@ -0,0 +1,37 @@
+from .exceptions import (
+    JobTimeoutException,
+    QueueFullException,
+    RetryException,
+    StopRetry,
+    TaskImportError,
+    TaskNotFound,
+)
+from .retry import exponential, fixed, linear
+from .schedule import cron_expr, periodic
+from .task import Task
+from .tasktiger import TaskTiger, run_worker
+from .worker import Worker
+
+__all__ = [
+    "TaskTiger",
+    "Worker",
+    "Task",
+    # Exceptions
+    "JobTimeoutException",
+    "RetryException",
+    "StopRetry",
+    "TaskImportError",
+    "TaskNotFound",
+    "QueueFullException",
+    # Retry methods
+    "fixed",
+    "linear",
+    "exponential",
+    # Schedules
+    "periodic",
+    "cron_expr",
+]
+
+
+if __name__ == "__main__":
+    run_worker()
