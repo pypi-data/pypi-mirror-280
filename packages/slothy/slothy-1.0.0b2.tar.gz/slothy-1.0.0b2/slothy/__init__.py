9e751f7589fcc53dd02fb07bf0b5ea3601c79964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:32 +0000

    Add package slothy/1.0.0b2/slothy-1.0.0b2.tar.gz

---
+++ packages/slothy/slothy-1.0.0b2.tar.gz/slothy-1.0.0b2/slothy/__init__.py
@@ -0,0 +1,88 @@
+"""Slothy's exported functionality."""
+
+from __future__ import annotations
+
+import sys
+from os import getenv
+from typing import Any
+
+__all__ = (
+    "SLOTHY_ENABLED",
+    "slothy_importing",
+    "slothy_importing_if",
+    "lazy_importing",
+    "lazy_importing_if",
+    "type_importing",
+)
+
+SLOTHY_ENABLED: bool
+"""Whether slothy is enabled."""
+
+try:
+    sys._getframe  # noqa: B018, SLF001
+except AttributeError:  # pragma: no cover
+    SLOTHY_ENABLED = False
+else:
+    SLOTHY_ENABLED = not getenv("SLOTHY_DISABLE")
+
+if SLOTHY_ENABLED:
+    from slothy._importing import slothy_importing, slothy_importing_if, type_importing
+else:
+    from contextlib import contextmanager, nullcontext
+    from typing import TYPE_CHECKING
+    from warnings import warn
+
+    if TYPE_CHECKING:
+        from collections.abc import Iterator
+        from contextlib import AbstractContextManager
+
+    if not getenv("SLOTHY_NO_WARN"):
+        warn(
+            category=RuntimeWarning,
+            message=(
+                "This Python implementation does not support "
+                "`sys._getframe()` and thus cannot use `slothy_importing`. "
+            ),
+            stacklevel=1,
+        )
+
+    EAGER_PREVENTION_MSG = (
+        "Unsupported Python implementation: "
+        "slothy imports cannot default to eager mode"
+    )
+
+    @contextmanager
+    def slothy_importing(
+        *,
+        prevent_eager: bool = True,
+        stack_offset: int = 1,  # noqa: ARG001
+    ) -> Iterator[None]:
+        """Replace slothy with a no-op on unsupported Python implementation."""
+        if prevent_eager:
+            raise RuntimeError(EAGER_PREVENTION_MSG)
+        yield
+
+    @contextmanager
+    def type_importing(
+        *,
+        default_type: object = Any,  # noqa: ARG001
+        stack_offset: int = 1,  # noqa: ARG001
+    ) -> Iterator[None]:
+        """Fail immediately on unsupported Python implementation."""
+        raise RuntimeError(EAGER_PREVENTION_MSG)
+        yield
+
+    def slothy_importing_if(
+        condition: object,
+        *,
+        prevent_eager: bool = True,
+        stack_offset: int = 1,  # noqa: ARG001
+    ) -> AbstractContextManager[None]:
+        """Replace slothy with a no-op on unsupported Python implementation."""
+        if condition and prevent_eager:
+            raise RuntimeError(EAGER_PREVENTION_MSG)
+        return nullcontext()
+
+
+lazy_importing = slothy_importing
+lazy_importing_if = slothy_importing_if
