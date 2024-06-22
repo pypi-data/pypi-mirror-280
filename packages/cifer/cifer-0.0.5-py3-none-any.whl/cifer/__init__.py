3234285d161c20b1be692ad7a2d9c220b9fb63fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:05:33 +0000

    Add package cifer/0.0.5/cifer-0.0.5-py3-none-any.whl

---
+++ packages/cifer/cifer-0.0.5-py3-none-any.whl/cifer/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "cifer.ai"
+__author__ = "cifer"
+__license__ = "MIT"
+__main__ = "app"
+
+__all__ = ["app", "data_request_management", "file_transfer", "portforwardlib"]
+
+from .app import app
