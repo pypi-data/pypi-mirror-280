9febac3ce1e833562b94a419ded322f11730078e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:19 +0000

    Add package qsreplace/0.0.3.post1/qsreplace-0.0.3.post1.tar.gz

---
+++ packages/qsreplace/qsreplace-0.0.3.post1.tar.gz/qsreplace-0.0.3.post1/setup.py
@@ -0,0 +1,23 @@
+import io
+from setuptools import setup, find_packages
+from os import path
+
+pwd = path.abspath(path.dirname(__file__))
+with io.open(path.join(pwd, 'README.md'), encoding='utf-8') as readme:
+    desc = readme.read()
+
+setup(
+    name='qsreplace',
+    version='0.0.3.post1',
+    description='A utility to replace query parameters in URLs',
+    long_description=desc,
+    long_description_content_type='text/markdown',
+    author='basedygt',
+    license='Apache-2.0 License',
+    author_email='basedygt@gmail.com',
+    url='https://github.com/basedygt/qsreplace',
+    packages=find_packages(),
+    py_modules=['qsreplace'],
+    keywords=['qsreplace', 'url editor', 'params editor'],
+    install_requires=['urllib3'],
+)
