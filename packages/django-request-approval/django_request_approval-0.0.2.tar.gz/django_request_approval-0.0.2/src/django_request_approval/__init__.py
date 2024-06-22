590d100f3bbb8e2d2d24fef58b240f7f5f0d1ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:05:50 +0000

    Add package django-request-approval/0.0.2/django_request_approval-0.0.2.tar.gz

---
+++ packages/django-request-approval/django_request_approval-0.0.2.tar.gz/django_request_approval-0.0.2/src/django_request_approval/__init__.py
@@ -0,0 +1,3 @@
+from .models import BaseStage, BaseApprover, BaseRequest, BaseApproval, RequestStatus, ApprovalStatus
+from .services import ApproverService, ApprovalService
+from .forms import ApprovalForm
