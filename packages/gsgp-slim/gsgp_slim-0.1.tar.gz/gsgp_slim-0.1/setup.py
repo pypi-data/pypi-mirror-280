f362208519edbc3aa02d064668b14b26f843af86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:28 +0000

    Add package gsgp-slim/0.1/gsgp_slim-0.1.tar.gz

---
+++ packages/gsgp-slim/gsgp_slim-0.1.tar.gz/gsgp_slim-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gsgp_slim',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'torch'
+    ],
+    entry_points={
+        'console_scripts': [
+            # If you have any command-line scripts
+            # 'command-name = mylibrary.module:function',
+        ],
+    },
+    author= 'Davide Farinati, Diogo Rasteiro, Liah Rosenfeld, Gloria Pietropolli', 
+    author_email='dfarinati@novaims.unl.pt, drasteiro@novaims.unl.pt, lrosenfeld@novaims.unl.pt, gloria.pietropolli@phd.units.it',
+    description='Semantic Learning algorithm based on Inflate and deflate Mutation (SLIM GSGP)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/DALabNOVA/slim',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
