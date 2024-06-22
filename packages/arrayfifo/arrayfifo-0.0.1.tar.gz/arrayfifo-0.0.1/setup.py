4db8013a7418a1419b899a3f684aec512c33a659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:43 +0000

    Add package arrayfifo/0.0.1/arrayfifo-0.0.1.tar.gz

---
+++ packages/arrayfifo/arrayfifo-0.0.1.tar.gz/arrayfifo-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='arrayfifo',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/danionella/arrayfifo',
+    license='MIT',
+    author='jlab.berlin',
+    author_email='',
+    description='Fast multiprocessing FIFO buffer (queue) for numpy arrays',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
