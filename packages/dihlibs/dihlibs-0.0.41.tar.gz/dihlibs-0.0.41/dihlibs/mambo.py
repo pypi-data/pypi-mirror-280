401d9d2e0dd0fd5d05d162302631cdabc759e14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:43 +0000

    Add package dihlibs/0.0.41/dihlibs-0.0.41.tar.gz

---
+++ packages/dihlibs/dihlibs-0.0.41.tar.gz/dihlibs-0.0.41/dihlibs/mambo.py
@@ -0,0 +1,13 @@
+from dihlibs.node import Node
+import dihlibs.functions as fn
+from dihlibs.dhis.meta import DHIS_Meta
+from dihlibs.dhis.configuration import Configuration
+
+
+conf=Configuration()
+meta=DHIS_Meta(conf)
+
+meta
+
+
+[]
