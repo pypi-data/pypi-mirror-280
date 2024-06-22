80cac2106c26525b823a0b620908a4ae2159df68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:10:06 +0000

    Add package zoDIAq/2.1.2/zodiaq-2.1.2.tar.gz

---
+++ packages/zoDIAq/zodiaq-2.1.2.tar.gz/zodiaq-2.1.2/src/zodiaq/loaders/library/mappings.py
@@ -0,0 +1,51 @@
+newColumns = [
+    "precursorMz",
+    "peptideName",
+    "peakMz",
+    "peakIntensity",
+    "precursorCharge",
+    "identification",
+    "proteinName",
+    "fragmentType",
+    "fragmentNumber",
+    "fragmentCharge",
+]
+
+oldColumnsSpectrast = [
+    "PrecursorMz",
+    "FullUniModPeptideName",
+    "ProductMz",
+    "LibraryIntensity",
+    "PrecursorCharge",
+    "transition_group_id",
+    "ProteinName",
+    "FragmentType",
+    "FragmentSeriesNumber",
+    "FragmentCharge",
+]
+
+oldColumnsFragpipe = [
+    "PrecursorMz",
+    "ModifiedPeptideSequence",
+    "ProductMz",
+    "LibraryIntensity",
+    "PrecursorCharge",
+    "PeptideSequence",
+    "ProteinId",
+    "FragmentType",
+    "FragmentSeriesNumber",
+    "FragmentCharge",
+]
+
+oldColumnsProsit = [
+    "PrecursorMz",
+    "LabeledPeptide",
+    "FragmentMz",
+    "RelativeIntensity",
+    "PrecursorCharge",
+    "StrippedPeptide",
+    "FragmentLossType",
+    "FragmentType",
+    "FragmentNumber",
+    "FragmentCharge",
+]
