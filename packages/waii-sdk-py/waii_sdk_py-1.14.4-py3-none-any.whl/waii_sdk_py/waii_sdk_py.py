494baa10195b4a648596f863f738c2b89998e0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 05:04:44 +0000

    Add package waii-sdk-py/1.14.4/waii_sdk_py-1.14.4-py3-none-any.whl

---
+++ packages/waii-sdk-py/waii_sdk_py-1.14.4-py3-none-any.whl/waii_sdk_py/waii_sdk_py.py
@@ -0,0 +1,42 @@
+from .history import HistoryImpl, History
+from .query import QueryImpl, Query
+from .database import DatabaseImpl, Database
+from .semantic_context import SemanticContextImpl, SemanticContext
+from .waii_http_client import WaiiHttpClient
+import importlib.metadata
+
+class Waii:
+
+    def __init__(self, initialize_legacy_fields: bool = False):
+        self.history = None
+        self.query = None
+        self.database = None
+        self.semantic_context = None
+        self.initialize_legacy_fields = initialize_legacy_fields
+
+    def initialize(self, url: str = "https://tweakit.waii.ai/api/", api_key: str = ""):
+        http_client = WaiiHttpClient(url, api_key)
+        self.history = HistoryImpl(http_client)
+        self.query = QueryImpl(http_client)
+        self.database = DatabaseImpl(http_client)
+        self.semantic_context = SemanticContextImpl(http_client)
+
+        if self.initialize_legacy_fields:
+            self.History = self.history
+            self.Query = self.query
+            self.Database = self.database
+            self.SemanticContext = self.semantic_context
+            Query.http_client = http_client
+            History.http_client = http_client
+            Database.http_client = http_client
+            SemanticContext.http_client = http_client
+
+        conns = self.database.get_connections().connectors
+        if len(conns) > 0:
+            self.database.activate_connection(conns[0].key)
+
+    @staticmethod
+    def version():
+        return importlib.metadata.version('waii-sdk-py')
+
+WAII = Waii(True)
