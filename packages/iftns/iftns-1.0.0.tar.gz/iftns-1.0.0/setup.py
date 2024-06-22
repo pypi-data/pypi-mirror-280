26fee307d99f399f9cb0561c67b3cdd5508af3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:13 +0000

    Add package iftns/1.0.0/iftns-1.0.0.tar.gz

---
+++ packages/iftns/iftns-1.0.0.tar.gz/iftns-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='iftns',
+    version='1.0.0',
+    author='Margret Westerkamp',
+    author_email='margret@mpa-garching.mpg.de',
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
