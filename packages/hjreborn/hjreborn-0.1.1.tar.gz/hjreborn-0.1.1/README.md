95c455499b968f392c027d834b58e2ad990845b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:05:34 +0000

    Add package hjreborn/0.1.1/hjreborn-0.1.1.tar.gz

---
+++ packages/hjreborn/hjreborn-0.1.1.tar.gz/hjreborn-0.1.1/README.md
@@ -0,0 +1,27 @@
+# 欢迎 / Welcome
+
+欢迎来到 Hello Judger Reborn（HJ 重生）项目的主页。
+
+Welcome to the main page of Hello Judge Reborn.
+
+致力于简化竞技编程竞赛中代码的评测流程。
+
+Committed to simplifying the code evaluation process in competitive programming competitions.
+
+本 Python 项目在 GNU Affero 通用公共许可证（AGPL）协议下开源，这通常意味着：
+
+This Python project is open source under the GNU Affero General Public License (AGPL), which typically means:
+
+- 当您对本项目进行更改后，您应该将您修改后的代码（或修改的片段）以 AGPL 协议开源。
+- After making changes to this project, you should open source your modified code (or modified fragments) using the AGPL license.
+- 当您对本项目进行更改后，我们有权将您的代码并入我们的代码中。
+- After you make changes to this project, we have the right to merge your code into our code.
+
+# 安装与启动 / Install and Launch
+
+- 直接下载源码。
+- Just download the source code.
+- 运行 `resdown.py` 以下载资源。注意，需要从 Github 下载资源。该程序只需要运行一次。
+- Run `resdown.py` to download resources.This program don't need to run twice.
+- 运行主程序 `main.py`。
+- Run the main program `main.py`.
---
+++ packages/hjreborn/hjreborn-0.1.1.tar.gz/hjreborn-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+setup(
+    name="hjreborn",
+    version="0.1.1",
+    packages=[
+        "hjreborn",
+        "hjreborn.config",
+        "hjreborn.core",
+        "hjreborn.frontend",
+        "hjreborn.i18n"
+    ],
+    install_requires=["rich", "parse", "click", "jsonschema", "simplejson", "psutil"],
+    entry_points={"console_scripts": ["hjreborn = hjreborn.main:group"]},
+    author="xiezheyuan",
+    author_email="xiezheyuan09@163.com",
+    description="A simple CLI offline judge system",
+    long_description="A simple CLI offline judge system",
+    long_description_content_type="text/plain",
+    url="https://github.com/hellojudger/HelloJudgerReborn",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Topic :: Education",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Education :: Testing",
+        "Programming Language :: C++"
+    ],
+    license="AGPL-3.0",
+    python_requires=">=3.8",
+)
