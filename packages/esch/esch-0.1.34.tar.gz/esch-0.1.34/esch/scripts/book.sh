17702b7e675491db5936d00d8696009e4fd851b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 14:04:46 +0000

    Add package esch/0.1.34/esch-0.1.34.tar.gz

---
+++ packages/esch/esch-0.1.34.tar.gz/esch-0.1.34/esch/scripts/book.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Configuration base dir has chapters sorted by number
+base_dir=$1
+base_dir_name=$(basename "$base_dir")
+config_json=$2
+
+chapter_files=$(ls "$base_dir"/*.md | sort -V)
+
+# Create a temporary file for intermediate processing
+full_book=$(mktemp)
+
+# Concatenate all chapter files and clean the content
+for file in $chapter_files; do
+    # Remove control characters and non-UTF8 characters
+    cat "$file" | tr -d '\000-\031' | iconv -f utf-8 -t utf-8 -c >> "$full_book"
+    # Add a newline between chapters
+    echo -e "\n\n" >> "$full_book"
+done
+
+# Convert the Markdown files to PDF with Pandoc
+pandoc "$full_book" -o "$base_dir/$base_dir_name.pdf" \
+    --biblatex \
+    --slide-level=3 \
+    --filter=pandoc-crossref
+
+# Cleanup intermediate files
+rm -f "$full_book"
+
+# Open the final PDF file
+open "$base_dir/$base_dir_name.pdf"
