e9a0babbbee47f79dea6cc526646724ffe884768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:51 +0000

    Add package ftbx/0.1.dev287/ftbx-0.1.dev287.tar.gz

---
+++ packages/ftbx/ftbx-0.1.dev287.tar.gz/ftbx-0.1.dev287/src/update.py
@@ -0,0 +1,38 @@
+"""
+
+    PROJECT: flex_toolbox
+    FILENAME: update.py
+    AUTHOR: David NAISSE
+    DATE: March 3rd, 2024
+
+    DESCRIPTION: update command functions
+
+    TEST STATUS: DOES NOT REQUIRE TESTING
+"""
+
+import subprocess
+
+from src.utils import update_toolbox_resources
+
+
+def update_command_func(args):
+    """
+    Action on update command.
+
+    TEST STATUS: DOES NOT REQUIRE TESTING
+    """
+
+    print(f"\nUpdating ftbx to latest version..\n")
+    subprocess.run(
+        [
+            "pip",
+            "install",
+            "ftbx",
+            "--upgrade",
+            "--quiet",
+        ],
+        check=True,
+    )
+
+    # fetch updated resources
+    update_toolbox_resources()
