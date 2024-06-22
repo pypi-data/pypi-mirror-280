9d95c9468012d73285e729697074bb03417c1279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:05:01 +0000

    Add package geobizml/0.1.0/geobizml-0.1.0-py3-none-any.whl

---
+++ packages/geobizml/geobizml-0.1.0-py3-none-any.whl/geobizml/__init__.py
@@ -0,0 +1,5 @@
+# geobizml/__init__.py
+
+from .visualization import *
+from .clusterAnalysis import *
+#from .ml import *
---
+++ packages/geobizml/geobizml-0.1.0-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,5 @@
+# geobizml/__init__.py
+
+from .test_visualization import *
+from .test_clusterAnalysis import *
+#from .ml import *
