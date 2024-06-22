5aa4a50ce780b767f918705692a3aa4b12ae032d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:05:33 +0000

    Add package nomenklatura/3.12.3/nomenklatura-3.12.3.tar.gz

---
+++ packages/nomenklatura/nomenklatura-3.12.3.tar.gz/nomenklatura-3.12.3/nomenklatura/__init__.py
@@ -0,0 +1,17 @@
+from nomenklatura.dataset import Dataset
+from nomenklatura.entity import CompositeEntity
+from nomenklatura.resolver import Resolver
+from nomenklatura.store import Store, View
+from nomenklatura.index import Index
+from nomenklatura.index import TantivyIndex
+
+__version__ = "3.12.3"
+__all__ = [
+    "Dataset",
+    "CompositeEntity",
+    "Resolver",
+    "Index",
+    "TantivyIndex",
+    "Store",
+    "View",
+]
---
+++ packages/nomenklatura/nomenklatura-3.12.3.tar.gz/nomenklatura-3.12.3/nomenklatura/index/__init__.py
@@ -0,0 +1,5 @@
+from nomenklatura.index.index import Index
+from nomenklatura.index.tantivy_index import TantivyIndex
+from nomenklatura.index.common import BaseIndex
+
+__all__ = ["BaseIndex", "Index", "TantivyIndex"]
