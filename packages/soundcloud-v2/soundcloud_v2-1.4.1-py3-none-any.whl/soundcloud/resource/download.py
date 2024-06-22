3280a05934601d22e28e5d7ea56466502863e658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 14:04:50 +0000

    Add package soundcloud-v2/1.4.1/soundcloud_v2-1.4.1-py3-none-any.whl

---
+++ packages/soundcloud-v2/soundcloud_v2-1.4.1-py3-none-any.whl/soundcloud/resource/download.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from soundcloud.resource.base import BaseData
+
+
+@dataclass
+class OriginalDownload(BaseData):
+    """Contains a download link for a track"""
+    redirectUri: str
