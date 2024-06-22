9ae6142c19b9bf160003ffa24fee96a4f3ce38ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:43 +0000

    Add package SheetCode/0.0.14/sheetcode-0.0.14-py3-none-any.whl

---
+++ packages/SheetCode/sheetcode-0.0.14-py3-none-any.whl/SheetCode/Overview.py
@@ -0,0 +1,22 @@
+import os
+from openpyxl.workbook import Workbook # need pip install openpyxl
+import SheetCode as Sheet
+import pandas as pd
+from termcolor import colored # need pip install termcolor
+from tabulate import tabulate
+
+SheetSummaries = pd.DataFrame(columns=['Reference','Name','Version','Global Status','Passed count','Failed count'])
+
+def Clear():
+    SheetSummaries = []
+
+def Store(sheet):
+    print(colored(f"Storing in overview", "green"))
+    SheetSummaries.loc[len(SheetSummaries.index)] = [sheet.Reference, sheet.Name, sheet.Version, "No test done" if sheet.GlobalStatus == None else "Passed" if sheet.GlobalStatus else "FAILED", sheet.Statistics.PassedCount, sheet.Statistics.FailedCount]
+
+def Save():
+    print(colored(f"Saving overview", "green"))
+    print(tabulate(SheetSummaries, headers='keys', tablefmt='simple_grid', showindex=False))
+    
+    if not os.path.exists(os.environ["OVERVIEW_PATH"]): os.mkdir(os.environ["OVERVIEW_PATH"])
+    SheetSummaries.to_excel(os.path.join(os.environ["OVERVIEW_PATH"], "Overview.xlsx"), index=False)
