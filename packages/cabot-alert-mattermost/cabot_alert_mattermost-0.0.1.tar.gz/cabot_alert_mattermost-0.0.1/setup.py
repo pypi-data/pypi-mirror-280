dc074bc10d22ce94fe5d17eb4619fee0778277ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:08 +0000

    Add package cabot-alert-mattermost/0.0.1/cabot_alert_mattermost-0.0.1.tar.gz

---
+++ packages/cabot-alert-mattermost/cabot_alert_mattermost-0.0.1.tar.gz/cabot_alert_mattermost-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cabot-alert-mattermost",
+    version="0.0.1",
+    author="Your Name",
+    description="for poc",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
