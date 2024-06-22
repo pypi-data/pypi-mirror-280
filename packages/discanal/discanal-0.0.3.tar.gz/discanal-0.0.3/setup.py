75285ee4864882740b2bd81bc8597999598214e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 14:04:28 +0000

    Add package discanal/0.0.3/discanal-0.0.3.tar.gz

---
+++ packages/discanal/discanal-0.0.3.tar.gz/discanal-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discanal", # Replace with your own username
+    version="0.0.3",
+    author="hyungrae0907",
+    author_email="hyungrae0907@gmail.com",
+    install_requires=['scikit-learn',],
+    description="판별분석",
+    url="https://github.com/KHR0907/datamining.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
