76230e8d28b12eccf0ea53daddc36b8e750673df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:40 +0000

    Add package byted-barocket/0.1.1/byted_barocket-0.1.1.tar.gz

---
+++ packages/byted-barocket/byted_barocket-0.1.1.tar.gz/byted_barocket-0.1.1/python/test.py
@@ -0,0 +1,7 @@
+from pkginfo import Wheel
+
+
+pkg = Wheel("/Users/bytedance/projects/codes/rust/ba-rocket/target/wheels/byted_barocket-0.1.0-cp39-cp39-macosx_10_12_x86_64.whl")
+
+print(pkg.name)
+
