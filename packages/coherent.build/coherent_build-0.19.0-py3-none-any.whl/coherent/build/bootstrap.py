7ffa52070ff0bedd6b429e5ba793a42e8b2b34be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:52 +0000

    Add package coherent.build/0.19.0/coherent_build-0.19.0-py3-none-any.whl

---
+++ packages/coherent.build/coherent_build-0.19.0-py3-none-any.whl/coherent/build/bootstrap.py
@@ -0,0 +1,16 @@
+import contextlib
+import importlib.resources
+import pathlib
+
+
+@contextlib.contextmanager
+def write_pyproject(target: pathlib.Path = pathlib.Path()):
+    path = target / 'pyproject.toml'
+    if path.exists():
+        yield
+        return
+    path.write_text(importlib.resources.files().joinpath('system.toml').read_text())
+    try:
+        yield
+    finally:
+        path.unlink()
