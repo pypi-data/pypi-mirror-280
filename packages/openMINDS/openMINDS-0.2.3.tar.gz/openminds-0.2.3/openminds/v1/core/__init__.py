02d11124dfc7a9eacae634e026522000e8931487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 04:04:15 +0000

    Add package openMINDS/0.2.3/openMINDS-0.2.3.tar.gz

---
+++ packages/openMINDS/openMINDS-0.2.3.tar.gz/openminds-0.2.3/openminds/v1/core/__init__.py
@@ -0,0 +1,33 @@
+from .actors import Contribution, Organization, Person
+from .data import ContentType, Copyright, FileBundle, FileInstance, FileRepository, Hash, License
+from .miscellaneous import (
+    DigitalIdentifier,
+    DigitalIdentifierSchema,
+    Funding,
+    QuantitativeValue,
+    QuantitativeValueRange,
+)
+from .products import (
+    Dataset,
+    DatasetVersion,
+    MetaDataModel,
+    MetaDataModelVersion,
+    Model,
+    ModelVersion,
+    Project,
+    Software,
+    SoftwareVersion,
+)
+from .research import (
+    ParameterSetting,
+    Protocol,
+    ProtocolExecution,
+    Subject,
+    SubjectGroup,
+    SubjectGroupState,
+    SubjectState,
+    TissueSample,
+    TissueSampleCollection,
+    TissueSampleCollectionState,
+    TissueSampleState,
+)
---
+++ packages/openMINDS/openMINDS-0.2.3.tar.gz/openminds-0.2.3/openminds/v3/core/data/license.py
@@ -0,0 +1,69 @@
+"""
+Structured information on a used license.
+"""
+
+# this file was auto-generated!
+
+from openminds.base import IRI
+
+from openminds.base import LinkedMetadata
+from openminds.properties import Property
+
+
+class License(LinkedMetadata):
+    """
+    Structured information on a used license.
+    """
+
+    type_ = "https://openminds.ebrains.eu/core/License"
+    context = {"@vocab": "https://openminds.ebrains.eu/vocab/"}
+    schema_version = "v3.0"
+
+    properties = [
+        Property(
+            "full_name",
+            str,
+            "fullName",
+            formatting="text/plain",
+            required=True,
+            description="Whole, non-abbreviated name of the license.",
+            instructions="Enter the full name of this license.",
+        ),
+        Property(
+            "legal_code",
+            IRI,
+            "legalCode",
+            required=True,
+            description="Type of legislation that claims to cover the law system (complete or parts) as it existed at the time the code was enacted.",
+            instructions="Enter the internationalized resource identifier (IRI) to the legal code of this license.",
+        ),
+        Property(
+            "short_name",
+            str,
+            "shortName",
+            formatting="text/plain",
+            required=True,
+            description="Shortened or fully abbreviated name of the license.",
+            instructions="Enter a short name (or alias) for this license that could be used as a shortened display title (e.g., for web services with too little space to display the full name).",
+        ),
+        Property(
+            "webpages",
+            str,
+            "webpage",
+            multiple=True,
+            unique_items=True,
+            min_items=1,
+            formatting="text/plain",
+            description="Hypertext document (block of information) found on the World Wide Web.",
+            instructions="Enter the internationalized resource identifiers (IRIs) to webpages related to this license (e.g., a homepage).",
+        ),
+    ]
+
+    def __init__(self, id=None, full_name=None, legal_code=None, short_name=None, webpages=None):
+        return super().__init__(
+            id=id,
+            full_name=full_name,
+            legal_code=legal_code,
+            short_name=short_name,
+            webpages=webpages,
+        )
