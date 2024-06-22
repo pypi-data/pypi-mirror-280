2919cbd4c81b63fd87088602a13819790fec2178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:12 +0000

    Add package e_lims_cookiecutter_template/0.0.1/e_lims_cookiecutter_template-0.0.1-py3-none-any.whl

---
+++ packages/e_lims_cookiecutter_template/e_lims_cookiecutter_template-0.0.1-py3-none-any.whl/e_lims_cookiecutter_template/e_lims_cookiecutter_template.py
@@ -0,0 +1,10 @@
+"""e-lims-cookiecutter-template main."""
+
+
+def hello(name: str) -> str:
+    """Say hello.
+
+    >>> hello('e-lims-cookiecutter-template')
+    'Hello e-lims-cookiecutter-template!'
+    """
+    return f'Hello {name}!'
