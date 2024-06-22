ba51ee9dafcfd4ebbac422741369f37ec264ed09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:04:58 +0000

    Add package zz-pix/0.0.9/zz-pix-0.0.9.tar.gz

---
+++ packages/zz-pix/zz-pix-0.0.9.tar.gz/zz-pix-0.0.9/pix/utils.py
@@ -0,0 +1,29 @@
+import json
+import pathlib
+
+
+def get_supported_formats(str=False, transparent=False):
+
+    supported_formats = ["png", "jpeg", "webp", "avif", "heif", "tiff", "bmp", "ico"]
+    if str:
+        return ", ".join(supported_formats)
+    if transparent:
+        return ["png", "webp", "avif", "heif"]
+    else:
+        return supported_formats
+
+
+def is_file_supported(file):
+    supported_formats = get_supported_formats()
+    ext = pathlib.Path(file).suffix
+    ext = ext[1:]
+    if ext == "jpg":
+        ext = "jpeg"
+    return ext in supported_formats
+
+
+def get_manifest():
+    root_dir = pathlib.Path(__file__).parent.absolute()
+    manifest_path = pathlib.Path.joinpath(root_dir, "manifest.json")
+    with open(manifest_path, "r") as f:
+        return json.load(f)
---
+++ packages/zz-pix/zz-pix-0.0.9.tar.gz/zz-pix-0.0.9/setup.py
@@ -0,0 +1,54 @@
+import json
+
+from setuptools import find_packages, setup
+
+manifest = json.load(open("pix/manifest.json", "r"))
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+setup(
+    name=manifest["name"],
+    version=manifest["version"],
+    author=manifest["author"],
+    description=manifest["description"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=manifest["url"],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": [
+            "manifest.json",
+        ]
+    },
+    install_requires=[
+        "pillow",
+        "pillow-avif-plugin",
+        "pillow-heif",
+        "termcolor",
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pix = pix.main:main",
+        ],
+    },
+    python_requires=">=3.9",
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Multimedia :: Graphics :: Editors",
+        "Topic :: Multimedia :: Graphics :: Editors :: Raster-Based",
+    ],
+)
