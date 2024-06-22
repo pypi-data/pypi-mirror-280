ac38e533369c04c49e99bc94f6438b9fb5620033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 15:05:50 +0000

    Add package amtrak-analysis/0.5/amtrak_analysis-0.5.tar.gz

---
+++ packages/amtrak-analysis/amtrak_analysis-0.5.tar.gz/amtrak_analysis-0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amtrak_analysis',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'statsmodels',
+    ],
+    package_data={
+        'amtrak_analysis': ['data/*.csv'],
+    },
+    include_package_data=True,
+    description='A library for Amtrak data analysis and smoothing methods',
+    url='https://github.com/yourusername/amtrak_analysis',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
