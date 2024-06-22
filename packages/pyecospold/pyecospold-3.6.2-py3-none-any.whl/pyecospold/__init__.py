cce2fe89c6de5f9a2388637f9917e7281bacd4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 03:04:50 +0000

    Add package pyecospold/3.6.2/pyecospold-3.6.2-py3-none-any.whl

---
+++ packages/pyecospold/pyecospold-3.6.2-py3-none-any.whl/pyecospold/__init__.py
@@ -0,0 +1,37 @@
+__all__ = (
+    "__version__",
+    "parse_directory_v1",
+    "parse_directory_v2",
+    "parse_file_v1",
+    "parse_file_v2",
+    "parse_zip_file_v1",
+    "parse_zip_file_v2",
+    "validate_directory_v1",
+    "validate_directory_v2",
+    "validate_file_v1",
+    "validate_file_v2",
+    "validate_zip_file_v1",
+    "validate_zip_file_v2",
+    "save_ecospold_file",
+    "Defaults",
+)
+
+__version__ = "3.6.2"
+
+from .config import Defaults
+from .core import (
+    parse_directory_v1,
+    parse_directory_v2,
+    parse_file_v1,
+    parse_file_v2,
+    parse_zip_file_v1,
+    parse_zip_file_v2,
+    save_ecospold_file,
+    validate_directory_v1,
+    validate_directory_v2,
+    validate_file_v1,
+    validate_file_v2,
+    validate_zip_file_v1,
+    validate_zip_file_v2,
+)
+
