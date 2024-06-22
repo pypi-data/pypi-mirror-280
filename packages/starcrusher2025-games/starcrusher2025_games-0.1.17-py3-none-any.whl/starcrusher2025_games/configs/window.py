2f4197af027309153e39c943aaaa7b4a7a5297e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-22 06:09:16 +0000

    Add package starcrusher2025-games/0.1.17/starcrusher2025_games-0.1.17-py3-none-any.whl

---
+++ packages/starcrusher2025-games/starcrusher2025_games-0.1.17-py3-none-any.whl/starcrusher2025_games/configs/window.py
@@ -0,0 +1,45 @@
+import pygame
+
+class Window:
+    def __init__(self, width=800, height=600):
+        self._width = width
+        self._height = height
+        self.screen = pygame.display.set_mode((width, height))
+        self.background_color = (0, 0, 0)
+        self.is_fullscreen = False
+
+    def set_size(self, width, height):
+        self._width = width
+        self._height = height
+        self.screen = pygame.display.set_mode((width, height))
+
+    def set_bgc(self, r, g, b):
+        try:
+            r = int(r)
+            g = int(g)
+            b = int(b)
+
+            if 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255:
+                self.background_color = (r, g, b)
+                self.screen.fill(self.background_color)
+            else:
+                print("RGB values must be between 0 and 255.")
+        except ValueError:
+            print("Invalid RGB values. Please provide integers.")
+
+    def toggle_fullscreen(self):
+        self.is_fullscreen = not self.is_fullscreen
+        if self.is_fullscreen:
+            self.screen = pygame.display.set_mode((self._width, self._height), pygame.FULLSCREEN)
+        else:
+            self.screen = pygame.display.set_mode((self._width, self._height))
+        self.screen.fill(self.background_color)
+
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
