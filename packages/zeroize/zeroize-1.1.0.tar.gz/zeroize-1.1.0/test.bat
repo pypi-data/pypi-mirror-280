a63ceabec54c4ba2b477f01946381a4403c4cfac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:04:53 +0000

    Add package zeroize/1.1.0/zeroize-1.1.0.tar.gz

---
+++ packages/zeroize/zeroize-1.1.0.tar.gz/zeroize-1.1.0/test.bat
@@ -0,0 +1,7 @@
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+python examples/lock_and_zeroize.py
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+python examples/mlock.py
+if %errorlevel% neq 0 exit /b %errorlevel%
