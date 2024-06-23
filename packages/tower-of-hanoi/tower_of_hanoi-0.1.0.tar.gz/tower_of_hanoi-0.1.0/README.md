d06cdc5fdd690ad50d619629636a011e13817478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:04:54 +0000

    Add package tower-of-hanoi/0.1.0/tower_of_hanoi-0.1.0.tar.gz

---
+++ packages/tower-of-hanoi/tower_of_hanoi-0.1.0.tar.gz/tower_of_hanoi-0.1.0/README.md
@@ -0,0 +1,3 @@
+# tower_of_hanoi
+
+forget the old method of tower of hanoi code.using this package you can get the output in 3 line of code
---
+++ packages/tower-of-hanoi/tower_of_hanoi-0.1.0.tar.gz/tower_of_hanoi-0.1.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tower_of_hanoi",
+    version="0.1.0",
+    description="forget the old method of tower of hanoi code.using this package you can get the output in 3 line of code",
+    author="Adarsh",
+    author_email="adarshmsg11@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
