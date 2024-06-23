11efc9bdb35b1f22f7f5bd40081b34ccf6aa4170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:36 +0000

    Add package mkdocs-extract-listings-plugin/0.1.3/mkdocs_extract_listings_plugin-0.1.3-py3-none-any.whl

---
+++ packages/mkdocs-extract-listings-plugin/mkdocs_extract_listings_plugin-0.1.3-py3-none-any.whl/mkdocs_extract_listings_plugin/__init__.py
@@ -0,0 +1,60 @@
+# builtin
+import os
+# pip
+from mkdocs.config.config_options import Type, ListOfItems
+from mkdocs.config.base import Config
+from mkdocs.exceptions import PluginError
+from mkdocs.plugins import BasePlugin, get_plugin_logger
+from mkdocs.structure.pages import Page
+from mkdocs.config.defaults import MkDocsConfig
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+logger = get_plugin_logger(__name__)
+
+
+class ListingsConfig(Config):
+    listings_file = Type(str, default="")
+    placeholder = Type(str, default="PLACEHOLDER_LISTINGS_PLUGIN")
+    default_css = Type(bool, default=True)
+    offline = Type(bool, default=False)
+    javascript_search_file = Type(str, default="")
+    exclude_language_list = ListOfItems(Type(str), default=[])
+
+
+# local
+from .page_processor import PageProcessor
+from .all_listings_page import update_all_listings_page
+from .search_page import write_javascript_file
+
+
+class ListingsPlugin(BasePlugin[ListingsConfig]):
+    def on_config(self, config: MkDocsConfig) -> None:
+        self.page_processor = PageProcessor(self.config)
+
+
+    def on_pre_build(self, config: MkDocsConfig) -> None:
+        # Reset before every build -> prevent duplicate entries when running mkdocs serve
+        self.page_processor.clear()
+
+        if self.config.listings_file:
+            if os.path.isabs(self.config.listings_file):
+                raise PluginError(f"'listings_file' can not be an absolute path: ${self.config.listings_file}")
+
+            listings_file = os.path.join(config.docs_dir, self.config.listings_file)
+            if not os.path.isfile(listings_file):
+                raise PluginError(f"'listings_file' does not reference a valid Markdown file: '{listings_file}' does not exist")
+            elif not self.config.listings_file.endswith(".md"):
+                logger.warning(f"Value for 'listings_file' should probably end in '.md', but is '{self.config.listings_file}'")
+        else:
+            if not self.config.javascript_search_file:
+                logger.warning("Neither 'javascript_search_file' nor 'listings_file' are set -> This plugin will do nothing. Please check the setup instructions at https://github.com/six-two/mkdocs-extract-listings-plugin/blob/main/README.md")
+
+    # https://www.mkdocs.org/dev-guide/plugins/#on_page_content
+    def on_page_content(self, html: str, page: Page, config: MkDocsConfig, files) -> None:
+        self.page_processor.process_page(html, page)
+
+    def on_post_build(self, config: MkDocsConfig) -> None:
+        update_all_listings_page(self.page_processor.page_data_list, self.config, config)
+
+        if self.config.javascript_search_file:
+            write_javascript_file(self.page_processor.page_data_list, self.config, config)
