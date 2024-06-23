4ce682b35503b0b0886165041b00f61f54e77a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 14:04:34 +0000

    Add package ezbolt/0.1.0/ezbolt-0.1.0.tar.gz

---
+++ packages/ezbolt/ezbolt-0.1.0.tar.gz/ezbolt-0.1.0/main.py
@@ -0,0 +1,21 @@
+import ezbolt
+
+# initialize bolt group
+bolt_group = ezbolt.boltgroup.BoltGroup()
+
+# add bolts    
+bolt_group.add_bolts(xo=0, yo=0, width=6, height=6, nx=3, ny=3)
+
+# preview geometry
+ezbolt.plotter.preview(bolt_group)
+
+# calculate bolt force with elastic method
+results = bolt_group.solve(Vx=0, Vy=-50, torsion=-200)
+
+# plot bolt forces
+ezbolt.plotter.plot_elastic(bolt_group)
+ezbolt.plotter.plot_ECR(bolt_group)
+ezbolt.plotter.plot_ICR(bolt_group)
+
+
+
