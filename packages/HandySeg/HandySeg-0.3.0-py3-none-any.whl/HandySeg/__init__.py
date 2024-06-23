1daea1a27a112b59cbadb3ade387a3bf879561e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:53 +0000

    Add package HandySeg/0.3.0/HandySeg-0.3.0-py3-none-any.whl

---
+++ packages/HandySeg/HandySeg-0.3.0-py3-none-any.whl/HandySeg/__init__.py
@@ -0,0 +1,3 @@
+from HandySeg.line_segmentation import line_segmenation
+from HandySeg.word_segmentaiton import WordSegmentation
+from HandySeg.preprocessing import convert2binary, find_pp, gray, cut_blank_header_footer, first_and_last_line, segmented_lines, mark_horizontal_seam, segmented_words
