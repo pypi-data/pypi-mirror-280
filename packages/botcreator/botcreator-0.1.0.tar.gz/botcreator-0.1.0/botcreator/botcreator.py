df96391d55dfe56c5924bcd3369ada0a08e0885d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:52 +0000

    Add package botcreator/0.1.0/botcreator-0.1.0.tar.gz

---
+++ packages/botcreator/botcreator-0.1.0.tar.gz/botcreator-0.1.0/botcreator/botcreator.py
@@ -0,0 +1,64 @@
+import random
+import re
+
+def botresponse(prompt, filename):
+    exact_matches = []
+    condition_matches = []
+
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            if '/' not in line:
+                continue
+            
+            prompt_part, response_part = line.strip().split('/')
+            responses = response_part.split('#')
+            
+            if '#' not in prompt_part and '&' not in prompt_part:
+                exact_matches.append((prompt_part, responses))
+            else:
+                prompt_conditions = prompt_part.split('#')
+                condition_matches.append((prompt_conditions, responses))
+    
+    for exact_prompt, responses in exact_matches:
+        if exact_prompt == prompt:
+            return random.choice(responses)
+    
+    for prompt_conditions, responses in condition_matches:
+        for condition in prompt_conditions:
+            sub_conditions = condition.split('&')
+            if all(sub_condition.strip() in prompt for sub_condition in sub_conditions):
+                return random.choice(responses)
+    
+    for exact_prompt, responses in exact_matches:
+        if exact_prompt in prompt:
+            return random.choice(responses)
+        
+    # Check for * (startswith) and ** (endswith) conditions
+    for exact_prompt, responses in exact_matches:
+        if exact_prompt.startswith('*'):
+            if prompt.startswith(exact_prompt[1:]):
+                return random.choice(responses)
+        elif exact_prompt.startswith('**'):
+            if prompt.endswith(exact_prompt[2:]):
+                return random.choice(responses)
+        elif exact_prompt.startswith('regex(') and exact_prompt.endswith(')'):
+            regex_pattern = exact_prompt[len('regex('):-1]
+            if re.match(regex_pattern, prompt):
+                return random.choice(responses)
+    
+    for prompt_conditions, responses in condition_matches:
+        for condition in prompt_conditions:
+            if condition.startswith('*'):
+                if prompt.startswith(condition[1:]):
+                    return random.choice(responses)
+            elif condition.startswith('**'):
+                if prompt.endswith(condition[2:]):
+                    return random.choice(responses)
+            elif condition.startswith('regex(') and condition.endswith(')'):
+                regex_pattern = condition[len('regex('):-1]
+                if re.match(regex_pattern, prompt):
+                    return random.choice(responses)
+    
+    return None
---
+++ packages/botcreator/botcreator-0.1.0.tar.gz/botcreator-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Easy chatbot development with Python. Create your own chatbots with just a few lines of code.'
+# Setting up
+setup(
+    name="botcreator",
+    version=VERSION,
+    author="EdexCode",
+    author_email="edexcode@gmail.com",
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "GitHub": "https://github.com/EdexCode/BotCreator"
+    },
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'bot', 'chatbot', 'botdeveloper', 'predefined'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
