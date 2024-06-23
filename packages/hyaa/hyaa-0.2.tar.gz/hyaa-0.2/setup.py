266e9fd9dca3bc30f3539c25459a9bcceebb6f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:15 +0000

    Add package hyaa/0.2/hyaa-0.2.tar.gz

---
+++ packages/hyaa/hyaa-0.2.tar.gz/hyaa-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="hyaa",
+    version="0.2",
+    py_modules=["hyaa"],
+    install_requires=[
+        "Click",
+        "requests",
+    ],
+    entry_points="""
+        [console_scripts]
+        hyaa=hyaa.main:random_quote
+    """,
+)
