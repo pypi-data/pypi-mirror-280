a7f8cfe07bffc27bcb820e077a8551ccf62770b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:23 +0000

    Add package homematicip/1.1.0.post1.dev52/homematicip-1.1.0.post1.dev52-py3-none-any.whl

---
+++ packages/homematicip/homematicip-1.1.0.post1.dev52-py3-none-any.whl/homematicip/configuration/config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+import logging
+from typing import List
+
+
+@dataclass
+class PersistentConfig:
+
+    level: int = logging.INFO
+    accesspoint_id: str = None
+    auth_token: str = None
+    log_file: str = ""
+
+
+@dataclass
+class Config(PersistentConfig):
+    lookup_url: str = "https://lookup.homematic.com:48335/getHost"
+
+    @classmethod
+    def from_persistent_config(cls, persistent_config: PersistentConfig):
+        return cls(level=persistent_config.level,
+                   accesspoint_id=persistent_config.accesspoint_id,
+                   auth_token=persistent_config.auth_token,
+                   log_file=persistent_config.log_file)
