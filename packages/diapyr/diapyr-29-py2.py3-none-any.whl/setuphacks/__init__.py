0a0014ffe8373226ddf2c7f0af0371001d6a8b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:04:41 +0000

    Add package diapyr/29/diapyr-29-py2.py3-none-any.whl

---
+++ packages/diapyr/diapyr-29-py2.py3-none-any.whl/setuphacks/__init__.py
@@ -0,0 +1,13 @@
+from . import fakesetup
+from traceback import format_exception_only
+import os, subprocess, sys
+
+class SetupException(Exception): pass
+
+def getsetupkwargs(setuppath, fields):
+    cwd, = (dict(cwd = d) if d else {} for d in [os.path.dirname(setuppath)])
+    setupkwargs = eval(subprocess.check_output([sys.executable, fakesetup.__file__, os.path.basename(setuppath)] + fields, **cwd))
+    if isinstance(setupkwargs, BaseException):
+        # Can't simply propagate SystemExit for example:
+        raise SetupException(format_exception_only(setupkwargs.__class__, setupkwargs)[-1].rstrip())
+    return setupkwargs
