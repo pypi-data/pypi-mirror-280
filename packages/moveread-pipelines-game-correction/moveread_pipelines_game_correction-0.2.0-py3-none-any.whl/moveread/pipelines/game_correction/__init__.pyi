ced17b71ab6e3580a2824f13615b41606cd5fc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:22 +0000

    Add package moveread-pipelines-game-correction/0.2.0/moveread_pipelines_game_correction-0.2.0-py3-none-any.whl

---
+++ packages/moveread-pipelines-game-correction/moveread_pipelines_game_correction-0.2.0-py3-none-any.whl/moveread/pipelines/game_correction/__init__.pyi
@@ -0,0 +1,6 @@
+from .spec import Input, Output, GameCorrection
+from ._types import CorrectResult, BadlyPreprocessed
+
+__all__ = [
+  'Input', 'Output', 'GameCorrection', 'CorrectResult', 'BadlyPreprocessed',
+]
