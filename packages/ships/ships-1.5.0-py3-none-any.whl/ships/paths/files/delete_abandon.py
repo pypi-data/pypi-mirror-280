9d201c125b754286c83b9030be16f05442fa7d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:04:47 +0000

    Add package ships/1.5.0/ships-1.5.0-py3-none-any.whl

---
+++ packages/ships/ships-1.5.0-py3-none-any.whl/ships/paths/files/delete_abandon.py
@@ -0,0 +1,19 @@
+
+
+
+
+'''
+	from ships.paths.files.delete_abandon import delete_abandon_file
+	delete_abandon_file ()
+'''
+
+import os
+
+def delete_abandon_file (file_path):
+	#print ('abandoning file:', file_path)
+
+	if os.path.exists (file_path):
+		os.remove (file_path)
+		return;
+		
+	raise Exception (f"file at path '{ file_path }' was not found.")
