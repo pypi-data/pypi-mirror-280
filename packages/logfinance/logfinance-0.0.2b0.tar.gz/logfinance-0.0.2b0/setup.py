5230b484cd6fb90098d6903ce102b68230d1a384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:04:42 +0000

    Add package logfinance/0.0.2b0/logfinance-0.0.2b0.tar.gz

---
+++ packages/logfinance/logfinance-0.0.2b0.tar.gz/logfinance-0.0.2b0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logfinance',
+    version='0.0.2-beta',
+    packages=find_packages(),
+    install_requires=[],
+    author='Erfan Varedi',
+    author_email='logfinanceresearch@gmail.com',
+    description='A simple library to calculate whatever for financial applications',
+    keywords='finance',
+    url='https://github.com/logfinance/python-logfinance',
+)
