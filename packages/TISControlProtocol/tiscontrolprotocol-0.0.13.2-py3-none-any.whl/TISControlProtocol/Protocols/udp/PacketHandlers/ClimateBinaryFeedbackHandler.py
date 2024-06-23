fb070cf9f57ba90edc3a0467a84f84073e00b4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:33 +0000

    Add package TISControlProtocol/0.0.13.2/tiscontrolprotocol-0.0.13.2-py3-none-any.whl

---
+++ packages/TISControlProtocol/tiscontrolprotocol-0.0.13.2-py3-none-any.whl/TISControlProtocol/Protocols/udp/PacketHandlers/ClimateBinaryFeedbackHandler.py
@@ -0,0 +1,21 @@
+from homeassistant.core import HomeAssistant
+import logging
+
+AC_NUMBER_MAP = {0x19: 0, 0x1A: 1, 0x1B: 2, 0x1C: 3, 0x1D: 4, 0x1E: 5, 0x1F: 6, 0x20: 7}
+
+
+async def handle_climate_binary_feedback(hass: HomeAssistant, info: dict):
+    ac_number = AC_NUMBER_MAP[int(info["additional_bytes"][0])]
+    sub_operation = info["additional_bytes"][1]
+    operation_value = info["additional_bytes"][2]
+
+    event_data = {
+        "device_id": info["device_id"],
+        "sub_operation": sub_operation,
+        "operation_value": operation_value,
+    }
+    try:
+        hass.bus.async_fire(f"tis_ac_{ac_number}", event_data)
+        logging.error(f"ac binary feedback event: {event_data}")
+    except Exception as e:
+        logging.error(f"error in firing event: {e}")
