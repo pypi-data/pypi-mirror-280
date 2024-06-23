d858eadb06f5a1d9f03c5de0fac5cb42c2d823bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:48 +0000

    Add package stashconnect/0.9.65/stashconnect-0.9.65.tar.gz

---
+++ packages/stashconnect/stashconnect-0.9.65.tar.gz/stashconnect-0.9.65/stashconnect/__init__.py
@@ -0,0 +1,12 @@
+"""
+StashConnect
+~~~~~~~~~~~~
+An API wrapper for [Stashcat](https://stashcat.com/en).
+
+:copyright: (c) 2024-present BuStudios
+:license: MIT, see LICENSE.
+"""
+
+__version__ = "0.9.65"
+
+from .client import *
