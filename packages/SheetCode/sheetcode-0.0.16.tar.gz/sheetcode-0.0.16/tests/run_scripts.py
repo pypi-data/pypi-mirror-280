295d99b2f4a2e311921e8034e31cd6a79272c696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:53 +0000

    Add package SheetCode/0.0.16/sheetcode-0.0.16.tar.gz

---
+++ packages/SheetCode/sheetcode-0.0.16.tar.gz/sheetcode-0.0.16/tests/run_scripts.py
@@ -0,0 +1,18 @@
+import sys;sys.path.append(".")
+from SheetCode import Scripts
+
+Scripts.Configure(specId = "FCT",
+                  specVersion = "1A",
+                  scriptsPath = "tests/scripts",
+                  sheetsPath = "tests/sheets",
+                  traceabilityPath = "tests/traceability",
+                  summaryPath = "tests/summary",
+                  rvtFilepath = "../../../../DAA-000048 RVT/2A - Ongoing/DAA-000048_2A.xlsm",
+                  parametersFilepath = "../Traceability/A-0000175109 1B App A_Traceability_RBC_9.4.0.xlsm",
+                  mode = Scripts.Mode.Execution)
+
+#Scripts.Run(["Example1", "Example2"])
+
+Scripts.RunAll()
+
+
