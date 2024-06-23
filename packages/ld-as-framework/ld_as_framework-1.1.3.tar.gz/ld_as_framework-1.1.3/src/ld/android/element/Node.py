1f2a6bcaf357e82d64d4d66165e4eb13b8acb2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:57 +0000

    Add package ld-as-framework/1.1.3/ld_as_framework-1.1.3.tar.gz

---
+++ packages/ld-as-framework/ld_as_framework-1.1.3.tar.gz/ld_as_framework-1.1.3/src/ld/android/element/Node.py
@@ -0,0 +1,21 @@
+# encoding:utf-8
+from ascript.android.node import Selector
+
+from ...common.Logger import log_ld
+
+from ..base.BaseProperties import AScriptQueryElement, Rect, CommonResult
+
+class NodeQuery(Selector, AScriptQueryElement):
+
+    """
+    节点查询对象
+    """
+    def _build_result(node_result):
+        pass
+
+    def _find_element(self, eleName):
+        pass
+
+    def _find_all_element(self, eleName):
+        pass
+
