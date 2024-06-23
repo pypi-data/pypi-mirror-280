3db0c3549f2866e6ee8dd9bca69ede8b3d635add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:40 +0000

    Add package oak-cli/0.3.0/oak_cli-0.3.0-py3-none-any.whl

---
+++ packages/oak-cli/oak_cli-0.3.0-py3-none-any.whl/oak_cli/ansible/oakestra_automations_submodule/ansible/roles/gantsign.ansible-role-golang/molecule/default/tests/test_role.py
@@ -0,0 +1,28 @@
+import re
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "name,pattern",
+    [
+        ("GOROOT", "^/opt/go/1.21.3$"),
+        ("GOPATH", "^/root/workspace-go$"),
+        ("PATH", "^(.+:)?/opt/go/1.21.3/bin(:.+)?$"),
+        ("PATH", "^(.+:)?/root/workspace-go/bin(:.+)?$"),
+    ],
+)
+def test_go_env(host, name, pattern):
+    cmd = host.run(". /etc/profile && printf $" + name)
+    assert re.search(pattern, cmd.stdout)
+
+
+def test_go(host):
+    cmd = host.run(". /etc/profile && go version")
+    assert cmd.rc == 0
+
+
+@pytest.mark.parametrize("command", ["gofmt"])
+def test_go_tools(host, command):
+    cmd = host.run(". /etc/profile && which " + command)
+    assert cmd.rc == 0
