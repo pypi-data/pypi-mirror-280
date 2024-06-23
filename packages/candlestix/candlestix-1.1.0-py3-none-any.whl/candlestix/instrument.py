696a48e5327a2f178ed8be91393a7478551af62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:01 +0000

    Add package candlestix/1.1.0/candlestix-1.1.0-py3-none-any.whl

---
+++ packages/candlestix/candlestix-1.1.0-py3-none-any.whl/candlestix/instrument.py
@@ -0,0 +1,26 @@
+import json
+
+from candlestix.candleconf import Exchange
+
+
+class Instrument:
+    def __init__(self,
+                 symbol: str,
+                 instrument_key: str,
+                 name: str,
+                 exchange: Exchange):
+
+        self.instrument_key = instrument_key
+        self.symbol = symbol
+        self.exchange = exchange
+        self.name = name
+
+    @staticmethod
+    def get_exchange_from_val(val: str):
+        for v in Exchange.__members__.values():
+            if val.upper() == v.value:
+                return v
+        return None
+
+    def __repr__(self):
+        return None  #TODO
