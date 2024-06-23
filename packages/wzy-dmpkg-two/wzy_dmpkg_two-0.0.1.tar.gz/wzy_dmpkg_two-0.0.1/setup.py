18514ba88642da234d547dafb284e3c25fea6461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:28 +0000

    Add package wzy-dmpkg-two/0.0.1/wzy_dmpkg_two-0.0.1.tar.gz

---
+++ packages/wzy-dmpkg-two/wzy_dmpkg_two-0.0.1.tar.gz/wzy_dmpkg_two-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wzy_dmpkg_two',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'math'
+    ],
+    description='Any description you can put',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Dr. Zhiyuan Wang',
+    author_email='wang1399@e.ntu.edu.sg',
+    url='https://github.com/Dr-WangZhiyuan/wzy_dmpkg_two',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
