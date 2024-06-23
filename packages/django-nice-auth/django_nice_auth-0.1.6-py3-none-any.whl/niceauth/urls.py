cc4efbd77524398a582d5d6569a8aab1186cf5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:53 +0000

    Add package django-nice-auth/0.1.6/django_nice_auth-0.1.6-py3-none-any.whl

---
+++ packages/django-nice-auth/django_nice_auth-0.1.6-py3-none-any.whl/niceauth/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import GetNiceAuthDataView, GetNiceAuthUrlView, VerifyNiceAuthView, NiceAuthRequestRetrieveView, NiceAuthResultRetrieveView
+
+urlpatterns = [
+    path('data/', GetNiceAuthDataView.as_view(), name='get_nice_auth_data'),
+    path('url/', GetNiceAuthUrlView.as_view(), name='get_nice_auth_url'),
+    path('verify/', VerifyNiceAuthView.as_view(), name='verify_nice_auth'),
+    path('request/<uuid:request_no>/', NiceAuthRequestRetrieveView.as_view(), name='retrieve_nice_auth_request'),
+    path('result/<uuid:request_no>/', NiceAuthResultRetrieveView.as_view(), name='retrieve_nice_auth_result'),
+]
