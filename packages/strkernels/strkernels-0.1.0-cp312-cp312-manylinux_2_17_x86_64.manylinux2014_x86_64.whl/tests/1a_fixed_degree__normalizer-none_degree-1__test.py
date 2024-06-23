7f1e928b6ec73258703999ae6596dff3e53be529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:04:57 +0000

    Add package strkernels/0.1.0/strkernels-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/strkernels/strkernels-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tests/1a_fixed_degree__normalizer-none_degree-1__test.py
@@ -0,0 +1,57 @@
+
+# Author: Denilson Fagundes Barbosa, denilsonfbar@gmail.com
+
+
+import unittest
+import numpy as np
+
+# kernel class import
+from sys import path
+path.append('..')
+from strkernels import FixedDegreeStringKernel
+
+
+class TestKernelMatrix(unittest.TestCase):
+
+    def setUp(self):
+        self.kernel = FixedDegreeStringKernel(normalizer=None, degree=1)
+
+    def test_empty_strings(self):
+        strings = np.array(["", "", ""])
+        expected_matrix = np.zeros((3, 3))
+        kernel_matrix = self.kernel(strings, strings)
+        np.testing.assert_array_equal(kernel_matrix, expected_matrix)
+
+    def test_single_character_strings(self):
+        strings = np.array(["A", "T", "A"])
+        expected_matrix = np.array([
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1]
+        ])
+        kernel_matrix = self.kernel(strings, strings)
+        np.testing.assert_array_equal(kernel_matrix, expected_matrix)
+
+    def test_single_set(self):
+        strings = np.array(["ATCG", "ATGG", "TACG", "GCTA"])
+        expected_matrix = np.array([
+            [4, 3, 2, 0],
+            [3, 4, 1, 0],
+            [2, 1, 4, 0],
+            [0, 0, 0, 4]
+        ])
+        kernel_matrix = self.kernel(strings, strings)
+        np.testing.assert_array_equal(kernel_matrix, expected_matrix)
+
+    def test_two_sets(self):
+        strings1 = np.array(["ATCG", "ATGG"])
+        strings2 = np.array(["ATCG", "GGGG", "CCCC"])
+        expected_matrix = np.array([
+            [4, 1, 1],
+            [3, 2, 0]
+        ])
+        kernel_matrix = self.kernel(strings1, strings2)
+        np.testing.assert_array_equal(kernel_matrix, expected_matrix)
+
+if __name__ == '__main__':
+    unittest.main()
