0c8c8c77585347bac9cc55c209087f0854a3ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:39 +0000

    Add package octoflow/0.1.16/octoflow-0.1.16-py3-none-any.whl

---
+++ packages/octoflow/octoflow-0.1.16-py3-none-any.whl/octoflow/__init__.py
@@ -0,0 +1,29 @@
+import contextlib
+
+from octoflow import logging
+from octoflow.config import config
+from octoflow.core import Module, Task
+from octoflow.utils.config import Config
+
+__version__ = "0.1.16"
+
+__all__ = [
+    "Config",
+    "Module",
+    "Task",
+    "config",
+    "logger",
+    "logging",
+]
+
+logging.logger = logging.get_logger(
+    name="octoflow",
+    level=config.logging.level,
+    formatter=config.logging.format,
+)
+
+with contextlib.suppress(ImportError):
+    from octoflow_plugins import package as default_plugins_package
+
+if "default_plugins_package" in locals():
+    default_plugins_package.import_modules()
