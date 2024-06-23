5cdb246ef04f8a54b9b2a588df010398bb6deabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 14:04:29 +0000

    Add package js-go/0.1.0/js_go-0.1.0.tar.gz

---
+++ packages/js-go/js_go-0.1.0.tar.gz/js_go-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='js-go',
+    version='0.1.0',
+    packages=find_packages(),
+    author='JS',
+    author_email='polarbear13@naver.com',
+    description='A Python implementation of the Go board game. This project includes game logic, rules enforcement, and unit tests to ensure correctness.',
+    url='https://github.com/orca0613/go-game',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT'
+)
