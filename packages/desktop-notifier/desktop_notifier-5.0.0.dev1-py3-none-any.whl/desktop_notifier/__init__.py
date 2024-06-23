6fe67e4a0747e3351187cddc5dceaf3a59ce606a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:28 +0000

    Add package desktop-notifier/5.0.0.dev1/desktop_notifier-5.0.0.dev1-py3-none-any.whl

---
+++ packages/desktop-notifier/desktop_notifier-5.0.0.dev1-py3-none-any.whl/desktop_notifier/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Desktop notifications for Windows, Linux, macOS, iOS and iPadOS.
+"""
+from .main import (
+    DesktopNotifier,
+    Button,
+    ReplyField,
+    Notification,
+    Urgency,
+    Capability,
+    Icon,
+    Sound,
+    Attachment,
+    DEFAULT_SOUND,
+    DEFAULT_ICON,
+)
+
+
+__version__ = "5.0.0.dev1"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/samschott/desktop-notifier"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__url__",
+    "Notification",
+    "Button",
+    "ReplyField",
+    "Urgency",
+    "Icon",
+    "Sound",
+    "Attachment",
+    "DesktopNotifier",
+    "Capability",
+    "DEFAULT_SOUND",
+    "DEFAULT_ICON",
+]
