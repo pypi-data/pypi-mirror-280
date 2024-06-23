199443ce05c9c424be9def47af1466c292d0696d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:33 +0000

    Add package modelmachine/0.1.10/modelmachine-0.1.10-py3-none-any.whl

---
+++ packages/modelmachine/modelmachine-0.1.10-py3-none-any.whl/modelmachine/__main__.py
@@ -0,0 +1,73 @@
+"""Modelmachine - model machine emulator."""
+
+import argparse
+import sys
+
+from modelmachine.__about__ import __version__
+from modelmachine.ide import assemble, debug, get_program
+
+
+def run_program(args):
+    """Get params from args and run file."""
+    cpu = get_program(args.filename, args.protect_memory)
+    cpu.run()
+
+
+def run_debug(args):
+    """Get params from args and run debug."""
+    cpu = get_program(args.filename, args.protect_memory)
+    debug(cpu)
+
+
+def run_asm(args):
+    """Get params from args and run assembler."""
+    assemble(args.asm_file, args.machine_file)
+
+
+def main(argv, stdout):
+    """Execute, when user call modelmachine."""
+    parser = argparse.ArgumentParser(description="Modelmachine " + __version__)
+
+    parser.add_argument(
+        "-m",
+        "--protect_memory",
+        action="store_true",
+        default=False,
+        help="raise an error, if program tries to read dirty memory",
+    )
+    subparsers = parser.add_subparsers(
+        title="commands", help="commands of model machine emulator"
+    )
+
+    run = subparsers.add_parser("run", help="run program")
+    run.add_argument("filename", help="file containing machine code")
+    run.set_defaults(func=run_program)
+
+    debug_parser = subparsers.add_parser(
+        "debug", help="run program in debug mode"
+    )
+    debug_parser.add_argument("filename", help="file containing machine code")
+    debug_parser.set_defaults(func=run_debug)
+
+    asm = subparsers.add_parser("asm", help="assemble model machine program")
+    asm.add_argument("asm_file", help="input file containing asm source")
+    asm.add_argument(
+        "machine_file", help="output file containing machine code"
+    )
+    asm.set_defaults(func=run_asm)
+
+    args = parser.parse_args(argv[1:])
+
+    if "func" not in args:
+        parser.print_help(stdout)
+    else:
+        args.func(args)
+
+
+def exec_main():
+    """Hook for testability."""
+    main(sys.argv, sys.stdout)
+
+
+if __name__ == "__main__":
+    exec_main()
---
+++ packages/modelmachine/modelmachine-0.1.10-py3-none-any.whl/modelmachine/memory.py
@@ -0,0 +1,252 @@
+"""Classes for memory amulation.
+
+Word is long integer.
+"""
+
+import warnings
+
+
+def big_endian_decode(array, word_size):
+    """Transform array of words to one integer."""
+    result = 0
+    for val in array:
+        result *= 2**word_size
+        result += val
+    return result
+
+
+def little_endian_decode(array, word_size):
+    """Transform array of words to one integer."""
+    return big_endian_decode(reversed(array), word_size)
+
+
+def little_endian_encode(value, word_size, bits):
+    """Transform long integer to list of small."""
+    copy_value = value
+    size = bits // word_size
+    if value < 0:
+        msg = f"Cannot encode negative value: {value}"
+        raise ValueError(msg)
+    if value == 0:
+        return [0] * size
+
+    result = []
+    while value != 0:
+        result.append(value % 2**word_size)
+        value //= 2**word_size
+
+    if len(result) * word_size > bits:
+        msg = (
+            f"Integer is too long: {copy_value}, expected "
+            f"{bits} bits integer"
+        )
+        raise ValueError(msg)
+    result += [0] * (size - len(result))
+    return result
+
+
+def big_endian_encode(value, word_size, bits):
+    """Transform long integer to list of small."""
+    return list(reversed(little_endian_encode(value, word_size, bits)))
+
+
+class AbstractMemory(dict):
+    """Class from which inherits concrete memory."""
+
+    word_size = None
+
+    def __init__(self, *, word_size, endianess="big", addresses=None):
+        """Define concrete memory with the word size."""
+        if addresses is None:
+            addresses = {}
+
+        super().__init__(addresses)
+        self.word_size = word_size
+        self.access_count = 0
+
+        if endianess == "big":
+            self.decode, self.encode = big_endian_decode, big_endian_encode
+        elif endianess == "little":
+            self.decode, self.encode = (
+                little_endian_decode,
+                little_endian_encode,
+            )
+        else:
+            msg = f"Unexpected endianess: {endianess}"
+            raise ValueError(msg)
+
+    def check_word_size(self, word):
+        """Check that value can be represented by word with the size."""
+        if not 0 <= word < 2**self.word_size:
+            msg = (
+                f"Wrong word format: {word}, "
+                f"should be 0 <= word < {2 ** self.word_size}"
+            )
+            raise ValueError(msg)
+
+    def __setitem__(self, address, word):
+        """Raise an error, if word has wrong format."""
+        self.check_word_size(word)
+        self.check_address(address)
+        super().__setitem__(address, word)
+
+    def __getitem__(self, address):
+        """Return word."""
+        self.check_address(address)
+        return super().__getitem__(address)
+
+    def check_address(self, address):
+        """Should raise an exception if address is invalid."""
+        raise NotImplementedError
+
+    def check_bits_count(self, _address, bits):
+        """Check that we want to read integer count of words."""
+        if bits % self.word_size != 0:
+            msg = (
+                "Cannot operate with non-integer word counter: "
+                f"needs {bits} bits, but word size is {self.word_size}"
+            )
+            raise KeyError(msg)
+
+    def fetch(self, address, bits):
+        """Load bits by address.
+
+        Size must be divisible by self.word_size.
+        """
+        self.check_address(address)
+        self.check_bits_count(address, bits)
+
+        self.access_count += 1
+
+        size = bits // self.word_size
+        if size == 1:  # Address not always is integer, sometimes string
+            return self[address]
+
+        return self.decode(
+            [self[i] for i in range(address, address + size)], self.word_size
+        )
+
+    def put(self, address, value, bits):
+        """Put size bits by address.
+
+        Size must be divisible by self.word_size.
+        """
+        self.check_address(address)
+        self.check_bits_count(address, bits)
+
+        enc_value = self.encode(value, self.word_size, bits)
+
+        self.access_count += 1
+
+        size = bits // self.word_size
+        if size == 1:  # Address not always is integer, sometimes string
+            self[address] = value
+        else:
+            for i in range(size):
+                self[address + i] = enc_value[i]
+
+
+class RandomAccessMemory(AbstractMemory):
+    """Random access memory.
+
+    Addresses is x: 0 <= x < memory_size.
+    If is_protected == True, you cannot read unassigned memory
+    (useful for debug).
+    """
+
+    def __init__(
+        self,
+        *,
+        word_size,
+        memory_size,
+        endianess="big",
+        is_protected=True,
+        addresses=None,
+    ):
+        """Read help(type(x))."""
+        super().__init__(
+            word_size=word_size, endianess=endianess, addresses=addresses
+        )
+        self.memory_size = memory_size
+        self.is_protected = is_protected
+
+    def __len__(self):
+        """Return size of memory in unified form."""
+        return self.memory_size
+
+    def check_address(self, address):
+        """Check that adress is valid."""
+        if not isinstance(address, int):
+            msg = f"Address should be int, not {type(address)!s}"
+            raise TypeError(msg)
+        if not 0 <= address < len(self):
+            msg = (
+                f"Invalid address {hex(address)}, "
+                f"should be 0 <= address < {len(self)}"
+            )
+            raise KeyError(msg)
+
+    def __missing__(self, address):
+        """If addressed memory not defined."""
+        self.check_address(address)
+        if self.is_protected:
+            msg = (
+                f"Cannot read memory by address: {hex(address)}, "
+                "it is dirty memory, clean it first"
+            )
+            raise KeyError(msg)
+
+        warnings.warn(
+            f"Read memory by address: {hex(address)}, "
+            "it is dirty memory, clean it first",
+            stacklevel=4,
+        )
+        return 0
+
+
+class RegisterMemory(AbstractMemory):
+    """Registers."""
+
+    def __init__(self, **kvargs):
+        """List of addresses are required."""
+        super().__init__(word_size=0, **kvargs)  # There is dynamic
+        # word size
+        self.register_sizes = {}
+
+    def add_register(self, name, register_size):
+        """Add register with specific size.
+
+        Raise an key error if register with this name already exists and
+        have another size.
+        """
+        if (
+            name in self.register_sizes
+            and self.register_sizes[name] != register_size
+        ):
+            msg = (
+                f"Cannot add register with name `{name}` and size "
+                f"`{register_size}`, register with this name and "
+                f"`{self.register_sizes[name]}` size already exists"
+            )
+            raise KeyError(msg)
+
+        if name not in self.register_sizes:
+            self.register_sizes[name] = register_size
+            self[name] = 0
+
+    def check_address(self, name):
+        """Check that we have the register."""
+        if name not in self.register_sizes:
+            msg = f"Invalid register name: {name}"
+            raise KeyError(msg)
+
+    def check_bits_count(self, name, size):
+        """Bit count must be equal to word_size."""
+        if size != self.register_sizes[name]:
+            msg = (
+                f"Invalid register {name} size: {size}."
+                f" Should be {self.register_sizes[name]}"
+            )
+            raise KeyError(msg)
+
+        self.word_size = size
