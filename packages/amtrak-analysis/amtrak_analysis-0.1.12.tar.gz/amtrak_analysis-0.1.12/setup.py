299706a9069e46c1afac4d6fa975d08b7ff37f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:34 +0000

    Add package amtrak-analysis/0.1.12/amtrak_analysis-0.1.12.tar.gz

---
+++ packages/amtrak-analysis/amtrak_analysis-0.1.12.tar.gz/amtrak_analysis-0.1.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amtrak_analysis',
+    version='0.1.12',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'amtrak_analysis': ['data/Amtrak.csv']},
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'statsmodels',
+    ],
+    description='A library for analyzing Amtrak ridership data',
+)
