4232de1c80d07ed0ec3705038e87f66703989b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:05:05 +0000

    Add package odoo14-addon-ssi-assurance-report-quality-control/14.0.1.0.1.dev2/odoo14_addon_ssi_assurance_report_quality_control-14.0.1.0.1.dev2-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-assurance-report-quality-control/odoo14_addon_ssi_assurance_report_quality_control-14.0.1.0.1.dev2-py3-none-any.whl/odoo/addons/ssi_assurance_report_quality_control/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Assurance Report - Quality Control Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_accountant_report",
+        "ssi_quality_control",
+    ],
+    "data": [],
+}
