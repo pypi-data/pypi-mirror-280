a7eccbf9bc0b757401b729ae7fb3194db00fadae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:25 +0000

    Add package python-iArt/1.1.3/python-iArt-1.1.3.tar.gz

---
+++ packages/python-iArt/python-iArt-1.1.3.tar.gz/python-iArt-1.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python-iArt",
+    version="1.1.3",
+    author="Siyu Heng, Jiawei Zhang, and Yang Feng",
+    author_email="siyuheng@nyu.edu,jz4721@nyu.edu,yang.feng@nyu.edu",
+    description="iArt: A Generalized Framework for Imputation-Assisted Randomization Tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Imputation-Assisted-Randomization-Tests/iArt-py",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        'mv-laplace',
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'xgboost',
+        'statsmodels',
+        'lightgbm',
+    ],
+)
