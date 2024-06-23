b7fe14907f8003c3a472d209d9c4e8098e3c8459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:06 +0000

    Add package perceptual-manifold-geometry/0.1.1/perceptual_manifold_geometry-0.1.1.tar.gz

---
+++ packages/perceptual-manifold-geometry/perceptual_manifold_geometry-0.1.1.tar.gz/perceptual_manifold_geometry-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='perceptual_manifold_geometry',
+    version='0.1.1',  
+    author='Yanbiao Ma',
+    author_email='ybmamail@stu.xidian.edu.cn',
+    description='A package for analyzing the geometry of perceptual manifolds',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mayanbiao1234/Geometric-metrics-for-perceptual-manifolds',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'ripser',
+        'persim',
+        'matplotlib',
+        'scikit-dimension',
+    ],
+)
