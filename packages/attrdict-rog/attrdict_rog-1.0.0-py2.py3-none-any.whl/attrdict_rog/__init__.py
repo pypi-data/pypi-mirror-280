0d0c1314a99c9a6137aed0d1fb696598661611a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:25 +0000

    Add package attrdict-rog/1.0.0/attrdict_rog-1.0.0-py2.py3-none-any.whl

---
+++ packages/attrdict-rog/attrdict_rog-1.0.0-py2.py3-none-any.whl/attrdict_rog/__init__.py
@@ -0,0 +1,10 @@
+"""
+attrdict contains several mapping objects that allow access to their
+keys as attributes.
+"""
+from attrdict_rog.mapping import AttrMap
+from attrdict_rog.dictionary import AttrDict
+from attrdict_rog.default import AttrDefault
+
+
+__all__ = ['AttrMap', 'AttrDict', 'AttrDefault']
