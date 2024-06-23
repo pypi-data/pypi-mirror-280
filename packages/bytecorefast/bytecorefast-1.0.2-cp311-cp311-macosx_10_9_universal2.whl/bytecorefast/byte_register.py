2732b575a069b251f7ec298a6e5388de08e5714e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:04:48 +0000

    Add package bytecorefast/1.0.2/bytecorefast-1.0.2-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/bytecorefast/bytecorefast-1.0.2-cp311-cp311-macosx_10_9_universal2.whl/bytecorefast/byte_register.py
@@ -0,0 +1,6 @@
+from bytecore.byte_register import ByteRegister as ByteCoreByteRegister
+
+
+class ByteRegister(ByteCoreByteRegister):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ packages/bytecorefast/bytecorefast-1.0.2-cp311-cp311-macosx_10_9_universal2.whl/bytecorefast/constant_register.py
@@ -0,0 +1,6 @@
+from bytecore.constant_register import ConstantRegister as ByteCoreConstantRegister
+
+
+class ConstantRegister(ByteCoreConstantRegister):
+    def __init__(self) -> None:
+        super().__init__()
