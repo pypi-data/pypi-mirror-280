216a3b6ba58dc12ed96bbb77d596653cef9dd5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:00 +0000

    Add package tinylang/3.0.0/tinylang-3.0.0-py3-none-any.whl

---
+++ packages/tinylang/tinylang-3.0.0-py3-none-any.whl/tinylang/llms/base.py
@@ -0,0 +1,29 @@
+from typing import Iterator, Dict, AsyncIterable
+from abc import ABC, abstractmethod
+
+
+class ChatBase(ABC):
+    def __init__(
+        self,
+        model: str,
+        api_key: str | None = None,
+        system_message: str | None = None,
+        init_kwargs: Dict = {},
+    ) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def invoke(self, user_input: str) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def ainvoke(self, user_input: str) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def stream_invoke(self, user_input: str) -> Iterator[str]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def astream_invoke(self, user_input: str) -> AsyncIterable[str]:
+        raise NotImplementedError
