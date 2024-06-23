433c8dbd95a1ce59b4982bf5fa9d7379f622abc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 14:04:37 +0000

    Add package chia-blockchain/2.4.0/chia_blockchain-2.4.0.tar.gz

---
+++ packages/chia-blockchain/chia_blockchain-2.4.0.tar.gz/chia_blockchain-2.4.0/chia/types/weight_proof.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import chia_rs
+
+ProofBlockHeader = chia_rs.ProofBlockHeader
+RecentChainData = chia_rs.RecentChainData
+SubEpochChallengeSegment = chia_rs.SubEpochChallengeSegment
+SubEpochData = chia_rs.SubEpochData
+SubEpochSegments = chia_rs.SubEpochSegments
+SubSlotData = chia_rs.SubSlotData
+WeightProof = chia_rs.WeightProof
