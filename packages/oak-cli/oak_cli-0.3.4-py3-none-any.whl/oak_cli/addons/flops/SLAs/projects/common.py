09f45c364d55fd436091cde31dde44df728eef91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:15 +0000

    Add package oak-cli/0.3.4/oak_cli-0.3.4-py3-none-any.whl

---
+++ packages/oak-cli/oak_cli-0.3.4-py3-none-any.whl/oak_cli/addons/flops/SLAs/projects/common.py
@@ -0,0 +1,15 @@
+import pathlib
+
+from oak_cli.addons.flops.SLAs.common import FLOpsSLAs
+
+
+class FLOpsProjectSLAs(FLOpsSLAs):
+    CIFAR10_KERAS = "cifar10_keras"
+    CIFAR10_PYTORCH = "cifar10_pytorch"
+    MNIST_SKLEARN_SMALL = "mnist_sklearn_small"
+    MNIST_SKLEARN_LARGE = "mnist_sklearn_large"
+
+    # Note: This should be refactored and placed in the common parent class.
+    @classmethod
+    def get_SLAs_path(cls) -> pathlib.Path:
+        return pathlib.Path(__file__).resolve().parent
