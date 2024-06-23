866b4eb7128029cde10fd3ae213b998265cf486b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:05:07 +0000

    Add package coinconverter/0.0.4/coinconverter-0.0.4.tar.gz

---
+++ packages/coinconverter/coinconverter-0.0.4.tar.gz/coinconverter-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Real-time currency converter'
+# Setting up
+setup(
+    name="coinconverter",
+    version=VERSION,
+    author="EdexCode",
+    author_email="edexcode@gmail.com",
+    license="MIT License",
+    project_urls={
+        "GitHub": "https://github.com/EdexCode/coinconverter"
+    },
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['bs4', 'requests'],
+    keywords=['python', 'currency', 'converter', 'realtime', 'convert', 'exchange', 'exchange rate', 'exchange rates'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
