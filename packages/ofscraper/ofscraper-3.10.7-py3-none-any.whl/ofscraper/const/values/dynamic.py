6b8d8798c5e1eb9e3bf85af5afcc334e065b40bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:06:14 +0000

    Add package ofscraper/3.10.7/ofscraper-3.10.7-py3-none-any.whl

---
+++ packages/ofscraper/ofscraper-3.10.7-py3-none-any.whl/ofscraper/const/values/dynamic.py
@@ -0,0 +1,25 @@
+DYNAMIC_RULE=None
+DYNAMIC_OPTIONS=["deviint", "digitalcriminals","manual","generic","datawhores","riley","xagler","rafa"]
+DYNAMIC_OPTIONS_ALL=  DYNAMIC_OPTIONS + \
+[
+                "digitalcriminals",
+                "dv",
+                "dev",
+                "dc",
+                "digital",
+                "digitals",
+                "manual",
+                "generic"]
+DYNAMIC_RULE_DEFAULT="datawhores"
+XAGLER_URL="https://raw.githubusercontent.com/xagler/dynamic-rules/main/onlyfans.json"
+RAFA_URL="https://raw.githubusercontent.com/rafa-9/dynamic-rules/main/rules.json"
+
+DIGITALCRIMINALS = (
+    "https://raw.githubusercontent.com/DATAHOARDERS/dynamic-rules/main/onlyfans.json"
+)
+DEVIINT = "https://raw.githubusercontent.com/deviint/onlyfans-dynamic-rules/main/dynamicRules.json"
+
+DATAWHORES_URL = "https://raw.githubusercontent.com/datawhores/onlyfans-dynamic-rules/main/dynamicRules.json"
+RILEY_URL = "https://raw.githubusercontent.com/riley-access-labs/onlyfans-dynamic-rules-1/patch-1/dynamicRules.json"
+DYNAMIC_GENERIC_URL=None
+ALLOW_OTHER_DYNAMIC_RULES=False
