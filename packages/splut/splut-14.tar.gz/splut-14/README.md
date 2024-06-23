585d84006a32b3fcda8e25156e9fcfd8d8a49aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:04:57 +0000

    Add package splut/14/splut-14.tar.gz

---
+++ packages/splut/splut-14.tar.gz/splut-14/README.md
@@ -0,0 +1,20 @@
+# splut
+Concurrency-related Python utils
+
+## Install
+These are generic installation instructions.
+
+### To use, permanently
+The quickest way to get started is to install the current release from PyPI:
+```
+pip3 install --user splut
+```
+
+### To use, temporarily
+If you prefer to keep .local clean, install to a virtualenv:
+```
+python3 -m venv venvname
+venvname/bin/pip install -U pip
+venvname/bin/pip install splut
+. venvname/bin/activate
+```
---
+++ packages/splut/splut-14.tar.gz/splut-14/splut/actor/__init__.py
@@ -0,0 +1,19 @@
+from .future import Future
+from .mailbox import Mailbox
+from .message import Message
+from functools import partial
+
+class Spawn:
+
+    def __init__(self, executor):
+        self.executor = executor
+
+    def __call__(self, *objs):
+        def post(name, *args, **kwargs):
+            future = Future()
+            mailbox.add(Message(name, args, kwargs, future))
+            return future
+        def __getattr__(self, name):
+            return partial(post, name)
+        mailbox = Mailbox(self.executor, objs)
+        return type(f"{''.join({type(obj).__name__: None for obj in objs})}Actor", (), {f.__name__: f for f in [__getattr__]})()
