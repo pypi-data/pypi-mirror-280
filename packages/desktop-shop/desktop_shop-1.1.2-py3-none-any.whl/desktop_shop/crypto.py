6df9469a86ffe2b0e0b1ef83bda67fa0c7f3c3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:30 +0000

    Add package desktop-shop/1.1.2/desktop_shop-1.1.2-py3-none-any.whl

---
+++ packages/desktop-shop/desktop_shop-1.1.2-py3-none-any.whl/desktop_shop/crypto.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 20:19:46 2020
+
+@author: Korean_Crimson
+"""
+from dataclasses import dataclass
+import enum
+import hashlib
+import random
+import secrets
+from typing import Callable, Dict, Optional
+
+HashFunctionCallable = Callable[[str, str, int], str]
+BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+class InvalidHashFunctionException(ValueError):
+    """Exception thrown when an invalid hash function is specified"""
+
+
+class HashFunctions(enum.Enum):
+    """hash functions enum (values are functions)"""
+
+    HASHLIB_PBKDF2_HMAC_SHA256 = enum.auto()
+
+
+@dataclass
+class HashFunction:
+    """Contains a hash function enum and its iterations keyword.
+    Encapsulates a hash operation.
+    """
+
+    function: HashFunctions
+    iterations: int
+
+    def hash(self, password: str, salt: str) -> str:
+        """Returns the result of the hash function hashing the provided password and salt"""
+        function = get_function_from_hash_function_enum(self.function)
+        return function(password, salt, self.iterations)
+
+    def __str__(self):
+        return f"{self.function.name}::{self.iterations}"
+
+
+def hash_string(password: str, salt: str, iterations: int = 100_000) -> str:
+    """Hashes the specified password with the passed salt.
+    Returns hash in hex format (str).
+    """
+    encoded_password = bytes(password, encoding="utf-8")
+    encoded_salt = bytes(salt, encoding="utf-8")
+    derived_key = hashlib.pbkdf2_hmac(
+        "sha256", encoded_password, encoded_salt, iterations=iterations
+    )
+    return derived_key.hex()
+
+
+def get_hash_function(iterations: int) -> HashFunction:
+    """Returns a HashFunction object"""
+    return HashFunction(function=HashFunctions.HASHLIB_PBKDF2_HMAC_SHA256, iterations=iterations)
+
+
+def get_hash_function_from_string(hash_function_name: str) -> Optional[HashFunction]:
+    """Looks up a hash function in the HashFunctions enum from a str key in
+    the format name::iterations.
+    """
+    name, iterations = hash_function_name.split("::")
+    hash_function = HashFunctions.__dict__.get(name)
+    if hash_function is not None:
+        return HashFunction(function=hash_function, iterations=int(iterations))
+    return None
+
+
+def get_function_from_hash_function_enum(
+    hash_function: HashFunctions,
+) -> HashFunctionCallable:
+    """Looks up the passed HashFunctions enum and returns a corresponding function handle"""
+    functions: Dict[HashFunctions, HashFunctionCallable] = {
+        HashFunctions.HASHLIB_PBKDF2_HMAC_SHA256: hash_string
+    }
+    hash_function_callable = functions.get(hash_function)
+    if hash_function_callable is None:
+        raise InvalidHashFunctionException("Hash function could not be found!")
+    return hash_function_callable
+
+
+def generate_new_salt() -> str:
+    """Generates a new base 62 encoded salt based on a cryptographically
+    secure random number, to hash strings.
+    """
+    rng = random.SystemRandom()
+    random_number = rng.randint(0, 2**64)
+    salt = encode_in_base62(random_number)
+    return salt
+
+
+def encode_in_base62(num: int, alphabet: str = BASE62) -> str:
+    """Encode a positive number into Base 62 and return the string."""
+    char_array = [] if num else [alphabet[0]]
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        char_array.append(alphabet[rem])
+    char_array.reverse()
+    encoded_string = "".join(char_array)
+    return encoded_string
+
+
+def generate_new_session_id() -> str:
+    """Creates a cryptographically-secure, URL-safe string"""
+    return secrets.token_urlsafe(16)
---
+++ packages/desktop-shop/desktop_shop-1.1.2-py3-none-any.whl/desktop_shop/gui/init.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:00:48 2021
+
+@author: Korean_Crimson
+"""
+import tkinter as tk
+from typing import Dict
+
+from desktop_shop import gui
+from desktop_shop.gui import components, config
+from desktop_shop.gui.components import Builder, Component, EntryFactory, Factory, Frame
+from desktop_shop.gui.views import views
+
+
+def _set_transparent_colour():
+    # this apparently fails on linux for no particular reason
+    try:
+        gui.root.wm_attributes("-transparentcolor", "purple")
+    except tk.TclError:
+        pass
+
+
+def init_root():
+    """Initialises and configures tk root"""
+    gui.root.title("OfflineShop")
+    _set_transparent_colour()
+    gui.root.config(bg=config.BG)
+    for _ in range(21):
+        gui.root.add_row(30)
+    for col in [150, 100, 100, 100, 150]:
+        gui.root.add_col(col)
+
+
+def init_builder():
+    """Initialises the component builder and registers the widget factories"""
+    return Builder(
+        frame=Factory(tk.Frame, Frame, kwargs=config.FRAME_THEME),
+        menu_frame=Factory(tk.Frame, Component, kwargs=config.MENU_THEME),
+        label=EntryFactory(tk.Label, Component, kwargs=config.LABEL_THEME),
+        label2=EntryFactory(tk.Label, Component, kwargs=config.ERROR_THEME),
+        entry=EntryFactory(tk.Entry, Component, kwargs=config.ENTRY_THEME),
+        button=Factory(tk.Button, Component, kwargs=config.BUTTON_THEME),
+        button2=Factory(tk.Button, Component, kwargs=config.BUTTON_THEME2),
+    )
+
+
+def init_views(window: components.Tk, builder: components.Builder) -> Dict[str, components.View]:
+    """Initialises all views"""
+    views_: Dict[str, components.View] = {
+        name: view.create(window, builder) for name, view in views.items()
+    }
+    views_["home"].activate()
+    views_["main_menu"].activate()
+    return views_
+
+
+def init():
+    """Init function that needs to be called before gui is started"""
+    init_root()
+    gui.app.builder = init_builder()
+    gui.app.views_dict = init_views(gui.root, gui.app.builder)
+    gui.app.data = {
+        "session_id": None,
+        "user_data": None,
+        "pw_hash": "",
+        "cart": [],
+    }
+    gui.app["home"].init_product_data(gui.root, gui.app.builder)  # type: ignore
