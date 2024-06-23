82a7aafed01a97ca06072e11ba5b8ef9b9087b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:53 +0000

    Add package gnies/0.3.3/gnies-0.3.3.tar.gz

---
+++ packages/gnies/gnies-0.3.3.tar.gz/gnies-0.3.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="gnies",
+    version="0.3.3",
+    author="Juan L. Gamella",
+    author_email="juangamella@gmail.com",
+    packages=["gnies", "gnies.scores"],
+    scripts=[],
+    url="https://github.com/juangamella/gnies",
+    license="BSD 3-Clause License",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["numpy>=1.17.0", "ges>=1.1.0"],
+)
