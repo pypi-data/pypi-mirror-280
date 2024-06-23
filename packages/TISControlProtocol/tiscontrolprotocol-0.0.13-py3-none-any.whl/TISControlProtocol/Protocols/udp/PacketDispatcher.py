4e165f0e69dd36eebbc08f24c549a8826e3ec95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:32 +0000

    Add package TISControlProtocol/0.0.13/tiscontrolprotocol-0.0.13-py3-none-any.whl

---
+++ packages/TISControlProtocol/tiscontrolprotocol-0.0.13-py3-none-any.whl/TISControlProtocol/Protocols/udp/PacketDispatcher.py
@@ -0,0 +1,16 @@
+from homeassistant.core import HomeAssistant  # type: ignore
+
+
+class PacketDispatcher:
+    def __init__(self, hass: HomeAssistant, OPERATIONS_DICT: dict):
+        self.hass = hass
+        self.operations_dict = OPERATIONS_DICT
+
+    async def dispatch_packet(self, info):
+        packet_handler = self.operations_dict.get(
+            tuple(info["operation_code"]), "unknown operation"
+        )
+        if packet_handler != "unknown operation":
+            await packet_handler(self.hass, info)
+        else:
+            print(f"unknown operation code: {info['operation_code']}")
