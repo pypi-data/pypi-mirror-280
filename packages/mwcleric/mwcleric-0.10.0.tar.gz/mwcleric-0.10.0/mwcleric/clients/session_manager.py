0670d9d39edf392327677ba3071d0c96d4f012f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:05:35 +0000

    Add package mwcleric/0.10.0/mwcleric-0.10.0.tar.gz

---
+++ packages/mwcleric/mwcleric-0.10.0.tar.gz/mwcleric-0.10.0/mwcleric/clients/session_manager.py
@@ -0,0 +1,26 @@
+from mwcleric.auth_credentials import AuthCredentials
+from mwcleric.clients.site import Site
+
+
+class SessionManager(object):
+    """Manages instances of WikiClient
+    """
+    existing_wikis = {}
+
+    def get_client(self, url: str = None, path: str = None, scheme=None,
+                   credentials: AuthCredentials = None, force_new=False,
+                   max_retries: int = 10,
+                   **kwargs):
+        if url in self.existing_wikis and not force_new:
+            return self.existing_wikis[url]['client']
+        if scheme is not None:
+            client = Site(url, path=path, scheme=scheme, max_retries=max_retries, **kwargs)
+        else:
+            client = Site(url, path=path, max_retries=max_retries, **kwargs)
+        if credentials:
+            client.login(username=credentials.username, password=credentials.password)
+        self.existing_wikis[url] = {'client': client}
+        return client
+
+
+session_manager = SessionManager()
