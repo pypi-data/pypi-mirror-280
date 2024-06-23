1c007ab6dc862884dc143dcf2ab376ef017593ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:04:55 +0000

    Add package murnitur/1.0.1/murnitur-1.0.1.tar.gz

---
+++ packages/murnitur/murnitur-1.0.1.tar.gz/murnitur-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name="murnitur",
+    version="1.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Caleb Okpara",
+    author_email="support@murnitur.com",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "openlit",
+        "deepeval",
+        "transformers>=4.39.3",
+        "pandas",
+        "vaderSentiment",
+        "openai",
+    ],
+    python_requires=">=3.7.1",
+)
