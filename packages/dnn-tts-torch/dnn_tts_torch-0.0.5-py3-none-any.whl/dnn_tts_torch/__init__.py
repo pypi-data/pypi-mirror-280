6b81e05cfcb531ba2ea386f6071cc9a8e867bd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:05:06 +0000

    Add package dnn-tts-torch/0.0.5/dnn_tts_torch-0.0.5-py3-none-any.whl

---
+++ packages/dnn-tts-torch/dnn_tts_torch-0.0.5-py3-none-any.whl/dnn_tts_torch/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+DNN_TTS_Torch library (dnn_tts_torch) v.0.0.5
+
+The MIT License Copyright © 2024 KirillPogranichny
+
+"""
+
+
+from .datasets import *
+from .models import *
+from .audio import *
+from .hparams import *
+from .logger import *
+from .synthesize import *
+from .utils import *
