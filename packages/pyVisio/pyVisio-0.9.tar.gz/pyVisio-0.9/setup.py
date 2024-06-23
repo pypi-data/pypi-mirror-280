6dcb12b52d4c9b6187048d8a65ce9efec6508519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:34 +0000

    Add package pyVisio/0.9/pyVisio-0.9.tar.gz

---
+++ packages/pyVisio/pyVisio-0.9.tar.gz/pyVisio-0.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "docs" / "pypi.md").read_text()
+
+setup(
+    name='pyVisio',
+    version='0.9',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'plotly',
+        'pandas',
+        'numpy',
+        'statsmodels',
+        'jinja2',
+        'reportlab'
+    ],
+    author='Hüseyin Kaya',
+    author_email='kaya87826@gmail.com',
+    description='Interactive and dynamic data visualization library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kayahuseyinn/pyVisio',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
