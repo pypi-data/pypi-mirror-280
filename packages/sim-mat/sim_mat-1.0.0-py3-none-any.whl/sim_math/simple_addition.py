1c5fba87b27c041a642c90d7155afb33c029d1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 03:04:45 +0000

    Add package sim-mat/1.0.0/sim_mat-1.0.0-py3-none-any.whl

---
+++ packages/sim-mat/sim_mat-1.0.0-py3-none-any.whl/sim_math/simple_addition.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 12:44:50 2024
+
+@author: EL221XK
+"""
+
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the result.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+    
+    Returns:
+    int or float: The sum of a and b.
+    """
+    return a + b
