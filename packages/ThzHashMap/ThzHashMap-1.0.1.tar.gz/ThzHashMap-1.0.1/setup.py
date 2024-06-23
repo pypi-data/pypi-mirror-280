8a5c39e9daa68d4b3235b2dd4a6ee642e85c3e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:04:54 +0000

    Add package ThzHashMap/1.0.1/ThzHashMap-1.0.1.tar.gz

---
+++ packages/ThzHashMap/ThzHashMap-1.0.1.tar.gz/ThzHashMap-1.0.1/setup.py
@@ -0,0 +1,29 @@
+﻿from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+with open("./README.md",mode='r',encoding='utf-8') as f:
+    des = f.read()
+extensions = [
+    Extension(
+        "hashmap",
+        sources=["thzhashmap/hashmap.pyx", "thzhashmap/hashmap_c.cpp","thzhashmap/__init__.pyx"],
+        language="c++",
+            )
+]
+
+setup(
+    name="ThzHashMap",
+    ext_modules=cythonize(extensions),
+    version="1.0.1",               # 版本号
+    author="Sen",
+    package=['thzhashmap'],
+    package_data={"thzhashmap": ["thzhashmap/*.pyx"]},
+    install_requires=['Cython'],
+    description="由天狐宗开发的哈希表，仅供学习",
+    long_description=des,
+    long_description_content_type='text/markdown',
+    author_email="tianhuzong@qq.com",
+    url="https://github.com/tianhuzong/hashmap",
+    license="MIT",
+    
+)
