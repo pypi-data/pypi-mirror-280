4ada7462327fa979f8fa7da403f3277104cdde6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 15:06:05 +0000

    Add package pgtui/0.3.0/pgtui-0.3.0.tar.gz

---
+++ packages/pgtui/pgtui-0.3.0.tar.gz/pgtui-0.3.0/pgtui/app.py
@@ -0,0 +1,28 @@
+from textual.app import App
+
+from pgtui.completer import QueryCompleter
+from pgtui.db import fetch_db_info
+from pgtui.entities import DbContext
+from pgtui.messages import ShowException
+from pgtui.screens.loading import LoadingScreen
+from pgtui.screens.query import QueryScreen
+from pgtui.widgets.dialog import MessageDialog
+
+
+class PgTuiApp(App[None]):
+    def __init__(self, ctx: DbContext, completer: QueryCompleter):
+        super().__init__()
+        self.ctx = ctx
+        self.completer = completer
+
+    def on_mount(self):
+        self.push_screen(LoadingScreen())
+        self.call_after_refresh(self.db_init)
+
+    async def db_init(self):
+        db_info = await fetch_db_info(self.ctx)
+        await self.switch_screen(QueryScreen(self.ctx, db_info, self.completer))
+
+    def on_show_exception(self, message: ShowException):
+        body = str(message.exception)
+        self.push_screen(MessageDialog("Error", body, error=True))
