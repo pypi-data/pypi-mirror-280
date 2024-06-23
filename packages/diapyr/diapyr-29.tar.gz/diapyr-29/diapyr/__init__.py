a93bdc404b806de796a76d4db9b331132d1502be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 04:04:41 +0000

    Add package diapyr/29/diapyr-29.tar.gz

---
+++ packages/diapyr/diapyr-29.tar.gz/diapyr-29/diapyr/__init__.py
@@ -0,0 +1,7 @@
+from .diapyr import DI, types
+from .iface import MissingAnnotationException, UnsatisfiableRequestException
+
+assert DI
+assert MissingAnnotationException
+assert types
+assert UnsatisfiableRequestException
