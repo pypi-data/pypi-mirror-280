e1c386922d7362cceafebd2af385c4e643924cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-23 14:04:42 +0000

    Add package dustapi/0.0.1/dustapi-0.0.1.tar.gz

---
+++ packages/dustapi/dustapi-0.0.1.tar.gz/dustapi-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dustapi',
+    version='0.0.2',
+    packages=find_packages(),
+    description='Fast light weight web server',
+    author='Praise Godwins',
+    install_requires=[
+        'werkzeug',
+        'jinja2',
+        'click',
+        'websockets',
+        'mkdocs',
+        'pyjwt',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dustapi=dustapi.cli:cli',
+        ],
+    },
+)
