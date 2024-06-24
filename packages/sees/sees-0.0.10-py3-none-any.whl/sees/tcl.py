5bb16c0b5978599753f13d19a334e531c251d555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:06:01 +0000

    Add package sees/0.0.10/sees-0.0.10-py3-none-any.whl

---
+++ packages/sees/sees-0.0.10-py3-none-any.whl/sees/tcl.py
@@ -0,0 +1,30 @@
+import sys
+import json
+from functools import partial
+from sees.cli import parse_args
+from sees import config, render, RenderError
+
+def _render(*args, rt=None):
+    try:
+        config = parse_args(["render", *args])
+#       print(config)
+        if config is None:
+            return ""
+        if "verbose" in config and config["verbose"]:
+            print(config)
+
+        config["sam_file"] = rt.to_dict()
+        render(**config)
+
+    except RenderError:
+        print(e, file=sys.stderr)
+
+    return ""
+
+def add_commands(rt):
+    try:
+        rt._tcl.createcommand("sees::render", partial(_render, rt=rt))
+    except:
+        pass
+
+
