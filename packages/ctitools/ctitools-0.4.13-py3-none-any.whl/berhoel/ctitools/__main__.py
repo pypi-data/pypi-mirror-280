0056807e3bde64b0320b0f0246b216af20d42c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:05:30 +0000

    Add package ctitools/0.4.13/ctitools-0.4.13-py3-none-any.whl

---
+++ packages/ctitools/ctitools-0.4.13-py3-none-any.whl/berhoel/ctitools/__main__.py
@@ -0,0 +1,14 @@
+"""Main code for module."""
+
+import sys
+
+from . import main
+
+__date__ = "2024/06/23 17:42:50 hoel"
+__author__ = "Berthold Höllmann"
+__copyright__ = "Copyright © 2022 by Berthold Höllmann"
+__credits__ = ["Berthold Höllmann"]
+__maintainer__ = "Berthold Höllmann"
+__email__ = "berhoel@gmail.com"
+
+sys.exit(main())
