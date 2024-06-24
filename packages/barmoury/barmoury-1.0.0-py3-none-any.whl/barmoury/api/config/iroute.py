1890fb08c24950ec0dbf34348572de1f8166b572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:51 +0000

    Add package barmoury/1.0.0/barmoury-1.0.0-py3-none-any.whl

---
+++ packages/barmoury/barmoury-1.0.0-py3-none-any.whl/barmoury/api/config/iroute.py
@@ -0,0 +1,28 @@
+
+from ...util import *
+from typing import List, Self
+from ..enum import RequestMethod
+from ..config import RouteEntries
+from starlette.applications import Request as StarletteRequest
+
+
+class Route:
+    route: str = ""
+    method: RequestMethod = None
+    
+    def __init__(self: Self, route: str = "", method: RequestMethod = None):
+        self.route = route
+        self.method = method
+    
+
+def should_not_filter(request: StarletteRequest, prefix: str, open_url_patterns: List[Route]) -> bool:
+    if not request.url or not request.url.path:
+        return
+    method = request.method
+    route = ("/" if prefix else "") + request.url.path.replace(prefix or "", "")
+    for open_url_pattern in open_url_patterns:
+        if (open_url_pattern.method == None or open_url_pattern.method.value == method) and util.pattern_to_regex(open_url_pattern.route).match(route):
+            if '{' in open_url_pattern.route and request.url.path in RouteEntries:
+                continue
+            return True
+    return False
