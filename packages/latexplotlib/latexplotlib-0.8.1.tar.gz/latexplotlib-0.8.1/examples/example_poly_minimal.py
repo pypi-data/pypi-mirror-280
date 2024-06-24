fa8d1819846c4161f0001a6a2144721c5af8350c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:46 +0000

    Add package latexplotlib/0.8.1/latexplotlib-0.8.1.tar.gz

---
+++ packages/latexplotlib/latexplotlib-0.8.1.tar.gz/latexplotlib-0.8.1/examples/example_poly_minimal.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import latexplotlib as lpl
+
+lpl.style.use("latex10pt-minimal")
+# lpl.size.set(200, 400)
+with lpl.size.context(200, 400):
+    fig, ax = lpl.subplots(1, 1)
+
+x = np.linspace(1, 5, 100)
+
+for t in range(4):
+    label = f"$x^{t}$"
+    ax.plot(x, x**t, label=label)
+
+ax.set_yscale("log")
+ax.set_title("Perfect matplotlib figures for \\LaTeX")
+ax.grid()
+
+fig.legend()
+fig.savefig("example_poly_minimal")
+fig.savefig("example_poly_minimal.png")
---
+++ packages/latexplotlib/latexplotlib-0.8.1.tar.gz/latexplotlib-0.8.1/tests/conftest.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--plot", action="store_true")
+
+
+@pytest.fixture(autouse=True)
+def _close_plots():
+    yield
+    plt.close("all")
+
+
+@pytest.fixture()
+def _show(pytestconfig):
+    yield
+    if pytestconfig.getoption("plot"):
+        plt.show(block=True)
