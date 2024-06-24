5dfb883eb81bfbf8282f3dadf487abaab8185d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:06:01 +0000

    Add package bunda/0.1/bunda-0.1-py3-none-any.whl

---
+++ packages/bunda/bunda-0.1-py3-none-any.whl/bunda/main_module.py
@@ -0,0 +1,9 @@
+def print_6():
+    with open('bunda/bunda/p6.py', 'r') as file:
+        code = file.read()
+    exec(code)
+
+def print_7():
+    with open('bunda/bunda/p7.py', 'r') as file:
+        code = file.read()
+    exec(code)
