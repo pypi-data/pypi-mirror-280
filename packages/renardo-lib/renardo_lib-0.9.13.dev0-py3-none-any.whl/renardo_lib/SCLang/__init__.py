7e9a4f3e8aa24bc311914b85ef28ed141792891a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:05:35 +0000

    Add package renardo-lib/0.9.13.dev0/renardo_lib-0.9.13.dev0-py3-none-any.whl

---
+++ packages/renardo-lib/renardo_lib-0.9.13.dev0-py3-none-any.whl/renardo_lib/SCLang/__init__.py
@@ -0,0 +1,5 @@
+from renardo_lib.SCLang.SCLang import *
+from renardo_lib.SCLang.SynthDef import SynthDefs, SynthDef, SynthDefProxy, SampleSynthDef, CompiledSynthDef
+# from renardo_lib.SCLang import Env
+
+
