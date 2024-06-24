63c576832ec706cab80fbd3f30ebbc433f730a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:05:50 +0000

    Add package hemse/1.0.1/hemse-1.0.1-py3-none-any.whl

---
+++ packages/hemse/hemse-1.0.1-py3-none-any.whl/hemse/common/color.py
@@ -0,0 +1,14 @@
+class Color:
+    primary = "#222831"
+    secondary = "#393E46"
+    third = "#2a9fd6"
+    text = "#EEEEEE"
+
+    chart = [
+        "#2a9fd6",
+        "#217fab",
+        "#195f80",
+        "#103f55",
+        "#081f2a",
+        "#000000",
+    ]
