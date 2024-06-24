4f2c85259500723b58e2a4c2d17aaf170b662148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 18:05:54 +0000

    Add package loongfei-utils/1.0.6/loongfei_utils-1.0.6-py3-none-any.whl

---
+++ packages/loongfei-utils/loongfei_utils-1.0.6-py3-none-any.whl/loongfei_utils/ConfigLogger.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+
+def setup_logger():
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)  # 设置日志级别为INFO
+
+    if not logger.handlers:
+
+        log_directory = os.path.join(os.path.dirname(__file__), 'logs')
+        os.makedirs(log_directory, exist_ok=True)  # 确保日志目录存在
+        file_handler = logging.FileHandler(os.path.join(log_directory, 'app.log'))
+        file_handler.setLevel(logging.INFO)  # 文件记录INFO及以上级别的日志
+
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)  # 控制台输出INFO及以上级别的日志
+
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
+
+
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+    return logger
+
+def config_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt='%(asctime)s  %(filename)s  [line:%(lineno)d]  %(levelname)s :%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    # 控制台的输出
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    # 将输出的流 添加给logger
+    logger.addHandler(ch)
+    return logger
