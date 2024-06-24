7071cdad423c4cea3c4c9eb1c09fb412157b7cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:36 +0000

    Add package sdk-practice/0.1.0/sdk_practice-0.1.0.tar.gz

---
+++ packages/sdk-practice/sdk_practice-0.1.0.tar.gz/sdk_practice-0.1.0/README.md
@@ -0,0 +1,2 @@
+# sdk_practice
+Learning how to create an SDK and publish it on PyPi
---
+++ packages/sdk-practice/sdk_practice-0.1.0.tar.gz/sdk_practice-0.1.0/sdk_practice/test_class.py
@@ -0,0 +1,10 @@
+class TestClass:
+    # Test class
+    def __init__(self):
+        self._value = 0
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        self._value = value
