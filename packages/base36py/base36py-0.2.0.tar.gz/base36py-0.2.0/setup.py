5376942eee7c33599cbeab09d652f603e4c5d2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:06:06 +0000

    Add package base36py/0.2.0/base36py-0.2.0.tar.gz

---
+++ packages/base36py/base36py-0.2.0.tar.gz/base36py-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="base36py",
+    version="0.2.0",
+    author="Danishjeet Singh",
+    author_email="danishjeetsingh@gmail.com",
+    description="A lightweight Python library for encoding and decoding numbers between base10 and base36.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanishjeetSingh/base36py",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
