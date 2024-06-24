d92890f82eb844a1aa304817f98f6eea81b8e718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:05:49 +0000

    Add package Scrapins/0.2.0/scrapins-0.2.0.tar.gz

---
+++ packages/Scrapins/scrapins-0.2.0.tar.gz/scrapins-0.2.0/README.md
@@ -0,0 +1,9 @@
+## Configuration OpenAI
+
+Pour utiliser les fonctionnalités liées à l'API OpenAI, vous devez créer un fichier `openai.json` dans le répertoire racine de votre projet avec le contenu suivant :
+
+```json
+{
+    "api_key": "votre_cle_api_openai"
+}
+```
---
+++ packages/Scrapins/scrapins-0.2.0.tar.gz/scrapins-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Scrapins',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        # Ajouter ici les dépendances nécessaires
+    ],
+    entry_points={
+        'console_scripts': [
+            # Ajouter ici des points d'entrée pour les scripts de la ligne de commande, si nécessaire
+        ],
+    },
+    author='Lùkas K.',
+    author_email='lukas.koltes@hotmail.fr',
+    description='Une description personnelle pour des outils de Scraping',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Grimmins/Scrapins',  # Remplacer par l'URL de votre projet
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
