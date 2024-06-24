d5f695da7ed9dfa6435a99888fbde586062b8877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:32 +0000

    Add package barmpy/1.2.0/barmpy-1.2.0.tar.gz

---
+++ packages/barmpy/barmpy-1.2.0.tar.gz/barmpy-1.2.0/setup.py
@@ -0,0 +1,22 @@
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='barmpy',
+    version='1.2.0',
+    description='Bayesian Additive Regression Models implementation for Python',
+    url='https://github.com/dvbuntu/barmpy',
+    author='Danielle Van Boxel',
+    author_email='vanboxel@arizona.edu',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scipy',
+        'scikit-learn',
+        'tqdm',
+    ]
+)
