9be8ae3ff896c71b46080fc1825314121691c276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 18:06:19 +0000

    Add package hwdesk/1.0.6/hwdesk-1.0.6.tar.gz

---
+++ packages/hwdesk/hwdesk-1.0.6.tar.gz/hwdesk-1.0.6/README.md
@@ -0,0 +1,34 @@
+# hwdesk
+
+## Installation
+
+You can install the package via pip:
+
+```
+pip install hwdesk
+```
+
+## Usage
+
+To start the program,
+
+```
+hwdesk
+```
+
+To specify the framerate,
+
+```
+hwdesk --fps 10
+```
+
+To exit the program, press `alt+f4` or `esc`.
+
+## Limitations
+
+- Mouse Drag
+- Mouse Middle
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ packages/hwdesk/hwdesk-1.0.6.tar.gz/hwdesk-1.0.6/hwdesk/gui.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+from threading import Event
+from typing import Any
+
+import cv2
+import keyboard
+from ch9329.exceptions import InvalidKey
+from ch9329.keyboard import Modifier
+from cv2.typing import MatLike
+from PIL import Image
+from PIL import ImageTk
+
+from hwdesk import logger
+from hwdesk.camera.base import BaseCamera
+from hwdesk.constants import HEIGHT
+from hwdesk.constants import WIDTH
+from hwdesk.controls.ch9329 import CH9329
+from hwdesk.controls.ch9329 import MODIFIER_MAP
+
+
+class GUI(tk.Tk):
+    def __init__(
+        self,
+        camera: BaseCamera,
+        ch9329: CH9329,
+        title: str = "Window",
+        exit_on_esc: bool = True,
+        fps: bool = True,
+    ):
+        super().__init__()
+        self.camera = camera
+        self.ch9329 = ch9329
+        self.title(title)
+        self.resizable(False, False)
+        self.exit_on_esc = exit_on_esc
+        self.show_fps = fps
+        self.fps_font_size = 16
+        self.exit_flag = Event()
+        self.modifiers: set[str] = set()
+
+        self.bind("<ButtonPress>", self.on_mouse_press)
+        self.bind("<ButtonRelease>", self.on_mouse_release)
+        self.bind("<MouseWheel>", self.on_wheel)
+        self.bind("<Motion>", self.on_move)
+
+        self.attributes("-fullscreen", True)  # type:ignore
+        self.canvas = tk.Canvas(self, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+        self.after(int(1000 / self.camera.fps), self.gui_loop)
+
+    def on_key_event(self, key_event: keyboard.KeyboardEvent):
+        if key_event.name == "esc" and self.exit_on_esc:
+            self.exit_flag.set()
+            return
+        is_modifier = keyboard.is_modifier(key_event.scan_code)
+        if key_event.name and key_event.event_type == "down":
+            try:
+                if is_modifier:
+                    self.modifiers.add(key_event.name)
+                    self.ch9329.key_press(key_event.name)
+                else:
+                    modifiers: list[Modifier] = []
+                    for m in self.modifiers:
+                        if m in MODIFIER_MAP:
+                            modifiers.append(MODIFIER_MAP[m])
+                    self.ch9329.key_press(key_event.name, modifiers)
+            except InvalidKey as e:
+                logger.error(f"Invalidkey: {e.args}")
+
+        elif key_event.name and key_event.event_type == "up":
+            if is_modifier and key_event.name in self.modifiers:
+                self.modifiers.remove(key_event.name)
+            self.ch9329.key_release()
+
+    def gui_loop(self):
+        if self.exit_flag.is_set():
+            self.destroy()
+            return
+        if self.focus_displayof():
+            keyboard.hook(self.on_key_event, suppress=True)
+        else:
+            keyboard.unhook_all()
+        if self.camera.img is not None:
+            self.imshow(self.camera.img)
+        self.after(int(1000 / self.camera.fps), self.gui_loop)
+
+    def imshow(self, img: MatLike):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.image = Image.fromarray(img)  # type: ignore
+        self.tk_image = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(0, 0, image=self.tk_image, anchor=tk.NW)  # type: ignore
+        if self.show_fps:
+            l = tk.Label(
+                self,
+                text=f"FPS:{self.camera.fps:.2f}",
+                fg="lime green",
+                bg="grey",
+                font=f"monospace {self.fps_font_size}",
+            )
+            l.place(x=0, y=0)
+        self.update()
+
+    def on_move(self, event: Any):
+        # TODO: fix drag
+        self.ch9329.move(event.x, event.y)
+
+    def on_wheel(self, event: Any):
+        self.ch9329.wheel(event.delta // 100)
+
+    def on_mouse_press(self, event: Any):
+        if event.num == 1:
+            self.ch9329.mouse_press("left")
+        elif event.num == 3:
+            self.ch9329.mouse_press("right")
+        else:
+            # TODO: implement mouse middle
+            pass
+
+    def on_mouse_release(self, event: Any):
+        self.ch9329.mouse_release()
