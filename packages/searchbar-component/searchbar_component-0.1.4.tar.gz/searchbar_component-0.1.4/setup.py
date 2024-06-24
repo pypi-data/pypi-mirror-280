139d85243d1b7925428ed33430e98ac83b9094c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:05:45 +0000

    Add package searchbar-component/0.1.4/searchbar_component-0.1.4.tar.gz

---
+++ packages/searchbar-component/searchbar_component-0.1.4.tar.gz/searchbar_component-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='searchbar_component',
+    version='0.1.4',  # increment the version number
+    description='A Streamlit component for a search bar with autosuggestions',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'streamlit>=0.63',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
