b07e52bb7102edde9572ca12cbf362ade7fc5adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:05:24 +0000

    Add package pp7stubs/1.0/pp7stubs-1.0-py3-none-any.whl

---
+++ packages/pp7stubs/pp7stubs-1.0-py3-none-any.whl/pp7stubs/proApiV1Audio_pb2.pyi
@@ -0,0 +1,7 @@
+from . import proApiV1Identifier_pb2 as _proApiV1Identifier_pb2
+from . import proApiV1MediaPlaylistItem_pb2 as _proApiV1MediaPlaylistItem_pb2
+from . import proApiV1Playlist_pb2 as _proApiV1Playlist_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
