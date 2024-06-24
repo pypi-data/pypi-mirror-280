614ab644dc2c111a8a248b6c3746803cef7ba69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 19:05:57 +0000

    Add package torch-timeseries/0.1.3/torch_timeseries-0.1.3.tar.gz

---
+++ packages/torch-timeseries/torch_timeseries-0.1.3.tar.gz/torch_timeseries-0.1.3/torch_timeseries/experiments/__init__.py
@@ -0,0 +1,41 @@
+from .forecast import ForecastExp
+from .uea_classification import UEAClassificationExp
+from .anomaly_detection import AnomalyDetectionExp
+from .imputation import ImputationExp
+from .DLinear import (
+    DLinearAnomalyDetection,
+    DLinearForecast,
+    DLinearImputation,
+    DLinearUEAClassification,
+)
+
+from .Autoformer import (
+    AutoformerAnomalyDetection,
+    AutoformerForecast,
+    AutoformerImputation,
+    AutoformerUEAClassification,
+)
+
+from .FEDformer import (
+    FEDformerAnomalyDetection,
+    FEDformerForecast,
+    FEDformerImputation,
+    FEDformerUEAClassification,
+)
+
+from .Informer import (
+    InformerAnomalyDetection,
+    InformerForecast,
+    InformerImputation,
+    InformerUEAClassification,
+)
+
+from .PatchTST import (
+    PatchTSTAnomalyDetection,
+    PatchTSTForecast,
+    PatchTSTImputation,
+    PatchTSTUEAClassification,
+)
+
+from .iTransformer import (iTransformerAnomalyDetection, iTransformerForecast, iTransformerImputation, iTransformerUEAClassification)
+
