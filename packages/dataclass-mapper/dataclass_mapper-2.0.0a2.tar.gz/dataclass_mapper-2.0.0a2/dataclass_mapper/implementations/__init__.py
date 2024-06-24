6b8ae3ac6ffd83a642281b7d19c71ec7223964e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:05:41 +0000

    Add package dataclass-mapper/2.0.0a2/dataclass_mapper-2.0.0a2.tar.gz

---
+++ packages/dataclass-mapper/dataclass_mapper-2.0.0a2.tar.gz/dataclass_mapper-2.0.0a2/dataclass_mapper/implementations/__init__.py
@@ -0,0 +1,18 @@
+from typing import List, Type
+
+from .base import ClassMeta, DataclassType, FieldMeta
+from .dataclasses import DataclassClassMeta
+from .pydantic_v1 import PydanticV1ClassMeta
+from .pydantic_v2 import PydanticV2ClassMeta
+from .simple_type import SimpleType
+from .sqlalchemy import SQLAlchemyClassMeta
+
+class_meta_types: List[Type[ClassMeta]] = [
+    DataclassClassMeta,
+    PydanticV1ClassMeta,
+    PydanticV2ClassMeta,
+    SQLAlchemyClassMeta,
+    SimpleType,
+]
+
+__all__ = ["FieldMeta", "ClassMeta", "DataclassType", "class_meta_types"]
