acffe7f7c37e27a5f88a743a2f09e693ae9ad245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 19:05:42 +0000

    Add package pytest-aux/0.0.13/pytest_aux-0.0.13.tar.gz

---
+++ packages/pytest-aux/pytest_aux-0.0.13.tar.gz/pytest_aux-0.0.13/pytest_aux/__init__.py
@@ -0,0 +1,34 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .main import (
+    # BASE
+    pytest_func_tester,
+    pytest_func_tester__no_kwargs,
+    pytest_func_tester__no_args,
+    pytest_func_tester__no_args_kwargs,
+
+    # AUX
+
+    # TYPES
+
+    # EXX
+)
+
+# =====================================================================================================================
