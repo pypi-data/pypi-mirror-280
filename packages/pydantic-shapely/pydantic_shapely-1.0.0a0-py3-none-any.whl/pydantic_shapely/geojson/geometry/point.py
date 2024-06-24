aaea1e9b2e80885faf18755ab4ff1b7b21c64471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:43 +0000

    Add package pydantic-shapely/1.0.0a0/pydantic_shapely-1.0.0a0-py3-none-any.whl

---
+++ packages/pydantic-shapely/pydantic_shapely-1.0.0a0-py3-none-any.whl/pydantic_shapely/geojson/geometry/point.py
@@ -0,0 +1,32 @@
+"""_summary_
+"""
+
+import typing
+
+import shapely
+from pydantic import BaseModel
+
+from ._base import GeometryBase
+
+CoordinatesPoint2D = typing.Tuple[float, float]
+CoordinatesPoint3D = typing.Tuple[float, float, float]
+CoordinatesPoint = typing.Union[CoordinatesPoint2D, CoordinatesPoint3D]
+PointTypeVar = typing.TypeVar(
+    "PointTypeVar", CoordinatesPoint2D, CoordinatesPoint3D, CoordinatesPoint
+)
+
+
+class PointBase(GeometryBase, typing.Generic[PointTypeVar]):
+    """A point geometry."""
+
+    type: str = "Point"
+    coordinates: PointTypeVar
+
+    def to_shapely(self) -> shapely.Point:
+        """Convert the point to a Shapely point."""
+        return shapely.Point(*self.coordinates)
+
+
+Point2D = PointBase[CoordinatesPoint2D]
+Point3D = PointBase[CoordinatesPoint3D]
+Point = PointBase[CoordinatesPoint]
