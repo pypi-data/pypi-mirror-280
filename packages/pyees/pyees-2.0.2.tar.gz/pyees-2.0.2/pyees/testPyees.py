bb41a655db722aa4fb44844b42b92301ded6f191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:21 +0000

    Add package pyees/2.0.2/pyees-2.0.2.tar.gz

---
+++ packages/pyees/pyees-2.0.2.tar.gz/pyees-2.0.2/pyees/testPyees.py
@@ -0,0 +1,32 @@
+import unittest
+
+from pyees.testFit import test as testFit
+from pyees.testSheet import test as testSheet
+from pyees.testUnit import test as testUnit
+from pyees.testVariable import test as testVariable
+from pyees.testProp import test as testProp
+from pyees.testSolve import test as testSolve
+
+    
+
+def main():
+    tests = [
+        testFit,
+        testSheet,
+        testUnit, 
+        testVariable,
+        testProp,
+        testSolve
+    ]
+
+    suites = []
+    for test in tests:
+        suites.append(unittest.TestLoader().loadTestsFromTestCase(test))
+
+    suite = unittest.TestSuite(suites)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+
+if __name__ == '__main__':
+    main()
