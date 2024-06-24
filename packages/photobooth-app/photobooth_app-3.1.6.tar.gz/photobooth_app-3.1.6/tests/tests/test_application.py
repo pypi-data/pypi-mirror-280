e15168dc77416c978929d6988d1426f6cc306f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:05:30 +0000

    Add package photobooth-app/3.1.6/photobooth_app-3.1.6.tar.gz

---
+++ packages/photobooth-app/photobooth_app-3.1.6.tar.gz/photobooth_app-3.1.6/tests/tests/test_application.py
@@ -0,0 +1,13 @@
+"""
+Testing virtual camera Backend
+"""
+
+import logging
+
+logger = logging.getLogger(name=None)
+
+
+def test_app():
+    import photobooth.application
+
+    photobooth.application._create_app()
