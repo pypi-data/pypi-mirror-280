b3ed4676001b34f8b47052ff2ea1115418ecf91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:05:22 +0000

    Add package open-irceline/0.0.6/open_irceline-0.0.6-py3-none-any.whl

---
+++ packages/open-irceline/open_irceline-0.0.6-py3-none-any.whl/open_irceline/__init__.py
@@ -0,0 +1,5 @@
+from .api import IrcelineRioClient, IrcelineForecastClient, IrcelineApiError
+from .belaqi import belaqi_index, belaqi_index_actual, belaqi_index_forecast
+from .data import RioFeature, ForecastFeature, FeatureValue, BelAqiIndex
+
+__version__ = '0.0.6'
