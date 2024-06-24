b22f85ec2260e12024082df52471537514e9c76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:05:55 +0000

    Add package octoflow/0.1.17/octoflow-0.1.17.tar.gz

---
+++ packages/octoflow/octoflow-0.1.17.tar.gz/octoflow-0.1.17/octoflow/__init__.py
@@ -0,0 +1,30 @@
+import contextlib
+
+from octoflow import logging
+from octoflow.config import config
+from octoflow.core import Module, Task, TaskManager
+from octoflow.utils.config import Config
+
+__version__ = "0.1.17"
+
+__all__ = [
+    "Config",
+    "Module",
+    "Task",
+    "TaskManager",
+    "config",
+    "logger",
+    "logging",
+]
+
+logging.logger = logging.get_logger(
+    name="octoflow",
+    level=config.logging.level,
+    formatter=config.logging.format,
+)
+
+with contextlib.suppress(ImportError):
+    from octoflow_plugins import package as default_plugins_package
+
+if "default_plugins_package" in locals():
+    default_plugins_package.import_modules()
