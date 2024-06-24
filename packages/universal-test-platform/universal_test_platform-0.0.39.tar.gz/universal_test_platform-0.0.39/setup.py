1c1ea1f1087896c5f5cf885799dde75bd4e48c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:05:43 +0000

    Add package universal-test-platform/0.0.39/universal_test_platform-0.0.39.tar.gz

---
+++ packages/universal-test-platform/universal_test_platform-0.0.39.tar.gz/universal_test_platform-0.0.39/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='universal-test-platform',
+    version='0.0.39',
+
+    package_dir  = {'': 'src'},
+    package_data = {'utp': ['*']},
+    packages     = find_packages('src'),
+    install_requires=['Click', 'pyyaml', 'robotlibcore-temp', 'robotframework-appiumlibrary', 'robotframework-seleniumlibrary', 'requests' ],
+    entry_points={
+        'console_scripts': [
+            'utp = utp.utp:cli'
+        ]
+    }
+)
