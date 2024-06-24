35f01a9860ec6bcb955d032a22a26212326eac21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 04:06:13 +0000

    Add package veriCAS/0.0.1001/veriCAS-0.0.1001-py3-none-any.whl

---
+++ packages/veriCAS/veriCAS-0.0.1001-py3-none-any.whl/veriCAS/__init__.py
@@ -0,0 +1,5 @@
+# veriCAS7/__init__.py
+
+from .atom_eco import parse_smiles_with_coefficients, calculate_atom_economy, get_atom_economy, main, atom_economy
+
+__all__ = ['parse_smiles_with_coefficients', 'calculate_atom_economy', 'get_atom_economy', 'main', 'atom_economy']
