b2c1f930e63ed380310685817632f77c587d7bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 03:05:38 +0000

    Add package textlong/0.4.1/textlong-0.4.1.tar.gz

---
+++ packages/textlong/textlong-0.4.1.tar.gz/textlong-0.4.1/textlong/__init__.py
@@ -0,0 +1,10 @@
+from .writing import idea, outline, from_outline, outline_from_outline
+from .project import Project, list_projects, init_project, is_project_existing
+
+__all__ = [
+  "idea",
+  "outline",
+  "from_outline",
+  "outline_from_outline",
+  "Project",
+]
---
+++ packages/textlong/textlong-0.4.1.tar.gz/textlong-0.4.1/textlong/writing/__init__.py
@@ -0,0 +1,3 @@
+from .markdown import MarkdownLoader
+from .command import Command
+from .base import write, stream_log, idea, outline, from_outline, outline_from_outline
