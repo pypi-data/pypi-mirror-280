671dc6601e17f40ddc894bee48b711292438d59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 17:05:05 +0000

    Add package aws-pdk/0.23.43/aws_pdk-0.23.43-py3-none-any.whl

---
+++ packages/aws-pdk/aws_pdk-0.23.43-py3-none-any.whl/aws_pdk/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_cognito_identitypool_alpha._jsii
+import cdk_nag._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws/pdk", "0.23.43", __name__[0:-6], "pdk@0.23.43.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
