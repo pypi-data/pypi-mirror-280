0110acd44b37b0d84f274b03304a86ebeada2a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:05:40 +0000

    Add package ataraxis-time/1.0.4/ataraxis_time-1.0.4-cp312-abi3-win_amd64.whl

---
+++ packages/ataraxis-time/ataraxis_time-1.0.4-cp312-abi3-win_amd64.whl/ataraxis_time/time_helpers/helper_functions.pyi
@@ -0,0 +1,6 @@
+import numpy as np
+from numpy.typing import NDArray as NDArray
+from typing import Literal
+
+def convert_time(time: int | float | NDArray[np.int8 | np.int16 | np.int32 | np.int64 | np.uint8 | np.uint16 | np.uint32 | np.uint64 | np.float16 | np.float32 | np.float64] | list[int | float], from_units: Literal['ns', 'us', 'ms', 's', 'm', 'h', 'd'], to_units: Literal['ns', 'us', 'ms', 's', 'm', 'h', 'd']) -> float | NDArray[np.float64] | list[float]: ...
+def get_timestamp(time_separator: str = ...) -> str: ...
