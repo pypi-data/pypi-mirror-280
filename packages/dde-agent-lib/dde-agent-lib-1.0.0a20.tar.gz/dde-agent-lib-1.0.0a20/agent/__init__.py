ccb22b2b5963e5393d7bcaeba73db4c4ba06a8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 18:05:58 +0000

    Add package dde-agent-lib/1.0.0a20/dde-agent-lib-1.0.0a20.tar.gz

---
+++ packages/dde-agent-lib/dde-agent-lib-1.0.0a20.tar.gz/dde-agent-lib-1.0.0a20/agent/__init__.py
@@ -0,0 +1,16 @@
+from threading import Timer
+from agent.utils.nacos_util import get_config_from_nacos_every_10_seconds, get_system_config_from_nacos
+
+
+def timedTask():
+    '''
+    第一个参数: 延迟多长时间执行任务(单位: 秒)
+    第二个参数: 要执行的任务, 即函数
+    第三个参数: 调用函数的参数(tuple)
+    '''
+    Timer(10, get_config_from_nacos_every_10_seconds, ()).start()
+
+
+print("lib start")
+get_system_config_from_nacos()
+timedTask()
